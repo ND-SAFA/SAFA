d43534833ef0dc6a7e0f676a47e9280d680aa2ea
Adam Brinckman <abrinckm@gmail.com>
2019-10-24 09:26:05 -0400

    Add label to hazards

--- src/main/java/edu/nd/crc/safa/services/TreeService.java
+++ src/main/java/edu/nd/crc/safa/services/TreeService.java
@@ -23,116 +23,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TreeService {
+
   @Autowired
   Driver driver;
 
   private final static Logger LOG = LoggerFactory.getLogger(TreeService.class);
 
 	public TreeService() {
-	}
-
-// let trees;
-//   if (delta.length) {
-//     trees = [dataDelta.trees[rootId]];
-//   } else {
-//     trees = rootId ? [data.trees[rootId]] : Object.values(data.trees);
-//   }
-//   const elements = [];
-//   // Iterate over root nodes
-//   for (const tree of trees) {
-//     tree['root-node'].__type = 'Hazard';
-//     const stack = [tree['root-node']];
-//     while (stack.length > 0) {
-//       const current = stack.shift();
-//       let classes = 'node';
-//       // Add current node to elements
-//       let label = `${current.__type}\n${current.id}\n${current.name}`;
-//       if (current.__type === 'AcceptanceTest') {
-//         label = `Acceptance test passed\n${current.id}\n${current.name}`;
-//       } else if (current.__type === 'Package') {
-//         // split ID along '\n' character to get ID and package name
-//         const packageName = current.id.split('\\n');
-//         label = `Package\n${packageName[0]}\n${packageName[1]}`;
-//       } else if (current.__type === 'Code') {
-//         // Parse file name from the end of current.name
-//         const slashIndex = current.name.lastIndexOf('/');
-//         const fileName = current.name.slice(slashIndex + 1);
-//         label = `Code with\nPassed Tests\n${fileName}`;
-//       }
-//       if (current['is-delegated']) {
-//         label = `Delegated to Adjacent System\n${current.id}\n${current.name}`;
-//       }
-//       if (current['is-modified']) {
-//         classes = 'node_modified';
-//       }
-//       if (current['is-new']) {
-//         classes = 'node_new';
-//       }
-//       if (current['is-deleted']) {
-//         classes = 'node_deleted';
-//       }
-//       elements.push({
-//         data: {
-//           id: current.id,
-//           label: label,
-//           // TODO replace with valid link to issue tracker
-//           href: 'https://notredamus.atlassian.net/browse/SAFA-14'
-//         },
-//         classes: classes
-//       });
-//       // Add an edge for each child
-//       const children = Object.entries(current.children);
-//       for (const [childType, childArray] of children) {
-//         for (const child of childArray) {
-//           elements.push({
-//             data: {
-//               source: current.id,
-//               target: child.DATA.id
-//             }
-//           });
-//           // Add child node to stack including node type
-//           child.DATA.__type = childType;
-//           stack.unshift(child.DATA);
-//         }
-//       }
-//     }
-
-  // private Map<String, Object> toCustomCytoscapeFormat(Collection<Tree> artifacts) {
-  //   artifacts.forEach(a -> System.out.println(a.getData()));
-  //   return new HashMap<String, Object>(2);
-  //   for each artifact 
-  // }
-
-	// private Map<String, Object> toD3Format(Collection<Tree> artifacts) {
-	// 	List<Map<String, Object>> nodes = new ArrayList<>();
-	// 	List<Map<String, Object>> rels = new ArrayList<>();
-  //   int i = 0;
-	// 	Iterator<Tree> result = artifacts.iterator();
-	// 	while (result.hasNext()) {
-	// 		Tree artifact = result.next();
-	// 		nodes.add(map("title", artifact.getTitle(), "label", "artifact"));
-	// 		int target = i;
-	// 		i++;
-	// 		for (Role role : artifact.getRoles()) {
-	// 			Map<String, Object> actor = map("title", role.getPerson().getName(), "label", "actor");
-	// 			int source = nodes.indexOf(actor);
-	// 			if (source == -1) {
-	// 				nodes.add(actor);
-	// 				source = i++;
-	// 			}
-	// 			rels.add(map("source", source, "target", target));
-	// 		}
-	// 	}
-	// 	return map("nodes", nodes, "links", rels);
-	// }
-
-	// private Map<String, Object> map(String key1, Object value1, String key2, Object value2) {
-	// 	Map<String, Object> result = new HashMap<String, Object>(2);
-	// 	result.put(key1, value1);
-	// 	result.put(key2, value2);
-	// 	return result;
-  // }
-
+  }
+  
+  public TreeService(Driver driver) {
+    this.driver = driver;
+  }
   
 	@Transactional(readOnly = true)
   public List<Map<String, Object>> trees(String projectId, String root) {
@@ -156,8 +58,10 @@ public class TreeService {
       for(int i = 0; i < records.size(); i++) {
         Record rec = records.get(i);
         Node node = (Node)rec.get("h").asObject();
+        String label = node.labels().iterator().next();
         Map<String, Object> mapping = new HashMap<String, Object>(node.asMap());
         mapping.put("classes", "node");
+        mapping.put("label", label);
         values.add(mapping);
         LOG.debug("[NODE " + node.id() +"] " + mapping);
       }
@@ -176,7 +80,7 @@ public class TreeService {
       }
 
       addNode(rec.get("artifact"), values, ids);
-      
+
       {
         ListValue rels = (ListValue)rec.get("rel");
         rels.asObject().forEach(o -> {
@@ -206,18 +110,18 @@ public class TreeService {
     ids.put(node.id(), node.get("id").asString());
   }
   
-  private static <T> Collection<T>  
-                  getCollectionFromIterable(Iterable<T> itr) 
-  { 
-      // Create an empty Collection to hold the result 
-      Collection<T> cltn = new ArrayList<T>(); 
+  // private static <T> Collection<T>  
+  //                 getCollectionFromIterable(Iterable<T> itr) 
+  // { 
+  //     // Create an empty Collection to hold the result 
+  //     Collection<T> cltn = new ArrayList<T>(); 
 
-      // Iterate through the iterable to 
-      // add each element into the collection 
-      for (T t : itr) 
-          cltn.add(t); 
+  //     // Iterate through the iterable to 
+  //     // add each element into the collection 
+  //     for (T t : itr) 
+  //         cltn.add(t); 
 
-      // Return the converted collection 
-      return cltn; 
-  } 
+  //     // Return the converted collection 
+  //     return cltn; 
+  // } 
 }

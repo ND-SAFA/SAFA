6dc4d00940ef49d535212bf6870e72f3fafca7f7
James Sweet <james.sweet88@googlemail.com>
2019-11-22 10:57:04 -0500

    Added code to modify issue nodes as well as fix query to only return the latest modification relationship

--- src/main/java/edu/nd/crc/safa/services/TreeService.java
+++ src/main/java/edu/nd/crc/safa/services/TreeService.java
@@ -92,7 +92,7 @@ public class TreeService {
   @Transactional(readOnly = true)
   public List<Map<String, Object>> versions(String projectId, String root, int version) {
     try ( Session session = driver.session() ) {
-      String query = "MATCH (p)-[r:UPDATES]->(c)\n" +
+      String query ="MATCH (p)-[r:UPDATES]->(c)\n" +
         String.format("WHERE r.version <= %s\n", version) +
         "WITH p, c, r AS rs ORDER BY r.version DESC\n" +
         "WITH p, c, head(collect([p, rs, c])) AS removed\n" +
@@ -101,12 +101,16 @@ public class TreeService {
         // Find all removed relationships
         "OPTIONAL MATCH (a)-[eR]-(b)\n" +
         "WHERE a IN eHead AND b IN eTail\n" +
-        "WITH eNo, apoc.coll.toSet(apoc.coll.flatten(collect(eR))) AS eRelationships\n" +
+        "WITH eNo, apoc.coll.toSet(apoc.coll.flatten(collect(eR))) AS eRel\n" +
+        // Remove modification relations newer than requested version
+        "OPTIONAL MATCH (a)-[eR:UPDATES]-(b)\n" +
+        String.format("WHERE eR.version > %s AND eR.type='MODIFIED'\n", version) +
+        "WITH eNo, apoc.coll.toSet(apoc.coll.flatten([eRel,collect(eR)])) AS eRelationships\n" +
         // Find any nodes added after wanted version
         "OPTIONAL MATCH (p)-[r:UPDATES]->(c)\n" +
         "WITH eRelationships, eNo, p, c, r AS rs ORDER BY r.version\n" +
         "WITH eRelationships, eNo, p, c, head(collect([p, rs, c])) AS removed\n" +
-        String.format("WITH eRelationships, eNo, apoc.coll.toSet([c in collect(removed) WHERE c[1].type='ADD' AND c[1].version > %s | c[2]]) as added\n", version) +
+        String.format("WITH eRelationships, eNo, apoc.coll.toSet([c in collect(removed) WHERE c[1].type<>'REMOVE' AND c[1].version > %s | c[2]]) as added\n", version) +
         "WITH eRelationships, apoc.coll.toSet(apoc.coll.flatten([eNo, added])) AS eNodes\n" +
         // Get Paths
         String.format("MATCH (h:Hazard {id: '%s'})\n", root) +
@@ -114,7 +118,7 @@ public class TreeService {
         // Prune unwanted nodes and relationships
         "WHERE NOT ANY(e IN eRelationships WHERE e IN relationships(path)) AND NOT ANY(e IN eNodes WHERE e IN nodes(path))\n" +
         // Return a unique set of nodes and relationships
-        "RETURN apoc.coll.toSet(apoc.coll.flatten(collect(nodes(path)))) AS artifact, apoc.coll.toSet(apoc.coll.flatten(collect(relationships(path)))) AS rel\n";
+        "RETURN apoc.coll.toSet(apoc.coll.flatten(collect(nodes(path)))) AS artifact, apoc.coll.toSet(apoc.coll.flatten(collect([r in relationships(path)]))) AS rel\n";
       StatementResult result = session.run(query);
       return parseArtifactTree(result);
     }
@@ -134,6 +138,20 @@ public class TreeService {
       addNode(nodes.get(i), values, ids);
     }
 
+    // Find the highest version of the modification
+    Map<String, Integer> maxModification = new HashMap<String, Integer>();
+    for( int i = 0; i < rels.size(); i++ ){
+      final Relationship r = rels.get(i);
+      if( r.type().equals("UPDATES") && r.get("type").asString().equals("MODIFIED") ){
+        final String root = ids.get(r.startNodeId());
+        final int version = r.get("version").asInt();
+
+        if( maxModification.getOrDefault(root, 0) < version ){
+          maxModification.put(root, version);
+        }
+      }
+    }
+
     for(int i = 0; i < rels.size(); i++) {
       final Relationship r = rels.get(i);
       if( !r.type().equals("UPDATES") ){
@@ -141,16 +159,32 @@ public class TreeService {
       }else{
         // Handle modifications
         if( r.get("type").asString().equals("MODIFIED") ) {
-          String root = ids.get(r.startNodeId());
+          final String root = ids.get(r.startNodeId());
+
+          // Make sure we only apply the latest version
+          final int version = r.get("version").asInt();
+          if( maxModification.get(root) != version ){
+            continue;
+          }
+
+          // Handle updating nodes
           for( Map<String,Object> value : values ){
             if( value.get("id").equals(root) ){
+              if( !value.get("label").equals("Code") && !value.get("label").equals("Package") ){
+                if( !value.containsKey("original") ){
+                  value.put("original", value.get("DATA").toString());
+                }
+                value.put("DATA", r.get("data").asString());
+              }
+
               if( value.get("label").equals("Code") ){
                 if( !value.containsKey("original") ){
                   value.put("original", value.get("commit").toString());
                 }
                 value.put("commit", r.get("data").asString());
-                value.put("modified", true);
               }
+
+              value.put("modified", true);
             }
           }
         }

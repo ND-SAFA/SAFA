6f8d19d6b8e347767b6be3566194e253a20d3b6d
lbakke <lbakke@nd.edu>
2021-01-20 17:12:39 -0600

    got a basic table created from backend

--- src/main/java/edu/nd/crc/safa/importer/MySQL.java
+++ src/main/java/edu/nd/crc/safa/importer/MySQL.java
@@ -3,6 +3,7 @@ package edu.nd.crc.safa.importer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +25,21 @@ public class MySQL {
             conn = DriverManager.getConnection(databaseURL,user,password);
             if (conn != null) {
                 System.out.println("Connected to the database");
+                System.out.println("Connected database successfully...");
+      
+                //STEP 4: Execute a query
+                System.out.println("Creating table in given database...");
+                Statement stmt = conn.createStatement();
+                
+                String sql = "CREATE TABLE TEST " +
+                             "(id INTEGER not NULL, " +
+                             " first VARCHAR(255), " + 
+                             " last VARCHAR(255), " + 
+                             " age INTEGER, " + 
+                             " PRIMARY KEY ( id ))"; 
+          
+                stmt.executeUpdate(sql);
+                System.out.println("Created table in given database...");
             }
         } catch (ClassNotFoundException ex) {
             System.out.println("Could not find database driver class");

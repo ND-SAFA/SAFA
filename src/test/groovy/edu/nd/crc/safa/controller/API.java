f34a06ad6949b8831dd9b7aa4a10b8f3577258ec
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-07-18 14:10:13 -0700

    removing unit tests for MySQL connection
    We don't have access to the service account credentials during the build process. Accessing these credentials is necessary to test the connection to the MySQL server therefore for the time being we will not test the connection. Instead, I am shifting towards being able to mock the DB connection so we can still run unit tests and the such.

--- src/test/groovy/edu/nd/crc/safa/controller/API.java
+++ src/test/groovy/edu/nd/crc/safa/controller/API.java
@@ -1,7 +1,5 @@
 package edu.nd.crc.safa.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import edu.nd.crc.safa.controller.projects.ProjectsController;
 
 import org.junit.jupiter.api.Test;
@@ -11,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,18 +17,10 @@ import org.springframework.test.web.servlet.MockMvc;
 public class API {
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
     private ProjectsController controller;
 
     @Test
     public void contextLoads() {
         assert controller != null;
-    }
-
-    @Test
-    public void testConnections() throws Exception {
-        mockMvc.perform(get("/connections"));
     }
 }

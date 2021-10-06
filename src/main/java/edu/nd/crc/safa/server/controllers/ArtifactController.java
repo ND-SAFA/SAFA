e0898035682d3bc5b44f9f014186762fa470346f
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-10-06 19:00:23 -0400

    Created ArtifactController for creating new artifacts

---
+++ src/main/java/edu/nd/crc/safa/server/controllers/ArtifactController.java
@@ -0,0 +1,77 @@
+package edu.nd.crc.safa.server.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import edu.nd.crc.safa.server.db.entities.app.ArtifactAppEntity;
+import edu.nd.crc.safa.server.db.entities.sql.Artifact;
+import edu.nd.crc.safa.server.db.entities.sql.Project;
+import edu.nd.crc.safa.server.db.entities.sql.ProjectVersion;
+import edu.nd.crc.safa.server.db.repositories.ArtifactRepository;
+import edu.nd.crc.safa.server.db.repositories.ProjectRepository;
+import edu.nd.crc.safa.server.db.repositories.ProjectVersionRepository;
+import edu.nd.crc.safa.server.responses.ServerError;
+import edu.nd.crc.safa.server.responses.ServerResponse;
+import edu.nd.crc.safa.server.services.ArtifactService;
+import edu.nd.crc.safa.server.services.ProjectService;
+import edu.nd.crc.safa.server.services.VersionService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class ArtifactController extends BaseController {
+
+    ProjectService projectService;
+    ArtifactService artifactService;
+    VersionService versionService;
+
+    ArtifactRepository artifactRepository;
+
+    @Autowired
+    public ArtifactController(ProjectRepository projectRepository,
+                              ProjectVersionRepository projectVersionRepository,
+                              ProjectService projectService,
+                              ArtifactService artifactService,
+                              VersionService versionService,
+                              ArtifactRepository artifactRepository) {
+        super(projectRepository, projectVersionRepository);
+        this.projectService = projectService;
+        this.artifactService = artifactService;
+        this.versionService = versionService;
+        this.artifactRepository = artifactRepository;
+    }
+
+    @PostMapping(value = "projects/{projectId}/artifacts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ServerResponse updateProjectVersionFromFlatFiles(
+        @PathVariable UUID projectId,
+        @RequestBody ArtifactAppEntity artifact) throws ServerError {
+        Project project = this.projectRepository.findByProjectId(projectId);
+        ProjectVersion projectVersion = this.versionService.createNextRevision(project);
+        this.artifactService.createOrUpdateArtifacts(projectVersion, List.of(artifact));
+        return new ServerResponse(artifact);
+    }
+
+    @GetMapping("projects/{projectId}/artifacts/validate/{artifactName}")
+    public ServerResponse checkIfNameExists(@PathVariable UUID projectId, @PathVariable String artifactName)
+        throws JSONException {
+        Project project = this.projectRepository.findByProjectId(projectId);
+        Optional<Artifact> artifactQuery = this.artifactRepository.findByProjectAndName(project, artifactName);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("artifactExists", artifactQuery.isPresent());
+        return new ServerResponse(response);
+    }
+}

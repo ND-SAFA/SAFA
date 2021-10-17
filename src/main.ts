382f86dca4a38015bb4d9519a98bb5d768ad9e29
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-10-17 13:25:15 -0700

    fixing bug where vue-cli-3 does not include material ui icons

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import vuetify from "@/plugins/vuetify";
 import "@/registerServiceWorker";
 import router from "@/router";
 import store from "@/store";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
 

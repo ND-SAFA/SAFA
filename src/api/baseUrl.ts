948eef20feca315545ad94b628cc930b59ed7067
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-10-16 17:01:06 -0700

    uploading deployment file

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -2,5 +2,3 @@ import dotenv, { DotenvConfigOutput } from "dotenv";
 
 const config: DotenvConfigOutput = dotenv.config();
 export const baseURL = config.parsed?.API_ENDPOINT;
-
-console.log("URL", baseURL);

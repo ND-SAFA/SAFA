43f9fc180c64def23e86a741af7a4115981261ee
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-10-25 19:08:48 -0400

    created modal for viewing error logs

---
+++ src/components/common/modals/ServerErrorModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <GenericModal
+    title="Server Errors"
+    :isOpen="isOpen"
+    :actionsHeight="0"
+    :isLoading="isLoading"
+    @onClose="onClose"
+  >
+    <template v-slot:body>
+      <code
+        v-for="(error, errorIndex) in errors"
+        :key="errorIndex"
+        justify="left"
+      >
+        <b>{{ errorIndex }}:</b>
+        <p>{{ error }}</p>
+      </code>
+    </template>
+  </GenericModal>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import GenericModal from "@/components/common/modals/GenericModal.vue";
+import { appModule } from "@/store";
+import { PanelType } from "@/types/store";
+
+export default Vue.extend({
+  components: {
+    GenericModal,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  props: {
+    isOpen: Boolean,
+    errors: Array as PropType<string[]>,
+  },
+  methods: {
+    onClose() {
+      appModule.closePanel(PanelType.errorDisplay);
+    },
+  },
+});
+</script>

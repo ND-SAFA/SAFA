db980b1480f1172f0c1f52b707a74d1e15eac44e
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-11-06 19:34:15 -0400

    testing that artifat can be created and deleted

--- src/test/java/unit/controllers/TestArtifactController.java
+++ src/test/java/unit/controllers/TestArtifactController.java
@@ -4,12 +4,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
+import java.util.Optional;
 
+import edu.nd.crc.safa.server.entities.db.Artifact;
 import edu.nd.crc.safa.server.entities.db.ArtifactBody;
 import edu.nd.crc.safa.server.entities.db.ModificationType;
 import edu.nd.crc.safa.server.entities.db.ProjectVersion;
 import edu.nd.crc.safa.server.services.DeltaService;
 
+import org.javatuples.Pair;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,42 +23,106 @@ public class TestArtifactController extends EntityBaseTest {
     @Autowired
     DeltaService deltaService;
 
+    /**
+     * Verifies that an artifact can be created and updated through ArtifactController.
+     */
     @Test
     public void testModificationDetected() throws Exception {
         String projectName = "test-project";
         String artifactName = "RE-10";
         String modifiedSummary = "this is a new summary";
 
+        // Step - POST artifact and return entities created
+        Pair<ProjectVersion, JSONObject> response = createArtifact(projectName, artifactName);
+        ProjectVersion projectVersion = response.getValue0();
+        String versionId = projectVersion.getVersionId().toString();
+        JSONObject artifactJson = response.getValue1();
+
+        verifyArtifactExists(projectVersion, artifactName);
+
+        // Step - Modify artifact summary
+        artifactJson.put("summary", modifiedSummary);
+
+        // VP - Submit modified artifact for saving
+        sendPost(getArtifactUrl(versionId), artifactJson, status().isCreated());
+
+        // Step - Retrieve ArtifactBodies
+        List<ArtifactBody> artifactBodies = artifactBodyRepository.getBodiesWithName(projectVersion.getProject(),
+            artifactName);
+
+        // VP - Verify one single artifact body
+        assertThat(artifactBodies.size()).isEqualTo(1);
+
+        // VP - Verify body contains modification
+        ArtifactBody body = artifactBodies.get(0);
+        assertThat(body.getModificationType()).isEqualTo(ModificationType.ADDED);
+        assertThat(body.getSummary()).isEqualTo(modifiedSummary);
+    }
+
+    /**
+     * Verifies that an artifact can be created and updated through ArtifactController.
+     */
+    @Test
+    public void testDeleteArtifact() throws Exception {
+        String projectName = "test-project";
+        String artifactName = "RE-10";
+
+        // Step - Create artifact
+        Pair<ProjectVersion, JSONObject> response = createArtifact(projectName, artifactName);
+        ProjectVersion projectVersion = response.getValue0();
+        String projectId = projectVersion.getProject().getProjectId().toString();
+
+        // VP - Verify that artifact exists
+        verifyArtifactExists(projectVersion, artifactName);
+
+        // Step - Delete artifact
+        String deleteUrl = String.format("/projects/%s/artifacts/%s", projectId, artifactName);
+        sendDelete(deleteUrl, status().is2xxSuccessful());
+
+        // VP - Verify artifact does not exist
+        verifyArtifactDoesNotExist(projectVersion, artifactName);
+    }
+
+    private Pair<ProjectVersion, JSONObject> createArtifact(String projectName, String artifactName) throws Exception {
         // Step - Create project with single version
         ProjectVersion projectVersion = entityBuilder
             .newProject(projectName)
             .newVersionWithReturn(projectName);
 
         // Step - Create Artifact JSON
-        JSONObject artifact = jsonBuilder
+        JSONObject artifactJson = jsonBuilder
             .withProject(projectName, projectName)
             .withArtifactAndReturn(projectName, artifactName, "requirements", "this is a body");
 
-        // Step - Send request to create artifact via json
+        // Step - Send request to create artifact
         String versionId = projectVersion.getVersionId().toString();
-        String url = String.format("/projects/versions/%s/artifacts", versionId);
-        sendPost(url, artifact, status().isCreated());
+        sendPost(getArtifactUrl(versionId), artifactJson, status().isCreated());
 
-        // Step - Modify artifact
-        artifact.put("summary", modifiedSummary);
+        return new Pair<>(projectVersion, artifactJson);
+    }
 
-        // VP - Submit modified artifact for saving
-        sendPost(url, artifact, status().isCreated());
+    private String getArtifactUrl(String versionId) {
+        return String.format("/projects/versions/%s/artifacts", versionId);
+    }
 
-        // Step - Retrieve ArtifactBodies
-        List<ArtifactBody> artifactBodies = artifactBodyRepository.getBodiesWithName(projectVersion.getProject(),
+    private void verifyArtifactExists(ProjectVersion projectVersion, String artifactName) {
+        verifyArtifactStatus(projectVersion, artifactName, true);
+    }
+
+    private void verifyArtifactDoesNotExist(ProjectVersion projectVersion, String artifactName) {
+        verifyArtifactStatus(projectVersion, artifactName, false);
+    }
+
+    private void verifyArtifactStatus(ProjectVersion projectVersion, String artifactName, boolean exists) {
+        Optional<Artifact> artifactQuery = this.artifactRepository.findByProjectAndName(projectVersion.getProject(),
             artifactName);
 
-        // VP - Only single artifact body exist and contains modification
-        assertThat(artifactBodies.size()).isEqualTo(1);
-        ArtifactBody body = artifactBodies.get(0);
-        assertThat(body.getModificationType()).isEqualTo(ModificationType.ADDED);
-        assertThat(body.getSummary()).isEqualTo(modifiedSummary);
+        assertThat(artifactQuery.isPresent()).isEqualTo(exists);
+        if (exists) {
+            Artifact artifact = artifactQuery.get();
+            List<ArtifactBody> artifactBody = this.artifactBodyRepository.findByArtifact(artifact);
+            assertThat(artifactBody.size()).isEqualTo(1);
+        }
     }
 
     @Test

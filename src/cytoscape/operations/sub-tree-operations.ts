6f54d8def319cf1b0a26cb66db2f34fd54bf1a4a
Tim <tjnewman111@gmail.com>
2021-11-13 10:55:23 -0800

    Fixed get root node infinite looping

--- src/cytoscape/operations/sub-tree-operations.ts
+++ src/cytoscape/operations/sub-tree-operations.ts
@@ -60,10 +60,12 @@ function getSubTree(
  *
  * @param cyPromise - A promise returning cytoscape whose root node is returned.
  * @param currentNode - Defines where we are in the tree during recursion.
+ * @param traversedNodes - A list of all traversed node IDs to avoid loops.
  */
 export async function getRootNode(
   cyPromise: Promise<CytoCore>,
-  currentNode?: SingularElementArgument
+  currentNode?: SingularElementArgument,
+  traversedNodes: string[] = []
 ): Promise<SingularElementArgument> {
   const cyCore = await cyPromise;
 
@@ -75,6 +77,13 @@ export async function getRootNode(
     currentNode = getMostConnectedNode(cyCore);
   }
 
+  // Avoid getting stuck in cycles.
+  if (traversedNodes.includes(currentNode.id())) {
+    throw Error("Root node doesnt exist on a circular graph.");
+  } else {
+    traversedNodes.push(currentNode.id());
+  }
+
   const edgesOutOfNode: EdgeCollection = cyCore
     .edges()
     .filter((e) => e.target() === currentNode);
@@ -82,7 +91,7 @@ export async function getRootNode(
   if (edgesOutOfNode.length === 0) {
     return currentNode;
   } else {
-    return getRootNode(cyPromise, edgesOutOfNode[0].source());
+    return getRootNode(cyPromise, edgesOutOfNode[0].source(), traversedNodes);
   }
 }
 

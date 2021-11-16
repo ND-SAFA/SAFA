8d5a88da23e8030010059b0169d8868082eacc59
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-11-16 15:38:27 -0500

    adding DetailsService implementation with create user function

---
+++ src/main/java/edu/nd/crc/safa/server/services/SafaUserService.java
@@ -0,0 +1,52 @@
+package edu.nd.crc.safa.server.services;
+
+import edu.nd.crc.safa.server.entities.db.SafaUser;
+import edu.nd.crc.safa.server.repositories.SafaUserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SafaUserService implements UserDetailsService {
+
+    SafaUserRepository safaUserRepository;
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SafaUserService(SafaUserRepository safaUserRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.safaUserRepository = safaUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final SafaUser customer = safaUserRepository.findByEmail(username);
+        if (customer == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return User
+            .withUsername(customer.getEmail())
+            .password(customer.getPassword())
+            .authorities("USER") // TODO: Replace with custom roles here
+            .build();
+    }
+
+    public SafaUser createNewUser(SafaUser newUser) {
+        newUser.setPassword(this.passwordEncoder.encode(newUser.getPassword()));
+        this.safaUserRepository.save(newUser);
+        return newUser;
+    }
+
+    public boolean loginUser(SafaUser user) {
+        SafaUser userFound = this.safaUserRepository.findByEmail(user.getEmail());
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
+        System.out.println("Encoded Password: " + encodedPassword);
+        return userFound.getPassword().equals(encodedPassword);
+    }
+}

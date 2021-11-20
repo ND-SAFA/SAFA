49aa1ae2ee5cbf14bcbfbd1eee57e9c27d56c222
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2021-11-20 17:36:26 -0500

    clarifying flag change

--- src/api/endpoints/session-api.ts
+++ src/api/endpoints/session-api.ts
@@ -12,7 +12,7 @@ import { baseURL, Endpoint, fillEndpoint } from "./endpoints";
  */
 
 const TEST_ENDPOINTS = true;
-const TEST_SESSION_EXISTING = false;
+const TEST_SESSION_EXISTING = false; // false required for API to work otherwise no token
 
 /**
  * Custom fetch call for session endpoints.

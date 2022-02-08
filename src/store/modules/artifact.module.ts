8938e70192413be7194f40a824aa13e23ac26d60
Tim <tjnewman111@gmail.com>
2022-02-07 18:04:29 -0800

    Completed artifact and trace modules

--- src/store/modules/artifact.module.ts
+++ src/store/modules/artifact.module.ts
@@ -1,6 +1,12 @@
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 
-import type { Artifact, DocumentArtifacts } from "@/types";
+import type {
+  Artifact,
+  DocumentArtifacts,
+  ArtifactQueryFunction,
+} from "@/types";
+import { getSingleQueryResult } from "@/util";
+import { artifactSelectionModule, subtreeModule } from "@/store";
 
 @Module({ namespaced: true, name: "artifact" })
 /**
@@ -31,6 +37,50 @@ export default class ArtifactModule extends VuexModule {
     );
   }
 
+  @Action
+  /**
+   * Updates the current artifacts in the project, preserving any that already existed.
+   *
+   * @param artifacts - The artifacts to set.
+   */
+  async addOrUpdateArtifacts(newArtifacts: Artifact[]): Promise<void> {
+    const newIds = newArtifacts.map(({ id }) => id);
+    const createNewArtifacts = (currentArtifacts: Artifact[]) => [
+      ...currentArtifacts.filter(({ id }) => !newIds.includes(id)),
+      ...newArtifacts,
+    ];
+
+    this.SET_PROJECT_ARTIFACTS(createNewArtifacts(this.projectArtifacts));
+    this.SET_CURRENT_ARTIFACTS(createNewArtifacts(this.currentArtifacts));
+
+    await subtreeModule.updateSubtreeMap();
+
+    const selectedArtifact = artifactSelectionModule.getSelectedArtifact;
+
+    if (selectedArtifact !== undefined) {
+      const query = newArtifacts.filter(
+        ({ name }) => name === selectedArtifact.name
+      );
+      if (query.length > 0) {
+        await artifactSelectionModule.selectArtifact(query[0].id);
+      }
+    }
+  }
+
+  @Action
+  /**
+   * Deletes the artifact with the given name.
+   */
+  async deleteArtifactByName(artifact: Artifact): Promise<void> {
+    const removeArtifact = (currentArtifacts: Artifact[]) =>
+      currentArtifacts.filter(({ name }) => name !== artifact.name);
+
+    this.SET_PROJECT_ARTIFACTS(removeArtifact(this.projectArtifacts));
+    this.SET_CURRENT_ARTIFACTS(removeArtifact(this.currentArtifacts));
+
+    await subtreeModule.updateSubtreeMap();
+  }
+
   @Mutation
   /**
    * Sets the project artifacts.
@@ -59,5 +109,38 @@ export default class ArtifactModule extends VuexModule {
    */
   get artifacts(): Artifact[] {
     return this.currentArtifacts;
+  }
+
+  /**
+   * @return A function for finding an artifact by name.
+   * @throws If more or less than 1 artifact is found to match.
+   */
+  get getArtifactByName(): ArtifactQueryFunction {
+    return (artifactName) => {
+      const query = this.artifacts.filter((a) => a.name === artifactName);
+
+      return getSingleQueryResult(query, `Find by name: ${artifactName}`);
+    };
+  }
+
+  /**
+   * @return A function for finding an artifact by id.
+   * @throws If more or less than 1 artifact is found to match.
+   */
+  get getArtifactById(): ArtifactQueryFunction {
+    return (targetArtifactId) => {
+      const query = this.artifacts.filter((a) => a.id === targetArtifactId);
+
+      return getSingleQueryResult(query, `Find by id: ${targetArtifactId}`);
+    };
+  }
+
+  /**
+   * @return A collection of artifacts, keyed by their id.
+   */
+  get getArtifactsById(): Record<string, Artifact> {
+    return this.artifacts
+      .map((artifact) => ({ [artifact.id]: artifact }))
+      .reduce((acc, cur) => ({ ...acc, ...cur }), {});
   }
 }
--- src/store/modules/trace.module.ts
+++ src/store/modules/trace.module.ts
@@ -1,6 +1,13 @@
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 
-import type { TraceLink, DocumentTraces } from "@/types";
+import type {
+  TraceLink,
+  DocumentTraces,
+  LinkFinder,
+  LinkValidator,
+} from "@/types";
+import { subtreeModule } from "@/store";
+import { getTraceId } from "@/util";
 
 @Module({ namespaced: true, name: "trace" })
 /**
@@ -35,6 +42,45 @@ export default class TraceModule extends VuexModule {
     );
   }
 
+  @Action
+  /**
+   * Updates the current trace links in the project, preserving any that already existed.
+   *
+   * @param traceLinks - The trace links to set.
+   */
+  async addOrUpdateTraceLinks(newTraces: TraceLink[]): Promise<void> {
+    const newIds = newTraces.map(({ traceLinkId }) => traceLinkId);
+    const createNewLinks = (currentTraces: TraceLink[]) => [
+      ...currentTraces.filter(
+        ({ traceLinkId }) => !newIds.includes(traceLinkId)
+      ),
+      ...newTraces,
+    ];
+
+    this.SET_PROJECT_TRACES(createNewLinks(this.projectTraces));
+    this.SET_CURRENT_TRACES(createNewLinks(this.currentTraces));
+
+    await subtreeModule.updateSubtreeMap();
+  }
+
+  @Action
+  /**
+   * Deletes the given trace link.
+   *
+   * @param traceLink - The trace link to remove.
+   */
+  async deleteTraceLink(traceLink: TraceLink): Promise<void> {
+    const removeLink = (currentTraces: TraceLink[]) =>
+      currentTraces.filter(
+        ({ traceLinkId }) => traceLinkId !== traceLink.traceLinkId
+      );
+
+    this.SET_PROJECT_TRACES(removeLink(this.projectTraces));
+    this.SET_CURRENT_TRACES(removeLink(this.currentTraces));
+
+    await subtreeModule.updateSubtreeMap();
+  }
+
   @Mutation
   /**
    * Sets the project trace links.
@@ -63,5 +109,40 @@ export default class TraceModule extends VuexModule {
    */
   get traces(): TraceLink[] {
     return this.currentTraces;
+  }
+
+  /**
+   * @return Returns a function to query a single trace link by the
+   * source and target artifact ids.
+   */
+  get getTraceLinkByArtifacts(): LinkFinder {
+    return (sourceId, targetId) => {
+      const traceQuery = this.traces.filter(
+        (trace) => trace.sourceId === sourceId && trace.targetId === targetId
+      );
+
+      if (traceQuery.length === 0) {
+        throw Error(
+          `Could not find trace link with id: ${getTraceId(sourceId, targetId)}`
+        );
+      }
+
+      return traceQuery[0];
+    };
+  }
+
+  /**
+   * @return A function that determines whether a link with the given source and target IDs exists.
+   */
+  get doesLinkExist(): LinkValidator {
+    return (sourceId, targetId) => {
+      const traceLinkQuery = this.traces.filter(
+        (trace) =>
+          (trace.sourceId === sourceId && trace.targetId === targetId) ||
+          (trace.targetId === sourceId && trace.sourceId === targetId)
+      );
+
+      return traceLinkQuery.length > 0;
+    };
   }
 }

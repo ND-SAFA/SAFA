4877bf0357befd41190c00921af7d79bfebbacf7
Tim <tjnewman111@gmail.com>
2022-02-17 15:50:24 -0800

    small additions to project selection

--- src/components/project/creator/validation-panels/FilePanelController.vue
+++ src/components/project/creator/validation-panels/FilePanelController.vue
@@ -58,7 +58,6 @@ export default Vue.extend({
   },
   computed: {
     isTracePanel(): boolean {
-      console.log(isTracePanel(this.panel), this.panel);
       return isTracePanel(this.panel);
     },
     isValid(): boolean {

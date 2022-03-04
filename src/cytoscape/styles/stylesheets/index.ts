e2751955030a668e896d6958b118444b892c01cd
Tim <tjnewman111@gmail.com>
2022-03-03 17:30:58 -0800

    some more cleanup

--- src/cytoscape/styles/stylesheets/index.ts
+++ src/cytoscape/styles/stylesheets/index.ts
@@ -1,8 +1,8 @@
-import { CytoscapeStyle } from "./base";
-import { EdgeHandlesStyle } from "./edge-handles";
+import { CytoscapeStyle } from "./base-styles";
+import { EdgeHandlesStyle } from "./edge-styles";
 
 export const GraphStyle = [...CytoscapeStyle, ...EdgeHandlesStyle];
 
-export * from "./base";
-export * from "./edge-handles";
+export * from "./base-styles";
+export * from "./edge-styles";
 export * from "./tim-styles";

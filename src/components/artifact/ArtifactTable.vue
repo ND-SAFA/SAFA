dcd5babc0c962ede76c114accfcdfb23230ddfc1
Tim <tjnewman111@gmail.com>
2022-03-16 14:15:10 -0700

    added chips to table

--- src/components/artifact/ArtifactTable.vue
+++ src/components/artifact/ArtifactTable.vue
@@ -5,22 +5,26 @@
         <v-container> </v-container>
       </template>
       <template
-        v-for="textName in textColumnNames"
-        v-slot:[textName]="{ item }"
+        v-for="{ id, dataType } in columns"
+        v-slot:[`item.${id}`]="{ item }"
       >
-        Text
-      </template>
-      <template
-        v-for="relName in relationColumnNames"
-        v-slot:[relName]="{ item }"
-      >
-        Relation
-      </template>
-      <template
-        v-for="selName in selectColumnNames"
-        v-slot:[selName]="{ item }"
-      >
-        Select
+        <span v-if="isFreeText(dataType)" class="text-body-1" :key="id">
+          {{ item[id] }}
+        </span>
+        <div v-if="isRelation(dataType)" :key="id">
+          <artifact-table-chip
+            v-for="artifactId in item[id].split('||')"
+            :key="artifactId"
+            :text="getArtifactName(artifactId)"
+          />
+        </div>
+        <div v-if="isSelect(dataType)" :key="id">
+          <artifact-table-chip
+            v-for="val in item[id].split('||')"
+            :key="val"
+            :text="val"
+          />
+        </div>
       </template>
       <template v-slot:item.actions="{ item }">
         <generic-icon-button
@@ -61,14 +65,15 @@ import Vue from "vue";
 import { Artifact, ColumnDataType } from "@/types";
 import { artifactModule, documentModule } from "@/store";
 import { deleteArtifactFromCurrentVersion } from "@/api";
-import { GenericIconButton, ArtifactCreatorModal } from "@/components/common";
+import { ArtifactCreatorModal, GenericIconButton } from "@/components/common";
+import ArtifactTableChip from "@/components/artifact/ArtifactTableChip.vue";
 
 /**
  * Represents a table of artifacts.
  */
 export default Vue.extend({
   name: "ArtifactTable",
-  components: { GenericIconButton, ArtifactCreatorModal },
+  components: { ArtifactTableChip, GenericIconButton, ArtifactCreatorModal },
   data() {
     return {
       selectedArtifact: undefined as Artifact | undefined,
@@ -92,26 +97,18 @@ export default Vue.extend({
         },
       ];
     },
-    textColumnNames() {
-      return documentModule.tableColumns
-        .filter(({ dataType }) => dataType === ColumnDataType.FREE_TEXT)
-        .map(({ id }) => `item.${id}`);
+    columns() {
+      return documentModule.tableColumns;
     },
-    relationColumnNames() {
-      return documentModule.tableColumns
-        .filter(({ dataType }) => dataType === ColumnDataType.RELATION)
-        .map(({ id }) => `item.${id}`);
-    },
-    selectColumnNames() {
-      return documentModule.tableColumns
-        .filter(({ dataType }) => dataType === ColumnDataType.SELECT)
-        .map(({ id }) => `item.${id}`);
-    },
-
     items() {
-      return artifactModule.artifacts;
+      return artifactModule.artifacts.map((artifact) => ({
+        ...artifact,
+        //TODO: remove test data
+        1: "A",
+        2: "3681f538-0f3f-4e6e-a73c-1ecbb5033a94||bce84af9-704b-4cef-8aea-7e479e27c51f",
+        3: "D||E",
+      }));
     },
-
     artifactCreatorTitle(): string {
       return this.selectedArtifact ? "Edit Artifact" : "Create Artifact";
     },
@@ -131,6 +128,19 @@ export default Vue.extend({
     handleCloseModal() {
       this.createDialogueOpen = false;
       this.selectedArtifact = undefined;
+    },
+
+    isFreeText(dataType: ColumnDataType): boolean {
+      return dataType === ColumnDataType.FREE_TEXT;
+    },
+    isRelation(dataType: ColumnDataType): boolean {
+      return dataType === ColumnDataType.RELATION;
+    },
+    isSelect(dataType: ColumnDataType): boolean {
+      return dataType === ColumnDataType.SELECT;
+    },
+    getArtifactName(id: string): string {
+      return artifactModule.getArtifactById(id).name;
     },
   },
 });

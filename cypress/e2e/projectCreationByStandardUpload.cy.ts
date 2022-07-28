85f15011783015c63a5dfd72245c75c4df13f4bc
thearod5 <vhsalbertorodriguez@gmail.com>
2022-07-28 17:11:12 -0400

    removing boilerplate code for practice

--- cypress/e2e/projectCreationByStandardUpload.cy.ts
+++
@@ -1,7 +0,0 @@
-describe("Project Creation", () => {
-  describe("Manual Project Creation", () => {
-    describe("I can manually create a project", () => {
-      //TODO: https://www.notion.so/nd-safa/I-can-manually-create-a-project-b5e5732f0f9a4b91afe1cf8bb3f6238a
-    });
-  });
-});

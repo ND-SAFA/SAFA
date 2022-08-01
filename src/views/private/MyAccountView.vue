029a987b0a9aabb209ffbdf9d2d2a8e99fcc2a99
Tim <tjnewman111@gmail.com>
2022-08-01 15:44:29 -0700

    Added template for my account page

---
+++ src/views/private/MyAccountView.vue
@@ -0,0 +1,18 @@
+<template>
+  <private-page> </private-page>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { PrivatePage } from "@/components";
+
+/**
+ * Displays the account page.
+ */
+export default Vue.extend({
+  name: "MyAccountView",
+  components: {
+    PrivatePage,
+  },
+});
+</script>

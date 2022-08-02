38f89cad018e13789c8f397568f14182e7129eb7
Tim <tjnewman111@gmail.com>
2022-08-02 15:41:31 -0700

    fixed an issue with the width of drawers erroniously increating

--- src/components/navigation/PrivatePage.vue
+++ src/components/navigation/PrivatePage.vue
@@ -1,27 +1,17 @@
 <template>
   <v-container :class="fullWindow ? 'full-window-page' : ''">
     <slot name="page" />
-    <app-bar />
-    <left-nav-drawer />
-    <right-nav-drawer />
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { LeftNavDrawer, RightNavDrawer } from "./side-panels";
-import AppBar from "./AppBar.vue";
 
 /**
  * Displays a page with top and side bars.
  */
 export default Vue.extend({
   name: "PrivatePage",
-  components: {
-    AppBar,
-    LeftNavDrawer,
-    RightNavDrawer,
-  },
   props: {
     fullWindow: Boolean,
   },

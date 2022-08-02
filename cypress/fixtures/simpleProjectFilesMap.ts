271dec78f2c38c3d9afe9d61ec7bd8cb5cc0244d
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-01 21:27:13 -0400

    adding name mapping for simple project files

---
+++ cypress/fixtures/simpleProjectFilesMap.ts
@@ -0,0 +1,15 @@
+import files from "../fixtures/simpleProjectFiles.json";
+
+export const SimpleProjectFilesMap = {
+  tim: files[0],
+  design: files[1],
+  design2design: files[2],
+  design2requirements: files[3],
+  environmentalassumptions: files[4],
+  environmentalassumptions2hazard: files[5],
+  hazard: files[6],
+  hazard2hazard: files[7],
+  requirement: files[8],
+  requirement2hazard: files[9],
+  requirement2requirement: files[10],
+};

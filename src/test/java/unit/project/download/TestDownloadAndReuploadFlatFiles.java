c8c8f05c0e58104e57d537274e442e5a15770141
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-03 14:41:53 -0400

    REFACTOR: Polishing up test case

---
+++ src/test/java/unit/project/download/TestDownloadAndReuploadFlatFiles.java
@@ -0,0 +1,114 @@
+package unit.project.download;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import edu.nd.crc.safa.builders.entities.ProjectBuilder;
+import edu.nd.crc.safa.builders.requests.SafaRequest;
+import edu.nd.crc.safa.config.AppRoutes;
+import edu.nd.crc.safa.flatfiles.services.DataFileBuilder;
+import edu.nd.crc.safa.server.entities.app.project.ArtifactAppEntity;
+import edu.nd.crc.safa.server.entities.app.project.FTAType;
+import edu.nd.crc.safa.server.entities.app.project.ProjectAppEntity;
+import edu.nd.crc.safa.server.entities.app.project.SafetyCaseType;
+import edu.nd.crc.safa.server.entities.db.DocumentType;
+import edu.nd.crc.safa.server.entities.db.ProjectVersion;
+
+import org.junit.jupiter.api.Test;
+import unit.ApplicationBaseTest;
+
+/**
+ * Creates a project containing one of each type of artifact and attempts to download and
+ * re-upload the project.
+ */
+class TestDownloadAndReuploadFlatFiles extends ApplicationBaseTest {
+    String projectName = "project-name";
+
+    @Test
+    void downloadAndReuploadFlatFiles() throws Exception {
+        setAuthorization();
+        // Step - Create project with artifacts from docs: artifact tree, safety case, fta
+        ProjectVersion projectVersion = ProjectBuilder
+            .withProject(projectName)
+            .withArtifact(Constants.ARTIFACT_TYPE) // R0
+            .withSafetyArtifact(Constants.SAFETY_CASE_TYPE) // C0
+            .withFtaArtifact(Constants.FTA_TYPE) // A0
+            .getCurrentVersion()
+            .getProjectVersion();
+        verifyProjectCreated(projectVersion);
+
+        // Step - Download current project as JSON flat files
+        List<File> projectFiles = new SafaRequest(AppRoutes.Projects.FlatFiles.downloadFlatFiles)
+            .withVersion(projectVersion)
+            .withFileType(DataFileBuilder.AcceptedFileTypes.JSON)
+            .getWithFilesInZip();
+
+        // Step - Create files with flat files downloaded
+        String newVersionIdString = SafaRequest
+            .withRoute(AppRoutes.Projects.FlatFiles.createProjectFromFlatFiles)
+            .getFlatFileHelper()
+            .postWithFiles(projectFiles)
+            .getJSONObject("projectVersion")
+            .getString("versionId");
+
+        // Step - Retrieve new project
+        UUID newVersionId = UUID.fromString(newVersionIdString);
+        ProjectVersion newProjectVersion = this.projectVersionRepository.findByVersionId(newVersionId);
+        verifyProjectCreated(newProjectVersion);
+    }
+
+    private void verifyProjectCreated(ProjectVersion projectVersion) {
+        // Step - Retrieve project
+        ProjectAppEntity projectAppEntity = getProjectAtVersion(projectVersion);
+
+        // VP - Verify that artifacts are created
+        assertThat(projectAppEntity.artifacts.size()).isEqualTo(Constants.N_ARTIFACTS);
+
+        // Step - Extract artifact information
+        Map<String, ArtifactAppEntity> name2artifact = new HashMap<>();
+        projectAppEntity
+            .artifacts
+            .forEach(artifact -> name2artifact.put(artifact.name, artifact));
+
+        // VP - Verify regular artifact
+        String artifactName = Constants.ARTIFACT_NAMES.get(DocumentType.ARTIFACT_TREE);
+        ArtifactAppEntity artifact = name2artifact.get(artifactName);
+        assertThat(artifact.type).isEqualTo(Constants.ARTIFACT_TYPE);
+        assertThat(artifact.documentType).isEqualTo(DocumentType.ARTIFACT_TREE);
+        assertThat(artifact.safetyCaseType).isNull();
+        assertThat(artifact.logicType).isNull();
+
+        // VP - Verify safety artifact
+        String safetyArtifactName = Constants.ARTIFACT_NAMES.get(DocumentType.SAFETY_CASE);
+        ArtifactAppEntity safetyArtifact = name2artifact.get(safetyArtifactName);
+        assertThat(safetyArtifact.type).isEqualTo(Constants.SAFETY_CASE_TYPE.name());
+        assertThat(safetyArtifact.documentType).isEqualTo(DocumentType.SAFETY_CASE);
+        assertThat(safetyArtifact.safetyCaseType).isEqualTo(Constants.SAFETY_CASE_TYPE);
+        assertThat(safetyArtifact.logicType).isNull();
+
+        // VP - Verify FTA artifact
+        String logicArtifactName = Constants.ARTIFACT_NAMES.get(DocumentType.FTA);
+        ArtifactAppEntity logicArtifact = name2artifact.get(logicArtifactName);
+        assertThat(logicArtifact.type).isEqualTo(Constants.FTA_TYPE.name());
+        assertThat(logicArtifact.documentType).isEqualTo(DocumentType.FTA);
+        assertThat(logicArtifact.safetyCaseType).isNull();
+        assertThat(logicArtifact.logicType).isEqualTo(Constants.FTA_TYPE);
+    }
+
+    protected static class Constants {
+        static final int N_ARTIFACTS = 3;
+        static final String ARTIFACT_TYPE = "requirements";
+        static final SafetyCaseType SAFETY_CASE_TYPE = SafetyCaseType.CONTEXT;
+        static final FTAType FTA_TYPE = FTAType.AND;
+        static final Map<DocumentType, String> ARTIFACT_NAMES = new HashMap<DocumentType, String>() {{
+            put(DocumentType.ARTIFACT_TREE, "R0");
+            put(DocumentType.SAFETY_CASE, "C0");
+            put(DocumentType.FTA, "A0");
+        }};
+    }
+}

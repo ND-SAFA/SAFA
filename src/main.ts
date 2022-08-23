9a7913330b7aeae66e7851422a7879bec20d5a4a
Tim <tjnewman111@gmail.com>
2022-08-22 19:10:00 -0700

    removed old module code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import "@/plugins/vue-cytoscape";
 import "@mdi/font/css/materialdesignicons.css";
 
 import App from "@/App.vue";
-import store from "@/store";
 import { router } from "@/router";
 import { vuetify, pinia } from "@/plugins";
 
@@ -12,7 +11,6 @@ Vue.config.productionTip = false;
 
 export default new Vue({
   router,
-  store,
   vuetify,
   pinia,
   render: (h) => h(App),

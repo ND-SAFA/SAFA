a9d7963d5abfb11a613d24ea27d6639810074486
Tim <tjnewman111@gmail.com>
2022-08-23 13:00:56 -0700

    improved artifact input: hide many artifacts, added checkboxes

--- src/components/common/input/ArtifactInput.vue
+++ src/components/common/input/ArtifactInput.vue
@@ -13,8 +13,22 @@
     :filter="filterArtifacts"
     @keydown.enter="$emit('enter')"
   >
-    <template v-slot:item="{ item }">
-      <generic-artifact-body-display display-title :artifact="item" />
+    <template v-slot:item="{ item, on, attrs }">
+      <v-list-item v-on="on" v-bind="attrs" dense>
+        <v-checkbox :value="isSelected(item)" />
+        <generic-artifact-body-display display-title :artifact="item" />
+      </v-list-item>
+    </template>
+    <template v-slot:selection="{ item, index }">
+      <v-chip v-if="index < 3" outlined close @click:close="handleDelete(item)">
+        <typography :value="item.name" />
+      </v-chip>
+      <typography
+        v-else-if="index === 3"
+        secondary
+        x="2"
+        :value="'+' + (selectedCount - 3)"
+      />
     </template>
   </v-autocomplete>
 </template>
@@ -23,8 +37,9 @@
 import Vue, { PropType } from "vue";
 import { ArtifactModel } from "@/types";
 import { artifactStore } from "@/hooks";
-import { GenericArtifactBodyDisplay } from "@/components/common/generic";
 import { filterArtifacts } from "@/util";
+import { GenericArtifactBodyDisplay } from "@/components/common/generic";
+import { Typography } from "@/components/common/display";
 
 /**
  * An input for artifacts.
@@ -35,6 +50,7 @@ import { filterArtifacts } from "@/util";
 export default Vue.extend({
   name: "ArtifactInput",
   components: {
+    Typography,
     GenericArtifactBodyDisplay,
   },
   props: {
@@ -66,6 +82,33 @@ export default Vue.extend({
   },
   methods: {
     filterArtifacts,
+    /**
+     * Determines whether an artifact is in the selected state.
+     *
+     * @param item - The artifact to check.
+     * @return Whether it is selected.
+     */
+    isSelected(item: ArtifactModel): boolean {
+      if (typeof this.model === "string") {
+        return item.id === this.model;
+      } else if (Array.isArray(this.model)) {
+        return this.model.includes(item.id);
+      } else {
+        return false;
+      }
+    },
+    /**
+     * Removes an artifact from the selection.
+     *
+     * @param item - The artifact to remove.
+     */
+    handleDelete(item: ArtifactModel): void {
+      if (typeof this.model === "string") {
+        this.model = "";
+      } else if (Array.isArray(this.model)) {
+        this.model = this.model.filter((id) => id !== item.id);
+      }
+    },
   },
   computed: {
     /**
@@ -75,6 +118,18 @@ export default Vue.extend({
       return this.onlyDocumentArtifacts
         ? artifactStore.currentArtifacts
         : artifactStore.allArtifacts;
+    },
+    /**
+     * @return The number of selected artifacts.
+     */
+    selectedCount(): number {
+      if (typeof this.model === "string") {
+        return 1;
+      } else if (Array.isArray(this.model)) {
+        return this.model.length;
+      } else {
+        return 0;
+      }
     },
   },
   watch: {

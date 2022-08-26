6c72860512ff39fab039cc7f1df2d2e7cb397017
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-25 20:25:24 -0400

    FEATURE: Sending job object via notification instead of triggering callback

--- src/main/java/edu/nd/crc/safa/features/jobs/entities/app/JobAppEntity.java
+++ src/main/java/edu/nd/crc/safa/features/jobs/entities/app/JobAppEntity.java
@@ -4,8 +4,10 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import edu.nd.crc.safa.features.jobs.entities.db.JobDbEntity;
+import edu.nd.crc.safa.features.notifications.services.NotificationService;
 import edu.nd.crc.safa.features.projects.entities.app.SafaError;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -32,12 +34,16 @@ public class JobAppEntity extends JobDbEntity {
                 f.setAccessible(true);
                 f.set(jobAppEntity, f.get(jobDbEntity));
             }
-            //  jobAppEntity.setCurrentStep(jobAppEntity.getCurrentStep() + 1); // 1 Index steps
             jobAppEntity.steps = JobSteps.getJobSteps(jobAppEntity.jobType);
             return jobAppEntity;
         } catch (IllegalAccessException e) {
             String errorMessage = "Illegally accessed field while creating job app entity. %s";
             throw new SafaError(errorMessage, e.getMessage());
         }
+    }
+
+    @JsonIgnore
+    public String getTopic() {
+        return NotificationService.getTopic(this.getId());
     }
 }

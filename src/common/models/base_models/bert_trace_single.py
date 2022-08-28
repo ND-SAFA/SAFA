55c21e68af2483d9177a59b80346a335cfa9cd91
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-28 16:34:34 -0400

    FIX: Updating BertTraceSingle to reflect RobertaForSequenceClassification architecture

--- src/common/models/base_models/bert_trace_single.py
+++ src/common/models/base_models/bert_trace_single.py
@@ -1,24 +1,7 @@
 from transformers import RobertaForSequenceClassification
 
 
-
-
-class BertTraceSingle(BertPreTrainedModel):
+class BertTraceSingle(RobertaForSequenceClassification):
     def __init__(self, config):
         super().__init__(config)
-        self.bert = AutoModel.from_config(config)
-        self.cls = ElectraClassificationHead(config)
-        self.init_weights()
-
-    def forward(self, input_ids, attention_mask, token_type_ids, labels=None, **kwargs):
-        return single_model_forward_pass(
-            model=self.bert,
-            cls_header=self.cls,
-            num_labels=self.config.num_labels,
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            labels=labels,
-            kwargs=kwargs,
-        )
-
+   

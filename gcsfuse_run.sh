28e48c80291d24f3499bf17a751b646a5b7afb4d
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-30 18:08:01 -0400

    FEATURE: Deploying to app engine

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -6,7 +6,7 @@ mkdir -p "$MNT_DIR"
 
 echo "Mounting GCS Fuse."
 # gcsfuse --foreground --debug_fuse --debug_fs --debug_gcs --debug_http "$BUCKET" "$MNT_DIR"
-gcsfuse "$BUCKET" "$MNT_DIR"
+gcsfuse --debug_fuse --debug_fs --debug_gcs --debug_http "$BUCKET" "$MNT_DIR"
 echo "Mounting completed."
 
 exec /app/venv/bin/python3 /app/src/manage.py runserver 0.0.0.0:80

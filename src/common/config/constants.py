8b4993bf50e1edf502009c75df1efef4f3c6df63
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-30 00:38:41 -0400

    FEATURE: Adding environment variable for cleaning test data

--- src/common/config/constants.py
+++ src/common/config/constants.py
@@ -6,3 +6,4 @@ LOSS = "loss"
 
 # -------------- MODELS --------------
 IS_TEST = os.getenv("DEPLOYMENT", "development") == "test"
+DELETE_TEST_OUTPUT = os.getenv("DELETE_TEST_OUTPUT", "true") == "true"

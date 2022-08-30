6b6963f5503771a0450f06da2535c7e68a616d9a
thearod5 <vhsalbertorodriguez@gmail.com>
2022-08-30 01:33:16 -0400

    REFACTOR: Adding hello world route to test connectivity

--- src/server/urls.py
+++ src/server/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
 from server import views
 
+
+def homePageView(request):
+    return HttpResponse("Hello, World!")
+
+
 urlpatterns = [
+    path('', homePageView),
     path('predict/', views.predict),
     path('fine-tune/', views.fine_tune),
 ]

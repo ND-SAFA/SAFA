d8755d16f4ff51eb6b2eddb9b9fb4cc1e6affa32
Adrian Rodriguez <adrianrodriguez1@MacBook-Pro-3.local>
2022-09-01 13:53:02 -0700

    New branch creation and new files for project setting

---
+++ cypress/e2e/projectSettings/projectMembersCRUD.cy.ts
@@ -0,0 +1,12 @@
+describe("Project Members CRUD", () => {
+  describe("As an owner, I can add a new member to a project", () => {
+    it("Cant add an invalid member", () => {});
+    it("Adds a new member to a project", () => {});
+  });
+  describe("As an owner, I can edit a project members permissions", () => {
+    it("Edits the permissions of a project member", () => {});
+  });
+  describe("As an owner, I can remove a member from a project", () => {
+    it("Removes a project member", () => {});
+  });
+});
---
+++ cypress/e2e/projectSettings/projectMembersDisplay.cy.ts
@@ -0,0 +1,8 @@
+describe("Porject members display", () => {
+  describe("I can search through a projects members", () => {
+    it("searches for a specific project member", () => {});
+  });
+  describe("I can see a project’s members", () => {
+    it("Displays all members of the project", () => {});
+  });
+});

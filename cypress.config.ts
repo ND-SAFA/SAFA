d17b779b7f9c519eaaec3d54e99d5f855ee76967
Tim <tjnewman111@gmail.com>
2022-10-11 13:23:03 -0700

    maybe fix tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "cypress";
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 export default defineConfig({
   projectId: "5kk96a",
   videoUploadOnPasses: false,
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: "https://localhost.safa.ai:8080",
   },

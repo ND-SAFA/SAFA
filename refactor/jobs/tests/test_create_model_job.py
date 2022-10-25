760637cab36cd004e8f1371195906c2c76520366
kat-dearstyne <katherine@jklfamily.com>
2022-10-25 18:57:57 -0400

    add test create model job

--- refactor/jobs/tests/test_create_model_job.py
+++ refactor/jobs/tests/test_create_model_job.py
@@ -1,7 +1,50 @@
-class BaseJob:
-    pass
+from unittest import mock
+from unittest.mock import patch
+
+from jobs.job_status import Status
+
+from api.responses.base_response import BaseResponse
+from jobs.create_model_job import CreateModelJob
+from test.base_test import BaseTest
+from tracer.models.model_generator import ModelGenerator
 
 
-class TestCreateModelJob(BaseJob):
-    # TODO
-    pass
+class TestCreateModelJob(BaseTest):
+    TEST_PARAMS = BaseTest.get_test_params(include_artifacts=False, include_settings=False)
+
+    @patch.object(ModelGenerator, '_ModelGenerator__load_model')
+    @patch.object(ModelGenerator, 'get_tokenizer')
+    def test_run_full(self, get_tokenizer_mock: mock.MagicMock, load_model_mock: mock.MagicMock):
+        load_model_mock.return_value = self.get_test_model()
+        get_tokenizer_mock.return_value = self.get_test_tokenizer()
+        test_model_job = self.get_test_model_job()
+        test_model_job.run()
+        self.output_test_success(test_model_job.result)
+
+    @patch.object(ModelGenerator, "get_model")
+    @patch.object(ModelGenerator, "get_tokenizer")
+    def test_run_failure(self, get_tokenizer_mock: mock.MagicMock, get_model_mock: mock.MagicMock):
+        get_tokenizer_mock.return_value = self.get_test_tokenizer()
+        get_model_mock.return_value = ValueError()
+        test_model_job = self.get_test_model_job()
+        test_model_job.run()
+        self.output_test_failure(test_model_job.result)
+
+    @patch.object(ModelGenerator, "get_tokenizer")
+    @patch.object(ModelGenerator, "get_model")
+    def get_test_model_job(self, get_model_mock: mock.MagicMock, get_tokenizer_mock: mock.MagicMock):
+        get_model_mock.return_value = self.get_test_model()
+        get_tokenizer_mock.return_value = self.get_test_tokenizer()
+        return CreateModelJob(**self.TEST_PARAMS)
+
+    def output_test_success(self, output_dict: dict):
+        self.assertIn(BaseResponse.MODEL_PATH, output_dict)
+        self.assertEqual(output_dict[BaseResponse.MODEL_PATH], self.TEST_PARAMS["output_dir"])
+        self.assertIn(BaseResponse.STATUS, output_dict)
+        self.assertEquals(output_dict[BaseResponse.STATUS], Status.SUCCESS)
+
+    def output_test_failure(self, output_dict: dict):
+        self.assertIn(BaseResponse.EXCEPTION, output_dict)
+        self.assertIn(BaseResponse.STATUS, output_dict)
+        self.assertEquals(output_dict[BaseResponse.STATUS], Status.FAILURE)
+

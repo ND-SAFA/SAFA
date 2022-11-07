61b00da53a2da9b5896102c9c53fa3dfa5c0b9fc
thearod5 <vhsalbertorodriguez@gmail.com>
2022-11-07 08:25:51 -0500

    REFACTOR: Adding section headers + reformatting

--- refactor/tracer/train/trace_args.py
+++ refactor/tracer/train/trace_args.py
@@ -9,19 +9,24 @@ from config.constants import EVALUATION_STRATEGY_DEFAULT, EVAL_DATASET_SIZE_DEFA
 
 
 class TraceArgs(TrainingArguments):
+    # Tokenizer
     pad_to_max_length: bool = PAD_TO_MAX_LENGTH_DEFAULT
     resample_rate: int = RESAMPLE_RATE_DEFAULT
     max_seq_length: int = MAX_SEQ_LENGTH_DEFAULT
-    eval_dataset_size: int = EVAL_DATASET_SIZE_DEFAULT
-    validation_percentage: float = VALIDATION_PERCENTAGE_DEFAULT
+    # Training
     num_train_epochs: int = N_EPOCHS_DEFAULT
+    validation_percentage: float = VALIDATION_PERCENTAGE_DEFAULT
+    # Evaluation
+    eval_dataset_size: int = EVAL_DATASET_SIZE_DEFAULT
     evaluation_strategy: str = EVALUATION_STRATEGY_DEFAULT
     save_strategy: str = SAVE_STRATEGY_DEFAULT
     save_total_limit: int = SAVE_TOTAL_LIMIT_DEFAULT
     load_best_model_at_end: bool = LOAD_BEST_MODEL_AT_END_DEFAULT
     metric_for_best_model_default: str = METRIC_FOR_BEST_MODEL_DEFAULT
     metrics: List[str] = None
+    # Misc
     callbacks: List = None
+    multi_gpu = True
 
     def __init__(self, output_dir, **kwargs):
         """

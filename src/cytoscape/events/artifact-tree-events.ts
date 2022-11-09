6fec46511df7d73e70a6c293758455a32c0341f4
Tim <tjnewman111@gmail.com>
2022-11-09 12:50:04 -0500

    changed artifact click to single click

--- src/cytoscape/events/artifact-tree-events.ts
+++ src/cytoscape/events/artifact-tree-events.ts
@@ -5,8 +5,8 @@ import { disableDrawMode } from "@/cytoscape";
 import { DefaultCytoEvents } from "@/cytoscape/events/cyto-events";
 import { CytoCore, CytoEvent, CytoEventHandlers } from "@/types/cytoscape/core";
 
-const doubleClickDelayMs = 350;
-let previousTapStamp = doubleClickDelayMs;
+// const doubleClickDelayMs = 350;
+// let previousTapStamp = doubleClickDelayMs;
 
 /**
  * Handlers for mouse events on the artifact tree.
@@ -33,18 +33,25 @@ export const ArtifactTreeCytoEvents: CytoEventHandlers = {
   },
   select: {
     events: [CytoEvent.TAP],
+    // action(cy: CytoCore, event: EventObject) {
+    //   const currentTimeStamp = event.timeStamp;
+    //   const artifact = event.target.data() as ArtifactModel;
+    //   const msFromLastTap = currentTimeStamp - previousTapStamp;
+    //
+    //   if (msFromLastTap === 0 || !artifact.id) return;
+    //
+    //   if (msFromLastTap < doubleClickDelayMs) {
+    //     selectionStore.toggleSelectArtifact(artifact.id);
+    //   }
+    //
+    //   previousTapStamp = currentTimeStamp;
+    // },
     action(cy: CytoCore, event: EventObject) {
-      const currentTimeStamp = event.timeStamp;
       const artifact = event.target.data() as ArtifactModel;
-      const msFromLastTap = currentTimeStamp - previousTapStamp;
 
-      if (msFromLastTap === 0 || !artifact.id) return;
+      if (!artifact.id) return;
 
-      if (msFromLastTap < doubleClickDelayMs) {
-        selectionStore.toggleSelectArtifact(artifact.id);
-      }
-
-      previousTapStamp = currentTimeStamp;
+      selectionStore.selectArtifact(artifact.id);
     },
   },
   selectAll: {

c4342a0a7b4f54fa8ef89986cb684fa14461eb1a
thearod5 <vhsalbertorodriguez@gmail.com>
2022-11-15 15:51:48 -0500

    FEATURE: Adding pre-training serializer + test

---
+++ refactor/server/serializers/pre_training_request_serializer.py
@@ -0,0 +1,22 @@
+from rest_enumfield import EnumField
+from rest_framework import serializers
+
+from server.serializers.model_identifier_serializer import ModelIdentifierSerializer
+from tracer.pre_processing.pre_processing_option import PreProcessingOption
+
+
+class PreTrainingRequestSerializer(ModelIdentifierSerializer):
+    trainingDataDir = serializers.CharField(max_length=200,
+                                            source="training_data_dir",
+                                            help_text="Path to directory containing pre-training documents.")
+    preProcessingOptions = serializers.ListField(
+        source='pre_processing_options',
+        child=EnumField(choices=PreProcessingOption, required=False, help_text="Custom pre-processing options."))
+    preProcessingParams = serializers.DictField(required=False, source="pre_processing_params",
+                                                help_text="TODO")
+    blockSize = serializers.IntegerField(required=False, source="block_size",
+                                         help_text="The length of a pre-training block.")
+    traceArgsParams = serializers.DictField(required=False, source="trace_args_params",
+                                            help_text="Custom training arguments.")
+    mlmProbability = serializers.FloatField(required=False, source="mlm_probability",
+                                            help_text="The probability a word in a block will be masked")

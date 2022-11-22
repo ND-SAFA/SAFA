751f75ac76f80f78ca00d4ea8eeb795459907af7
thearod5 <vhsalbertorodriguez@gmail.com>
2022-11-21 19:00:51 -0500

    FEATURE: Adding push script

---
+++ src/scripts/push.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+import sys
+
+from transformers import AutoModel
+
+from tracer.models.base_models.supported_base_model import SupportedBaseModel
+from tracer.models.model_generator import ModelGenerator
+from tracer.train.trace_args import TraceArgs
+from tracer.train.trace_trainer import TraceTrainer
+
+parser = argparse.ArgumentParser(description='Runs regular bert model.')
+parser.add_argument('model')
+parser.add_argument('export')
+parser.add_argument('-commit', default="FEATURE: Pushing model from tgen.")
+parser.add_argument('-root', default='~/tgen/src')
+args = parser.parse_args()
+
+args.root = os.path.expanduser(args.root)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
+assert os.path.exists(args.root), args.root
+sys.path.append(args.root)
+
+if __name__ == "__main__":
+    local_model_generator = ModelGenerator(SupportedBaseModel.NL_BERT, args.model)
+    local_model_generator.auto_class = AutoModel
+    output_dir = os.path.join(args.root, "output", args.export)
+    local_trace_args = TraceArgs(None, None)
+    trace_trainer = TraceTrainer(local_trace_args, model_generator=local_model_generator)
+    trace_trainer.push_to_hub(args.commit)

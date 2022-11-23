71aeca4654bbb13f8fc9f1a8139e87ba7d332aa1
thearod5 <vhsalbertorodriguez@gmail.com>
2022-11-22 22:28:21 -0500

    REFACTOR: Extracting logic for wraping dataset in trainer dataset container to util

--- src/server/serializers/job_factory/prediction_request_serializer.py
+++ src/server/serializers/job_factory/prediction_request_serializer.py
@@ -6,7 +6,6 @@ from jobs.job_factory import JobFactory
 from server.serializers.dataset.dataset_creator_serializer import DatasetCreatorSerializer
 from server.serializers.job_factory.model_identifier_serializer import ModelIdentifierSerializer
 from server.serializers.serializer_utility import SerializerUtility
-from tracer.datasets.trainer_datasets_container import TrainerDatasetsContainer
 
 
 class PredictionRequestSerializer(ModelIdentifierSerializer):
@@ -16,9 +15,6 @@ class PredictionRequestSerializer(ModelIdentifierSerializer):
 
     def create(self, validated_data: Dict, dataset_param_key="eval") -> JobFactory:
         kwargs: Dict = SerializerUtility.create_children_serializers(validated_data, self.fields.fields)
-        dataset = kwargs.pop("data").create()
+        SerializerUtility.create_trainer_dataset_container(kwargs, dataset_param_key)
         kwargs.update(kwargs.pop("settings"))  # put settings in main kwargs
-        container_kwargs = {dataset_param_key: dataset}
-        trainer_datasets_container = TrainerDatasetsContainer(**container_kwargs)
-        kwargs["trainer_dataset_container"] = trainer_datasets_container
         return JobFactory(**kwargs)
--- src/server/serializers/serializer_utility.py
+++ src/server/serializers/serializer_utility.py
@@ -3,8 +3,26 @@ from typing import Dict, OrderedDict
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from tracer.datasets.trainer_datasets_container import TrainerDatasetsContainer
+
 
 class SerializerUtility:
+    @staticmethod
+    def create_trainer_dataset_container(kwargs: Dict, container_param: str, dataset_param: str = "data",
+                                         export_param: str = "trainer_dataset_container"):
+        """
+        Reads dataset from kwargs and wraps it in a trainer dataset container.
+        :param kwargs: The kwargs to extract dataset from.
+        :param container_param: The name of parameter in the dataset container to store dataset under.
+        :param dataset_param: The name of the parameter in kwargs containing dataset.
+        :param export_param: The name of the parameter to export trainer dataset container to in kwargs.
+        :return: None
+        """
+        dataset = kwargs.pop(dataset_param).create()
+        container_kwargs = {container_param: dataset}
+        trainer_datasets_container = TrainerDatasetsContainer(**container_kwargs)
+        kwargs[export_param] = trainer_datasets_container
+
     @staticmethod
     def update_error():
         raise NotImplementedError("Update has not implemented for serializers. Please create new serialier.")

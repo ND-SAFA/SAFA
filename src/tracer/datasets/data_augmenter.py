ff7bef6d0283f6ed0b2849e9ec3265222f856e76
kat-dearstyne <katherine@jklfamily.com>
2022-11-22 21:14:33 -0500

    add tests for data augmenter

--- src/tracer/datasets/data_augmenter.py
+++ src/tracer/datasets/data_augmenter.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag, word_tokenize
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
+import math
 
 Synset = nltk.corpus.reader.wordnet.Synset
 
@@ -46,31 +47,71 @@ class DataAugmenter:
         augmented_data = copy(data_entries)
         while n_sample > 0:
             for entry in random.sample(data_entries, k=n_sample):
-                augmented_data.append(self._generate_new_content(entry))
+                augmented_data.append(self._generate_new_content(entry, self.replacement_rate))
             n_sample = self._get_number_to_sample(n_orig, len(augmented_data), n_expected)
         return augmented_data
 
     @staticmethod
-    def _get_number_to_sample(n_orig: int, n_total: int, n_expected: int):
-        n_sample = n_total - n_expected
-        return n_sample if n_sample <= n_total else n_orig
-
-    def _generate_new_content(self, orig_content: str) -> str:
+    def _generate_new_content(orig_content: str, replacement_rate: float) -> str:
         """
         Generates new content by replacing words in the original content
+        :param orig_content: the original content
+        :param replacement_rate: the rate at which to replace words
         :return: the new content
         """
-        word_reps = self._to_word_representations(orig_content)
-        n_replacements = int(len(word_reps) * self.replacement_rate)
-        indices2replace = set(random.sample([i for i in range(len(word_reps)) if self._should_replace(word_reps[i])],
-                                            k=n_replacements))
+        word_reps = DataAugmenter._to_word_representations(orig_content)
+        indices2sample = [i for i in range(len(word_reps)) if DataAugmenter._should_replace(word_reps[i])]
+        n_replacements = min(math.ceil(len(word_reps) * replacement_rate), len(indices2sample))
+        indices2replace = set(random.sample(indices2sample, k=n_replacements))
         new_content = []
         for i, wr in enumerate(word_reps):
             word = wr.replacements.pop() if i in indices2replace else wr.word
             new_content.append(word)
-            if wr.is_end_of_sentence:
-                new_content.append("\n")
         return " ".join(new_content)
+
+    @staticmethod
+    def _get_number_to_sample(n_orig: int, n_total: int, n_expected: int) -> int:
+        """
+        Gets the number of data entries to select for word replacements
+        :param n_orig: the number of orig data entries
+        :param n_total: the current total of orig data entries
+        :param n_expected: the number of desired data entries
+        :return: the number of data entries to select
+        """
+        return min(n_expected - n_total, n_orig)
+
+    @staticmethod
+    def _get_synonyms(orig_word: str, pos: str) -> Set[str]:
+        """
+        Gets all possible synonyms for a word
+        :param orig_word: the original word
+        :param pos: the part of speech
+        :return: a set of synonyms
+        """
+        synsets = wn.synsets(DataAugmenter.lemmatizer.lemmatize(orig_word), pos=pos) if pos else []
+        return {name for syn in synsets for name in syn.lemma_names() if name.lower() != orig_word.lower()}
+
+    @staticmethod
+    def _to_word_representations(orig_content: str) -> List[WordRepresentation]:
+        """
+        Converts all words in the content into word representations
+        :param orig_content: the original content
+        :return: the content as a list of word representations
+        """
+        word_representations = []
+        for sentence in orig_content.splitlines():
+            sentence_word_reps = []
+            word_tag_pairs = pos_tag(word_tokenize(sentence))
+            for word, tag in word_tag_pairs:
+                pos = DataAugmenter._get_word_pos(tag)
+                replacements = DataAugmenter._get_synonyms(word, pos)
+                sentence_word_reps.append(WordRepresentation(word=word, pos=pos, replacements=replacements,
+                                                             is_stop_word=word in DataAugmenter.STOPWORDS))
+            last_word = sentence_word_reps.pop()
+            last_word.is_end_of_sentence = True
+            sentence_word_reps.append(last_word)
+            word_representations.extend(sentence_word_reps)
+        return word_representations
 
     @staticmethod
     def _get_word_pos(tag) -> Optional[str]:
@@ -90,43 +131,10 @@ class DataAugmenter:
         return None
 
     @staticmethod
-    def _get_synonyms(orig_word: str, pos: str) -> Set[str]:
-        """
-        Gets all possible synonyms for a word
-        :param orig_word: the original word
-        :param pos: the part of speech
-        :return: a set of synonyms
-        """
-        synsets = wn.synsets(DataAugmenter.lemmatizer.lemmatize(orig_word), pos=pos) if pos else []
-        return {name for syn in synsets for name in syn.lemma_names() if name != orig_word}
-
-    @staticmethod
-    def _to_word_representations(orig_content: str) -> List[WordRepresentation]:
-        """
-        Converts all words in the content into word representations
-        :param orig_content: the original content
-        :return: the content as a list of word representations
-        """
-        word_representations = []
-        for sentence in orig_content.splitlines():
-            sentence_word_reps = []
-            tokens = word_tokenize(sentence)
-            for word, tag in pos_tag(tokens):
-                pos = DataAugmenter._get_word_pos(tag)
-                replacements = DataAugmenter._get_synonyms(word, pos)
-                sentence_word_reps.append(WordRepresentation(word=word, pos=pos, replacements=replacements,
-                                                             is_stop_word=word in DataAugmenter.STOPWORDS))
-            last_word = sentence_word_reps.pop()
-            last_word.is_end_of_sentence = True
-            sentence_word_reps.append(last_word)
-            word_representations.extend(sentence_word_reps)
-        return word_representations
-
-    @staticmethod
     def _should_replace(word_rep: WordRepresentation) -> bool:
         """
         Determine if the word should be replaced
         :param word_rep: the word representation
         :return: True if the word should be replaced else False
         """
-        return word_rep.replacements and not word_rep.is_stop_word and word_rep.pos not in DataAugmenter.POS2EXCLUDE
+        return not word_rep.is_stop_word and word_rep.pos not in DataAugmenter.POS2EXCLUDE and len(word_rep.replacements) >= 1
--- src/tracer/datasets/tests/test_data_augmenter.py
+++ src/tracer/datasets/tests/test_data_augmenter.py
@@ -1,12 +1,80 @@
+import math
+
 from test.base_test import BaseTest
-from tracer.datasets.data_augmenter import DataAugmenter
+from tracer.datasets.data_augmenter import DataAugmenter, WordRepresentation
+from nltk.corpus import wordnet as wn
 
 
 class TestDataAugmenter(BaseTest):
 
     def test_run(self):
-        # TODO
-        pass
+        da = self.get_data_augmenter()
+        data_entries = ["The city planning is missing in this depressing city", "the cars always break",
+                        "I hate South Bend alot", "South Bend is frigid"]
+        result = da.run(data_entries, 7)
+        self.assertEquals(len(result), 7)
+
+    def test_get_number_to_sample(self):
+        self.assertEquals(2, DataAugmenter._get_number_to_sample(5, 10, 12))
+        self.assertEquals(0, DataAugmenter._get_number_to_sample(5, 10, 10))
+        self.assertEquals(5, DataAugmenter._get_number_to_sample(5, 10, 17))
+
+    def test_generate_new_content(self):
+        orig_content = "The city planning is missing in this depressing city and the cars always break"
+        indices2sample = [4, 7, 12, 13]
+
+        new_content = DataAugmenter._generate_new_content(orig_content, 0.15)
+        self.word_replacement_test(orig_content, new_content, indices2sample, 0.15)
+
+        new_content = DataAugmenter._generate_new_content(orig_content, 0.3)
+        self.word_replacement_test(orig_content, new_content, indices2sample, 0.3)
+
+    def test_get_synonyms(self):
+        synonyms = {syn.lower() for syn in DataAugmenter._get_synonyms("south", "n")}
+        self.assertTrue("south" not in synonyms)
+        self.assertTrue("southward" in synonyms)
+
+    def test_get_word_pos(self):
+        noun = DataAugmenter._get_word_pos("NNP")
+        self.assertEquals(wn.NOUN, noun)
+        verb = DataAugmenter._get_word_pos("VBZ")
+        self.assertEquals(wn.VERB, verb)
+        adj = DataAugmenter._get_word_pos("JJ")
+        self.assertEquals(wn.ADJ, adj)
+        adv = DataAugmenter._get_word_pos("RB")
+        self.assertEquals(wn.ADV, adv)
+        unk = DataAugmenter._get_word_pos("PDT")
+        self.assertEquals(None, unk)
+
+    def test_to_word_representation(self):
+        orig_content = "South Bend is frigid"
+        orig_words = orig_content.split()
+        word_reps = DataAugmenter._to_word_representations(orig_content)
+        self.assertEquals(len(orig_words), len(word_reps))
+        for i, word in enumerate(orig_words):
+            self.assertEquals(word, word_reps[i].word)
+            self.assertEquals(word == "is", word_reps[i].is_stop_word)
+            self.assertEquals(i == len(word_reps) - 1, word_reps[i].is_end_of_sentence)
+            self.assertGreater(len(word_reps[i].replacements), 0)
+
+    def test_should_replace(self):
+        wr1 = WordRepresentation(word="I", is_stop_word=True, pos='s', replacements={"myself"})
+        wr2 = WordRepresentation(word="hate", is_stop_word=False, pos='v', replacements={"loathe"})
+        wr3 = WordRepresentation(word="South Bend", is_stop_word=False, pos='n', replacements={"horrorville"})
+        wr4 = WordRepresentation(word="alot", is_stop_word=False, pos='r', replacements=set())
+        word_representations = [wr1, wr2, wr3, wr4]
+        expected_return_val = [False, True, False, False]
+        for i, wr in enumerate(word_representations):
+            self.assertEquals(DataAugmenter._should_replace(wr), expected_return_val[i])
 
     def get_data_augmenter(self):
         return DataAugmenter(0.15)
+
+    def word_replacement_test(self, orig_content, new_content, indices2sample, replacement_rate):
+        orig_words = orig_content.split()
+        n_replacements = 0
+        for i, word in enumerate(new_content.split()):
+            if word != orig_words[i]:
+                self.assertIn(i, indices2sample)
+                n_replacements += 1
+        self.assertEquals(n_replacements, min(math.ceil(len(orig_words) * replacement_rate), len(indices2sample)))

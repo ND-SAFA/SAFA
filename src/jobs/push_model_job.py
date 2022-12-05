99c6de53ff8ed1a1ffee9a09186779ebc3943007
thearod5 <vhsalbertorodriguez@gmail.com>
2022-12-05 15:05:38 -0500

    FIX: Adding pushing tokenizer to push script

--- src/jobs/push_model_job.py
+++ src/jobs/push_model_job.py
@@ -12,4 +12,6 @@ class PushModelJob(AbstractJob):
         model_generator = self.get_model_generator()
         model = model_generator.get_model()
         model.push_to_hub(self.output_dir, use_temp_dir=True)
+        tokenizer = model_generator.get_tokenizer()
+        tokenizer.push_to_hub(self.output_dir)
         return JobResult.from_dict({JobResult.MODEL_PATH: self.output_dir})

f510ec22fff5f941f64caa138b651d4b4bf0b6b6
thearod5 <vhsalbertorodriguez@gmail.com>
2022-12-05 17:15:32 -0500

    HOTFIX: NLBERT for sequence classification

--- src/common/models/base_models/supported_base_model.py
+++ src/common/models/base_models/supported_base_model.py
@@ -1,13 +1,14 @@
 from enum import Enum
 
-from common.models.base_models.nl_bert import NLBert
+from transformers import AutoModelForSequenceClassification
+
 from common.models.base_models.bert_trace_siamese import BertTraceSiamese
-from common.models.base_models.pl_bert import PLBert
 from common.models.base_models.electra_trace_single import ElectraTraceSingle
+from common.models.base_models.pl_bert import PLBert
 
 
 class SupportedBaseModel(Enum):
     PL_BERT = PLBert
-    NL_BERT = NLBert
+    NL_BERT = AutoModelForSequenceClassification
     BERT_TRACE_SIAMESE = BertTraceSiamese
     ELECTRA_TRACE_SINGLE = ElectraTraceSingle

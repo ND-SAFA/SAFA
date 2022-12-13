93e69f9ad4fb85e2ec81bc649df85a7007d6fb7c
thearod5 <vhsalbertorodriguez@gmail.com>
2022-12-13 16:10:26 -0500

    FEATURE: Experiment variable serializer

--- src/experiments/variables/multi_variable.py
+++ src/experiments/variables/multi_variable.py
@@ -1,9 +1,10 @@
-from typing import List, Any
+from typing import Any, List
 
 from experiments.variables.variable import Variable
 
 
 class MultiVariable(Variable, list):
+    SYMBOL = "*"
 
     def __init__(self, values: List[Any]):
         Variable.__init__(self, values)

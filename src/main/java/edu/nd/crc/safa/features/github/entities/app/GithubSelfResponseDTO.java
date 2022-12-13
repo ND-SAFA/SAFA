ea4999b8da67ed63c02c0c9673a818709e29aea5
Evan Allan <evanallan1026@gmail.com>
2022-12-13 17:45:16 -0600

    Fix deserialization issue keeping valid github auth code responses from being parsed

--- src/main/java/edu/nd/crc/safa/features/github/entities/app/GithubSelfResponseDTO.java
+++ src/main/java/edu/nd/crc/safa/features/github/entities/app/GithubSelfResponseDTO.java
@@ -3,12 +3,14 @@ package edu.nd.crc.safa.features.github.entities.app;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Transfer object wrapping data we need from the /self endpoint
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GithubSelfResponseDTO {
 

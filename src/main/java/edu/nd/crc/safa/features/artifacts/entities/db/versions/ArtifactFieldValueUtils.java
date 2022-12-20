7c62de191ff97d59217adcb6151efe66f73021e9
Evan Allan <evanallan1026@gmail.com>
2022-12-20 16:20:55 -0600

    Get existing field version if it exists

--- src/main/java/edu/nd/crc/safa/features/artifacts/entities/db/versions/ArtifactFieldValueUtils.java
+++ src/main/java/edu/nd/crc/safa/features/artifacts/entities/db/versions/ArtifactFieldValueUtils.java
@@ -1,7 +1,10 @@
 package edu.nd.crc.safa.features.artifacts.entities.db.versions;
 
+import java.util.Optional;
+
 import edu.nd.crc.safa.features.artifacts.entities.db.schema.ArtifactFieldStorageType;
 import edu.nd.crc.safa.features.artifacts.entities.db.schema.ArtifactSchemaField;
+import edu.nd.crc.safa.features.artifacts.repositories.versions.ArtifactFieldVersionRepository;
 import edu.nd.crc.safa.features.common.ServiceProvider;
 import edu.nd.crc.safa.features.projects.entities.app.SafaError;
 
@@ -25,10 +28,7 @@ public class ArtifactFieldValueUtils {
     public static void saveArtifactValue(ServiceProvider serviceProvider, ArtifactSchemaField field,
                                          ArtifactVersion artifactVersion, String value) {
 
-        ArtifactFieldVersion fieldVersion = new ArtifactFieldVersion();
-        fieldVersion.setSchemaField(field);
-        fieldVersion.setArtifactVersion(artifactVersion);
-        serviceProvider.getArtifactFieldVersionRepository().save(fieldVersion);
+        ArtifactFieldVersion fieldVersion = getFieldVersion(serviceProvider, field, artifactVersion);
 
         ArtifactFieldStorageType storageType = field.getType().getStorageType();
         if (storageType.isArrayType()) {
@@ -38,6 +38,32 @@ public class ArtifactFieldValueUtils {
         } else {
             saveArtifactValueInner(serviceProvider, storageType, value, fieldVersion);
         }
+    }
+
+    /**
+     * Gets the current field version matching the given field and artifact version, if it exists. Else
+     * returns a newly constructed one.
+     *
+     * @param serviceProvider Service provider - provides access to repositories.
+     * @param field The field object we want to access.
+     * @param artifactVersion The artifact version we want to access a field for.
+     * @return The field version if it existed previously, or a new one.
+     */
+    private static ArtifactFieldVersion getFieldVersion(ServiceProvider serviceProvider, ArtifactSchemaField field,
+                                                        ArtifactVersion artifactVersion) {
+
+        ArtifactFieldVersionRepository repo = serviceProvider.getArtifactFieldVersionRepository();
+
+        Optional<ArtifactFieldVersion> foundFieldVersion =
+            repo.findByArtifactVersionAndSchemaField(artifactVersion, field);
+
+        return foundFieldVersion.orElseGet(() -> {
+            ArtifactFieldVersion fieldVersion = new ArtifactFieldVersion();
+            fieldVersion.setArtifactVersion(artifactVersion);
+            fieldVersion.setSchemaField(field);
+            repo.save(fieldVersion);
+            return fieldVersion;
+        });
     }
 
     /**
@@ -67,7 +93,9 @@ public class ArtifactFieldValueUtils {
      * @param value The value to set.
      * @param fieldVersion The artifact field version we set a value in.
      */
-    private static void saveArtifactValueInner(ServiceProvider serviceProvider, ArtifactFieldStorageType storageType, String value, ArtifactFieldVersion fieldVersion) {
+    private static void saveArtifactValueInner(ServiceProvider serviceProvider, ArtifactFieldStorageType storageType,
+                                               String value, ArtifactFieldVersion fieldVersion) {
+
         IFieldValue fieldValueObj = storageType.getFieldValueSupplier().get();
         fieldValueObj.setFieldVersion(fieldVersion);
         fieldValueObj.setValueFromString(value);

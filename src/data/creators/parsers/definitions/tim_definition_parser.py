35c832263c08169fb3190d631fd7645e29ba5f54
thearod5 <vhsalbertorodriguez@gmail.com>
2022-12-22 17:43:46 -0800

    FIX: Adding format specific column conversions

--- src/data/creators/parsers/definitions/tim_definition_parser.py
+++ src/data/creators/parsers/definitions/tim_definition_parser.py
@@ -9,12 +9,19 @@ from util.json_util import JSONUtil
 class TimDefinitionParser(IDefinitionParser):
     def __init__(self, project_path: str):
         super().__init__(project_path, conversions={
-            "traces": {
+            "json-traces": {
                 "sourceName": "source",
                 "targetName": "target"
-            }, "artifacts": {
+            },
+            "csv-traces": {
+                "source": "source",
+                "target": "target"
+            },
+            "json-artifacts": {
                 "name": "id",
-                "body": "content",
+                "body": "content"
+            },
+            "csv-artifacts": {
                 "id": "id",
                 "content": "content"
             }
@@ -27,17 +34,30 @@ class TimDefinitionParser(IDefinitionParser):
         JSONUtil.require_properties(self.definition, [SafaFormat.DATAFILES_KEY])
         artifact_definitions = self.definition[SafaFormat.DATAFILES_KEY]
         for artifact_name, artifact_definition in artifact_definitions.items():
-            artifact_definition[StructureKeys.COLS] = "artifacts"
+            col_conversion_name = self.get_file_format(artifact_definition[StructureKeys.FILE])
+            artifact_definition[StructureKeys.COLS] = "-".join([col_conversion_name, "artifacts"])
         return artifact_definitions
 
     def get_trace_definitions(self) -> Dict[str, Dict]:
         self.definition.pop(SafaFormat.DATAFILES_KEY)
+
+        def get_conversion_name(trace_definition):
+            return "-".join([self.get_file_format(trace_definition[StructureKeys.FILE]), "traces"])
+
         return {
             key: {
                 "file": definition["File"],
                 "source": definition["Source"],
                 "target": definition["Target"],
-                "cols": "traces"
+                "cols": get_conversion_name(definition)
             }
             for key, definition in self.definition.items()
         }
+
+    def get_file_format(self, file_path: str):
+        supported_formats = ["csv", "json"]
+        for format_name in supported_formats:
+            format_id = "." + format_name
+            if format_id in file_path:
+                return format_name
+        raise Exception(file_path, "did not have a supported format:", supported_formats)

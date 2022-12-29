68e5b18631e84cfc6befea582b569e68f3cf26b5
thearod5 <vhsalbertorodriguez@gmail.com>
2022-12-29 13:36:15 -0800

    FIX: Missing import after fixes

--- src/util/object_creator.py
+++ src/util/object_creator.py
@@ -10,7 +10,7 @@ from experiments.experiment_step import ExperimentStep
 from jobs.components.job_args import JobArgs
 from jobs.supported_job_type import SupportedJobType
 from models.model_manager import ModelManager
-from testres.paths.paths import TEST_OUTPUT_DIR
+from testres.paths.paths import PRETRAIN_DIR, TEST_OUTPUT_DIR
 from testres.test_data_manager import TestDataManager
 from train.trainer_args import TrainerArgs
 from util.definition_creator import DefinitionCreator

c8e2d23171c1f14c19d12eea83a30d01e72a4ed0
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-02 13:10:56 -0800

    FIX: Setting experimental vars whenever list of jobs detected

--- src/variables/experimental_variable.py
+++ src/variables/experimental_variable.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, List
 
+from jobs.abstract_job import AbstractJob
+from jobs.components.job_result import JobResult
 from variables.multi_variable import MultiVariable
 from variables.variable import Variable
 
@@ -13,5 +15,11 @@ class ExperimentalVariable(MultiVariable):
         """
         if len(values) > 0 and not isinstance(values[0], Variable):
             values = MultiVariable.from_list(values).value
-        self.experimental_param_names_to_vals = experimental_param_name_to_val
+        if len(values) > 0 and isinstance(values[0], AbstractJob):
+            self.experimental_param_names_to_vals = self.__extract_experimental_params(values)
+        else:
+            self.experimental_param_names_to_vals = experimental_param_name_to_val
         super().__init__(values)
+
+    def __extract_experimental_params(self, values: List[Any]):
+        return list(map(lambda j: j.result[JobResult.EXPERIMENTAL_VARS], values))

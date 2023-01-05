dc387841883a47caffbaaf6dea7bedbc5d8b334d
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-04 19:03:10 -0800

    REFACTOR: Removing empty test

--- src/server/serializers/tests/test_experiment_serailizer.py
+++ src/server/serializers/tests/test_experiment_serailizer.py
@@ -1,7 +1,5 @@
-from experiments.experiment_step import ExperimentStep
 from testres.base_test import BaseTest
 from util.definition_creator import DefinitionCreator
-from util.object_creator import ObjectCreator
 from variables.definition_variable import DefinitionVariable
 from variables.multi_variable import MultiVariable
 
@@ -19,7 +17,3 @@ class TestExperimentSerializer(BaseTest):
         self.assertIsInstance(jobs_variable, MultiVariable)
         for job in jobs_variable.value:
             self.assertIsInstance(job, DefinitionVariable)
-
-    def test_playground(self):
-        step = ObjectCreator.create(ExperimentStep)
-        print(step)

cd137b2c3f8e5e1f472129c06e2d98ac5473e8a1
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-12 10:22:57 -0500

    FIX: Typo leading to always returning second slice

--- src/data/datasets/splitting/random_split_strategy.py
+++ src/data/datasets/splitting/random_split_strategy.py
@@ -20,6 +20,6 @@ class RandomSplitStrategy(AbstractSplitStrategy):
 
         first_slice_pos_ids, second_slice_pos_ids = train_test_split(trace_dataset.pos_link_ids, test_size=percent_split)
         first_slice_neg_ids, second_slice_neg_ids = train_test_split(trace_dataset.neg_link_ids, test_size=percent_split)
-        slice_ids = first_slice_pos_ids + first_slice_neg_ids if slice == 1 else second_slice_pos_ids + second_slice_neg_ids
+        slice_ids = first_slice_pos_ids + first_slice_neg_ids if slice_num == 1 else second_slice_pos_ids + second_slice_neg_ids
         slice_links = {link_id: trace_dataset.links[link_id] for link_id in slice_ids}
         return TraceDataset(slice_links)

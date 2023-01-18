3614bb23618fc772a3d1dbc25940515cc5bc5775
Evan Allan <evanallan1026@gmail.com>
2023-01-18 00:12:08 -0600

    Don't create a new project mapping if doing an update as it will already exist

--- src/main/java/edu/nd/crc/safa/features/jobs/entities/jobs/JiraProjectUpdateJob.java
+++ src/main/java/edu/nd/crc/safa/features/jobs/entities/jobs/JiraProjectUpdateJob.java
@@ -1,11 +1,17 @@
 package edu.nd.crc.safa.features.jobs.entities.jobs;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import edu.nd.crc.safa.features.common.ProjectEntities;
 import edu.nd.crc.safa.features.common.ServiceProvider;
 import edu.nd.crc.safa.features.jira.entities.api.JiraIdentifier;
 import edu.nd.crc.safa.features.jira.entities.app.JiraIssuesResponseDTO;
+import edu.nd.crc.safa.features.jira.entities.db.JiraProject;
 import edu.nd.crc.safa.features.jira.services.JiraConnectionService;
 import edu.nd.crc.safa.features.jobs.entities.db.JobDbEntity;
+import edu.nd.crc.safa.features.projects.entities.app.SafaError;
+import edu.nd.crc.safa.features.projects.entities.db.Project;
 import edu.nd.crc.safa.features.users.entities.db.SafaUser;
 
 /**
@@ -33,5 +39,18 @@ public class JiraProjectUpdateJob extends CreateProjectViaJiraJob {
         return this.serviceProvider
             .getJiraParsingService()
             .parseProjectEntitiesFromIssues(dto.getIssues());
+    }
+
+    @Override
+    protected JiraProject getJiraProjectMapping(Project project, UUID orgId, Long jiraProjectId) {
+        Optional<JiraProject> optional = this.serviceProvider
+                .getJiraProjectRepository()
+                .findByProjectAndJiraProjectId(project, jiraProjectId);
+
+        if (optional.isEmpty()) {
+            throw new SafaError("JIRA project is not mapped to this SAFA project");
+        }
+
+        return optional.get();
     }
 }

ac7a845859e8ec0acf6c23e71ec9c349b519bde2
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-27 08:05:04 -0500

    FIX: Setting model on devices before prediction

--- src/train/trace_trainer.py
+++ src/train/trace_trainer.py
@@ -3,12 +3,14 @@ from typing import Optional, Tuple
 
 import torch
 from accelerate import Accelerator, find_executable_batch_size
+from datasets import Dataset
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import _LRScheduler
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import PreTrainedModel, Trainer
 from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers.trainer_utils import PredictionOutput
 
 from config.override import overrides
 from data.datasets.data_key import DataKey
@@ -106,6 +108,21 @@ class TraceTrainer(BaseTrainer):
             self.on_epoch(epoch_index)
         return TraceTrainOutput(global_step=global_step, training_loss=training_loss, metrics=training_metrics,
                                 eval_metrics=self.save_strategy.stage_evaluations)
+
+    def predict(self, test_dataset: Dataset, resume_from_checkpoint: str = None, **kwargs) -> PredictionOutput:
+        """
+        Moves model to accelerate device then predicts current model on dataset.
+        :param test_dataset: The test dataset to predict on.
+        :param resume_from_checkpoint: A checkpoint to load before prediction.
+        :param kwargs: Additional parameters passed to super predict function.
+        :return: The prediction output.
+        """
+        model, optimizer, scheduler, eval_data_loader = self.create_or_load_state(self.model,
+                                                                                  self.get_eval_dataloader(),
+                                                                                  resume_from_checkpoint)
+        self.model = model
+        self.eval_dataset = eval_data_loader
+        return super().predict(test_dataset, **kwargs)
 
     def create_or_load_state(self, model: PreTrainedModel, data_loader: DataLoader, resume_from_checkpoint: Optional[str] = None) \
             -> Tuple[PreTrainedModel, Optimizer, _LRScheduler, DataLoader]:

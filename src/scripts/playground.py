30fe3669edfe520ce91ace673604d0ed89feef3a
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-30 12:33:18 -0500

    testing playground script

--- src/scripts/playground.py
+++ src/scripts/playground.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from threading import Thread
 
 import dotenv
 from accelerate import Accelerator
@@ -8,11 +9,25 @@ from transformers import AutoModelForSequenceClassification
 dotenv.load_dotenv()
 sys.path.append(os.environ["ROOT_PATH"])
 
+
+class ThreadExample(Thread):
+    accelerate = None
+
+    def start(self):
+        model_1 = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
+        accelerate = self.get_accelerate()
+        model_1 = accelerate.prepare_model(model_1)
+        model_2 = AutoModelForSequenceClassification.from_pretrained("roberta-base")
+        accelerate.free_memory()
+        accelerate.prepare_model(model_2)
+        print("Done")
+
+    def get_accelerate(self):
+        if self.accelerate is None:
+            self.accelerate = Accelerator()
+        return self.accelerate
+
+
 if __name__ == "__main__":
-    model_1 = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
-    accelerate = Accelerator()
-    model_1 = accelerate.prepare_model(model_1)
-    model_2 = AutoModelForSequenceClassification.from_pretrained("roberta-base")
-    accelerate.free_memory()
-    accelerate.prepare_model(model_2)
-    print("Done")
+    thread = ThreadExample()
+    thread.run()

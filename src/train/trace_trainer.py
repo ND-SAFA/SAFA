46ea5fc6027ef0515946ae2b369331c5a44485c2
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-30 04:38:31 -0500

    DEBUG: Printing size of dataset too

--- src/train/trace_trainer.py
+++ src/train/trace_trainer.py
@@ -117,6 +117,7 @@ class TraceTrainer(BaseTrainer):
         self.accelerator = self._create_accelerator()
         test_dataloader = self.get_test_dataloader(test_dataset)
         self.model, eval_data_loader = self.accelerator.prepare(self.model, test_dataloader)
+        self.accelerator.print(f"Test dataset size: {len(test_dataloader.dataset)}")
         self.model.eval()
         eval_predictions, eval_labels = [], []
         for batch in eval_data_loader:

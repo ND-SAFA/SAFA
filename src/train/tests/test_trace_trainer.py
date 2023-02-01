655d5231d16f273dc25c5c15e2647cf92cac1cce
thearod5 <vhsalbertorodriguez@gmail.com>
2023-01-31 20:44:57 -0500

    FIX: Fixed broken test cases that now depend on default values having f2 being included

--- src/train/tests/test_trace_trainer.py
+++ src/train/tests/test_trace_trainer.py
@@ -13,6 +13,7 @@ from testres.base_trace_test import BaseTraceTest
 from testres.test_assertions import TestAssertions
 from testres.test_data_manager import TestDataManager
 from train.metrics.metrics_manager import MetricsManager
+from train.trace_output.stage_eval import Metrics
 from train.trace_output.trace_prediction_output import TracePredictionOutput
 from train.trace_trainer import TraceTrainer
 from train.trainer_args import TrainerArgs
@@ -27,28 +28,30 @@ class TestTraceTrainer(BaseTraceTest):
     SOURCE_LAYERS = TestDataManager.get_path([TestDataManager.Keys.ARTIFACTS, TestDataManager.Keys.SOURCE])
 
     EXPECTED_PREDICTION_SIZE = len(TARGET_LAYERS) * len(SOURCE_LAYERS)
-    TEST_METRIC_NAMES = ["accuracy", "map"]
+    TEST_METRIC_DEFINITION = [["accuracy", ["accuracy"]], ["map", ["map"]], ["map_at_k", ["map@1", "map@2", "map@3"]],
+                              ["f", ["f1", "f2"]]]
+    TEST_METRICS_NAMES = [m for m, aliases in TEST_METRIC_DEFINITION]
 
     @patch.object(TraceTrainer, "save_model")
     def test_perform_training(self, save_model_mock: mock.Mock):
-        test_trace_trainer = self.get_custom_trace_trainer(metrics=self.TEST_METRIC_NAMES)
+
+        test_trace_trainer = self.get_custom_trace_trainer(metrics=self.TEST_METRICS_NAMES)
         test_trace_trainer.model_manager.get_tokenizer().padding = True
         train_output = test_trace_trainer.perform_training()
         self.assertGreater(train_output.training_loss, 0)
 
     def test_perform_prediction(self):
         test_trace_trainer = self.get_custom_trace_trainer(dataset_container_args={"val_dataset_creator": None},
-                                                           metrics=self.TEST_METRIC_NAMES)
+                                                           metrics=self.TEST_METRICS_NAMES)
         trace_prediction_output = test_trace_trainer.perform_prediction()
         trace_prediction_job_result = JobResult.from_trace_output(trace_prediction_output)
         eval_dataset = test_trace_trainer.trainer_dataset_manager[DatasetRole.EVAL]
         TestAssertions.verify_prediction_output(self, trace_prediction_job_result, eval_dataset)
 
     def test_perform_prediction_with_metrics(self):
-        test_trace_trainer = self.get_custom_trace_trainer(metrics=self.TEST_METRIC_NAMES)
-        output = test_trace_trainer.perform_prediction()
-        for metric in self.TEST_METRIC_NAMES:
-            self.assertIn(metric, output.metrics)
+        test_trace_trainer = self.get_custom_trace_trainer(metrics=self.TEST_METRICS_NAMES)
+        prediction_output = test_trace_trainer.perform_prediction()
+        self.assert_metrics(prediction_output.metrics)
 
     def test_output_to_dict(self):
         trace_output = TracePredictionOutput(TestDataManager.EXAMPLE_PREDICTION_OUTPUT)
@@ -60,12 +63,11 @@ class TestTraceTrainer(BaseTraceTest):
 
     def test_eval(self):
         output = deepcopy(TestDataManager.EXAMPLE_PREDICTION_OUTPUT)
-        test_trace_trainer = self.get_custom_trace_trainer(metrics=self.TEST_METRIC_NAMES)
+        test_trace_trainer = self.get_custom_trace_trainer(metrics=self.TEST_METRICS_NAMES)
         metrics_manager = MetricsManager(test_trace_trainer.trainer_dataset_manager[DatasetRole.EVAL].links.values(),
                                          output.predictions)
-        result = metrics_manager.eval(self.TEST_METRIC_NAMES)
-        for metric in self.TEST_METRIC_NAMES:
-            self.assertIn(metric, result)
+        eval_metrics = metrics_manager.eval(self.TEST_METRICS_NAMES)
+        self.assert_metrics(eval_metrics)
 
     def set_train_dataset(self, test_trace_trainer):
         train_dataset = self.create_trainer_dataset_manager()[DatasetRole.TRAIN]
@@ -93,6 +95,15 @@ class TestTraceTrainer(BaseTraceTest):
             output_files = list(os.listdir(folder_path))
             for file in checkpoint_files:
                 self.assertIn(file, output_files)
+
+    def assert_metrics(self, metrics: Metrics):
+        """
+        Verifies that metrics contains all the desired metrics.
+        :param metrics: The metrics object being checked.
+        """
+        for metric, aliases in self.TEST_METRIC_DEFINITION:
+            for alias in aliases:
+                self.assertIn(alias, metrics)
 
     @staticmethod
     def create_trainer_dataset_manager(kwargs: Dict = None) -> TrainerDatasetManager:

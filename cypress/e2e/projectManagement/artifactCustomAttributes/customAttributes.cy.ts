638be381b147439eff70fc191b5cf7731af46473
Tim <tjnewman111@gmail.com>
2023-02-02 16:24:30 -0800

    updated custom attribute test setup

--- cypress/e2e/projectManagement/artifactCustomAttributes/customAttributes.cy.ts
+++ cypress/e2e/projectManagement/artifactCustomAttributes/customAttributes.cy.ts
@@ -1,11 +1,9 @@
-import { Routes } from "@/fixtures";
-
 describe("Custom Attributes", () => {
   before(() => {
-    cy.initEmptyProject();
+    cy.initProject();
+  });
 
-    cy.expandViewport()
-      .visit(Routes.MY_PROJECTS)
-      .locationShouldEqual(Routes.MY_PROJECTS);
+  beforeEach(() => {
+    cy.initProjectVersion();
   });
 });

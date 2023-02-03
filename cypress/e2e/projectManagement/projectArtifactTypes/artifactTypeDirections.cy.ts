d318cfcd662d629bb41f5b8e9f5eca7bfd08832d
Tim <tjnewman111@gmail.com>
2023-02-02 18:01:48 -0800

    small test fixes

--- cypress/e2e/projectManagement/projectArtifactTypes/artifactTypeDirections.cy.ts
+++ cypress/e2e/projectManagement/projectArtifactTypes/artifactTypeDirections.cy.ts
@@ -10,19 +10,20 @@ describe("Artifact Type Directions", () => {
   });
 
   describe("I cannot create trace links between types in both directions", () => {
-    it("Cannot create restricted links", () => {
-      // Selects two of the same artifact.
+    it("Cannot create links from an artifact to itself", () => {
       cy.createNewTraceLink()
         .getCy(DataCy.traceSaveSubmitButton)
         .should("be.disabled");
+    });
 
-      // Selects an existing link.
-      cy.fillTraceLinkFields("F15", "F9")
+    it("Cannot create an existing link", () => {
+      cy.createNewTraceLink("F15", "F9")
         .getCy(DataCy.traceSaveSubmitButton)
         .should("be.disabled");
+    });
 
-      // Selects an invalid link direction.
-      cy.fillTraceLinkFields("F5", "D9")
+    it("Cannot create a link in an invalid direction", () => {
+      cy.createNewTraceLink("F5", "D9")
         .getCy(DataCy.traceSaveSubmitButton)
         .should("be.disabled");
     });

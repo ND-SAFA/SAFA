bf7e01b96aea386c2abe5dc25482b2f8b8ba417d
kat-dearstyne <katherine@jklfamily.com>
2023-02-06 10:53:57 -0500

    add multi to supported creators

--- src/data/creators/supported_dataset_creator.py
+++ src/data/creators/supported_dataset_creator.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from data.creators.mlm_pre_train_dataset_creator import MLMPreTrainDatasetCreator
+from data.creators.multi_trace_dataset_creator import MultiTraceDatasetCreator
 from data.creators.split_dataset_creator import SplitDatasetCreator
 from data.creators.trace_dataset_creator import TraceDatasetCreator
 
@@ -9,3 +10,4 @@ class SupportedDatasetCreator(Enum):
     MLM_PRETRAIN = MLMPreTrainDatasetCreator
     SPLIT = SplitDatasetCreator
     TRACE = TraceDatasetCreator
+    MULTI = MultiTraceDatasetCreator

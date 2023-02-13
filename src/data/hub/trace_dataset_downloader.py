9ee7352aa2e9ace2063fb172420874a48901f039
thearod5 <vhsalbertorodriguez@gmail.com>
2023-02-13 10:12:25 -0600

    FIX: Removed zip file name and found it dynamically

--- src/data/hub/trace_dataset_downloader.py
+++ src/data/hub/trace_dataset_downloader.py
@@ -15,17 +15,14 @@ class TraceDatasetDownloader:
     Responsible for downloading and loading files for supported dataset.
     """
 
-    def __init__(self, dataset_name: str, zip_file_name: str = None, **config_kwargs):
+    def __init__(self, dataset_name: str, **config_kwargs):
         """
         Initializes adapter for dataset specified in descriptor and converts it to a trace dataset.
         :param dataset_name: The name of the dataset to download and prepare.
         :param creator_arguments: Kwargs passed to dataset creator.
         :param config_kwargs: Additional parameters to builder configuration.
         """
-        if zip_file_name is None:
-            zip_file_name = dataset_name
         self.dataset_name = dataset_name
-        self.zip_file_name = zip_file_name
         self.descriptor: AbstractDatasetDescriptor = SupportedDatasets.get_value(dataset_name)
         super().__init__(**config_kwargs)  # calls _info where above is needed
         self.trace_dataset_creator = None
@@ -43,8 +40,10 @@ class TraceDatasetDownloader:
             download_manager = DownloadManager(download_config=download_config)
             data_dir = download_manager.download_and_extract(self.descriptor.get_url())
             assert os.path.isdir(data_dir), f"Expected {data_dir} to be folder."
+            zip_file_query = FileUtil.ls_dir(data_dir)
+            assert len(zip_file_query) == 1, f"Found more than one folder for extracted files:{zip_file_query}"
+            self.project_path = zip_file_query[0]  # include path to directory
             definition_content = JsonUtil.dict_to_json(self.descriptor.get_definition())
-            self.project_path = os.path.join(data_dir, self.zip_file_name)
             definition_file_path = os.path.join(self.project_path, StructureProjectDefinition.STRUCTURE_DEFINITION_FILE_NAME)
             FileUtil.write(definition_content, definition_file_path)
         return self.project_path

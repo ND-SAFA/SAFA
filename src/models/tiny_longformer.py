13dc5843d5e9003432563fed1f3375d3a52ede86
thearod5 <vhsalbertorodriguez@gmail.com>
2023-02-24 12:56:33 -0500

    FIX: Trying tiny model

--- src/models/tiny_longformer.py
+++ src/models/tiny_longformer.py
@@ -5,5 +5,5 @@ class TinyLongformer(LongformerForSequenceClassification):
 
     def __init__(self, config):
         config.intermediate_size = 3
-        config.hidden_size = 60
+        config.hidden_size = 24
         super().__init__(config)

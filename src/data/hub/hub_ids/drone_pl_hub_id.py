1d5fb946e04c25fd0defeed4062ceb3198c5358b
thearod5 <vhsalbertorodriguez@gmail.com>
2023-02-25 20:17:46 -0500

    Adding drone pl task

--- src/data/hub/hub_ids/drone_pl_hub_id.py
+++ src/data/hub/hub_ids/drone_pl_hub_id.py
@@ -1,6 +1,8 @@
+import os
 from typing import Dict
 
 from data.hub.abstract_hub_id import AbstractHubId
+from data.readers.definitions.structure_project_definition import StructureProjectDefinition
 from util.override import overrides
 
 
@@ -9,12 +11,27 @@ class DronePLHubId(AbstractHubId):
     Describes the DroneResponse project reader.
     """
 
+    def __init__(self, stage: str):
+        """
+        Loads stage of Drone programming language task.
+        :param stage: Either train, val, or eval
+        """
+        self.stage = stage
+
+    def get_definition_path(self, data_dir: str) -> str:
+        """
+        Returns path to stage definition file.
+        :param data_dir: The base project path containing all stages.
+        :return: Path to stage definition file.
+        """
+        return os.path.join(data_dir, self.stage, StructureProjectDefinition.STRUCTURE_DEFINITION_FILE_NAME)
+
     @overrides(AbstractHubId)
     def get_url(self) -> str:
         """
         :return: Returns URL to DroneResponse on the SAFA bucket containing definition file.
         """
-        return "https://safa-datasets-open.s3.amazonaws.com/datasets/Drone.zip"
+        return "https://safa-datasets-open.s3.amazonaws.com/datasets/drone_pl.zip"
 
     @overrides(AbstractHubId)
     def get_definition(self) -> Dict:
@@ -23,12 +40,13 @@ class DronePLHubId(AbstractHubId):
         """
         return {
             "artifacts": {
+
                 "Design Definitions": {
-                    "path": "Originals/Definitions/designdefinitions.csv",
+                    "path": "../Originals/Definitions/designdefinitions.csv",
                     "cols": "artifacts"
                 },
                 "Code": {
-                    "path": "Originals/Definitions/flatCode",
+                    "path": "../Originals/Definitions/flatCode",
                     "cols": "artifacts"
                 }
             },
@@ -36,7 +54,7 @@ class DronePLHubId(AbstractHubId):
                 "dd2codematrix.csv": {
                     "source": "Design Definitions",
                     "target": "Code",
-                    "path": "Originals/TraceMatrices/dd2codematrix.txt",
+                    "path": "../Originals/TraceMatrices/dd2codematrix.txt",
                     "cols": "traces",
                     "params": {
                         "sep": " "
@@ -55,7 +73,7 @@ class DronePLHubId(AbstractHubId):
             },
             "overrides": {
                 "allowed_orphans": 362,
-                "allowed_missing_sources": 98,
-                "allowed_missing_targets": 74
+                "allowed_missing_sources": 4,
+                "allowed_missing_targets": 17
             }
         }

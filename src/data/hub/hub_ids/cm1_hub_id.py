e448869b55be1cdb206c6164d2381e86632319b0
thearod5 <vhsalbertorodriguez@gmail.com>
2023-02-27 08:09:33 -0500

    FIX: Removing out of date structure definition

--- src/data/hub/hub_ids/cm1_hub_id.py
+++ src/data/hub/hub_ids/cm1_hub_id.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 from data.hub.abstract_hub_id import AbstractHubId
 from util.override import overrides
 
@@ -15,41 +13,3 @@ class CM1HubId(AbstractHubId):
         :return: Returns URL to CM1 on the SAFA bucket containing definition file.
         """
         return "https://safa-datasets-open.s3.amazonaws.com/datasets/CM1.zip"
-
-    def get_definition(self) -> Dict:
-        """
-        :return: Returns this project's structured project definition.
-        """
-        return {
-            "artifacts": {
-                "High Level Requirements": {
-                    "path": "CM1-sourceArtifacts.xml",
-                    "cols": "CM1-artifacts"
-                },
-                "Low Level Requirements": {
-                    "path": "CM1-targetArtifacts.xml",
-                    "cols": "CM1-artifacts"
-                }
-            },
-            "traces": {
-                "high2low.csv": {
-                    "source": "High Level Requirements",
-                    "target": "Low Level Requirements",
-                    "path": "CM1-answerSet.xml",
-                    "cols": "CM1-traces"
-                }
-            },
-            "conversions": {
-                "CM1-artifacts": {
-                    "id": "id",
-                    "content": "content"
-                },
-                "CM1-traces": {
-                    "source_artifact_id": "source",
-                    "target_artifact_id": "target"
-                }
-            },
-            "overrides": {
-                "allowed_orphans": 26
-            }
-        }

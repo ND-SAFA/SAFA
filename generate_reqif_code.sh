71aff897f785eca75e03730ed8947460c0525bd3
Evan Allan <evanallan1026@gmail.com>
2023-03-09 13:32:59 -0600

    Trying to change variable names with custom bindings but it's getting tedious so I'm just going to do the rest manually

--- generate_reqif_code.sh
+++ generate_reqif_code.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-xjc -d "$SCRIPT_DIR/src/main/java/" -p edu.nd.crc.safa.utilities.reqif -mark-generated -b "$SCRIPT_DIR/custom-binding.xjb" "$SCRIPT_DIR/reqif.xsd"
+xjc -d "$SCRIPT_DIR/src/main/java/" -p edu.nd.crc.safa.utilities.reqif.datatypes -mark-generated -b "$SCRIPT_DIR/custom-binding.xjb" "$SCRIPT_DIR/reqif.xsd"

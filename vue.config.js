b0e10adb8e8d3361f9d59145d8593a9f1711a166
Tim <tjnewman111@gmail.com>
2023-03-09 16:17:23 -0800

    updated deprecated https server config

--- vue.config.js
+++ vue.config.js
@@ -21,9 +21,12 @@ module.exports = {
   },
   devServer: fs.existsSync("./certs/localhost.safa.ai.pem")
     ? {
-        https: {
-          key: fs.readFileSync("./certs/localhost.safa.ai-key.pem"),
-          cert: fs.readFileSync("./certs/localhost.safa.ai.pem"),
+        server: {
+          type: "https",
+          options: {
+            key: fs.readFileSync("./certs/localhost.safa.ai-key.pem"),
+            cert: fs.readFileSync("./certs/localhost.safa.ai.pem"),
+          },
         },
         host: "localhost.safa.ai",
         port: 8080,

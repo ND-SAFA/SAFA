cf61be03e93e100ba4483ddcfa2a49e6c82cd2cb
thearod5 <vhsalbertorodriguez@gmail.com>
2023-03-15 14:54:10 -0700

    Adding predict job for casual model

--- src/models/llama/llama_task.py
+++ src/models/llama/llama_task.py
@@ -1,8 +1,9 @@
 from enum import Enum
 
-from models.llama.modeling_llama import LLaMAForSequenceClassification, LLaMAForSequenceSimilarity
+from models.llama.modeling_llama import LLaMAForCausalLM, LLaMAForSequenceClassification, LLaMAForSequenceSimilarity
 
 
 class LLaMATask(Enum):
     SEQUENCE_CLASSIFICATION = LLaMAForSequenceClassification
     SEQUENCE_SIMILARITY = LLaMAForSequenceSimilarity
+    CASUAL_LM = LLaMAForCausalLM

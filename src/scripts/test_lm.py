1ca0eb6456e5c54ca260f20039f660db8b107266
thearod5 <vhsalbertorodriguez@gmail.com>
2023-03-22 13:14:38 -0700

    Printing out the name of each layer in llama model

--- src/scripts/test_lm.py
+++ src/scripts/test_lm.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     trainer_dataset_manager = create_dataset_manager()
     logger.info("Created dataset manager! starting to loading model...")
     model = model_manager.get_model()
-    print(model.named_parameters())
+    print([name for name, param in model.named_parameters()])
     logger.info("Model loaded! Creating collator and trainer args...")
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding="max_length", max_length=512)

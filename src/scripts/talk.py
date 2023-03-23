2a126841edd7c74865d6dff912353d485f188d38
thearod5 <vhsalbertorodriguez@gmail.com>
2023-03-23 11:58:55 -0700

    Fixing mp size

--- src/scripts/talk.py
+++ src/scripts/talk.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     model = model_manager.get_model()
     deepspeed_config = JsonUtil.read_json_file(os.path.join(PROJ_PATH, "deepspeed.json"))
     ds_engine = deepspeed.init_inference(model,
-                                         mp_size=2,
+                                         mp_size=4,
                                          dtype=torch.half,
                                          replace_with_kernel_inject=True)
     model = ds_engine.module

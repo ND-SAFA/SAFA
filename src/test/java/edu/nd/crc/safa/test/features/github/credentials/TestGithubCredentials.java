9e3cff6ff4960106ad3617d26130fa6c4dd26ffa
Evan Allan <evanallan1026@gmail.com>
2023-04-25 15:37:33 -0500

    This should have been removed earlier

--- src/test/java/edu/nd/crc/safa/test/features/github/credentials/TestGithubCredentials.java
+++ src/test/java/edu/nd/crc/safa/test/features/github/credentials/TestGithubCredentials.java
@@ -2,7 +2,6 @@ package edu.nd.crc.safa.test.features.github.credentials;
 
 import edu.nd.crc.safa.config.AppRoutes;
 import edu.nd.crc.safa.features.github.entities.app.GithubResponseDTO;
-import edu.nd.crc.safa.features.github.entities.db.GithubAccessCredentials;
 import edu.nd.crc.safa.test.features.github.base.AbstractGithubTest;
 import edu.nd.crc.safa.test.requests.SafaRequest;
 
@@ -10,7 +9,6 @@ import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @Log4j2
@@ -28,22 +26,5 @@ public class TestGithubCredentials extends AbstractGithubTest {
         Assertions.assertEquals(GithubResponseDTO.GithubResponseMessage.MISSING.name(),
             response.get("message"));
     }
-
-    @Test
-    void checkingInvalidCredentialsDeletesThem() throws Exception {
-        GithubAccessCredentials credentials = new GithubAccessCredentials();
-
-        super.mockRepositoryFindByUser(credentials);
-        super.mockRepositoryDelete();
-
-        JSONObject response = SafaRequest
-            .withRoute(AppRoutes.Github.Credentials.VALID)
-            .getWithoutBody(MockMvcResultMatchers.status().is2xxSuccessful());
-
-        Assertions.assertEquals(GithubResponseDTO.GithubResponseMessage.EXPIRED.name(),
-            response.get("message"));
-
-        Mockito.verify(repositoryMock, Mockito.times(1))
-            .delete(Mockito.any(GithubAccessCredentials.class));
-    }
+    
 }

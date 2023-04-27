59f3281c867e62d6fe60a00a64e06a167b410d9a
thearod5 <vhsalbertorodriguez@gmail.com>
2023-04-27 11:39:36 -0700

    Removing no longer needed custom init

--- tgen/data/prompts/prompt_args.py
+++ tgen/data/prompts/prompt_args.py
@@ -6,14 +6,6 @@ class PromptArgs:
     """
     Defines arguments for defining properties for prompt dataset creation.
     """
-
-    def __init__(self, prompt_separator: str, completion_prefix: str, completion_suffix: str):
-        """
-        Constructs prompt args using base prompt and all arguments defined in AI prompt args.
-        :param prompt_separator: The delimiter between prompt and completion.
-        :param completion_prefix: The token to prefix completion labels with.
-        :param completion_suffix: The token to append completion labels with.
-        """
-        self.prompt_separator = prompt_separator
-        self.completion_prefix = completion_prefix
-        self.completion_suffix = completion_suffix
+    prompt_separator: str  # Goes in between the prompt and completion
+    completion_prefix: str  # Goes before the completion label during fine-tuning for classification
+    completion_suffix: str  # Goes after the completion label during fine-tuning for classification

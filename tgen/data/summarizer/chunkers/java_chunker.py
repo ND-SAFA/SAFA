424d9bf6fb6e1f2978b762c5cadf88f17136cc78
kat-dearstyne <katherine@jklfamily.com>
2023-04-28 21:45:17 -0400

    this doesnt work

--- tgen/data/summarizer/chunkers/java_chunker.py
+++ tgen/data/summarizer/chunkers/java_chunker.py
@@ -1,6 +1,13 @@
+import os
 from typing import List
 
+from tgen.constants.deliminator_constants import NEW_LINE
 from tgen.data.summarizer.chunkers.abstract_chunker import AbstractChunker
+import ast
+import javalang
+import javalang.ast
+
+from tgen.util.file_util import FileUtil
 
 
 class JavaChunker(AbstractChunker):
@@ -8,11 +15,31 @@ class JavaChunker(AbstractChunker):
     Handles chunking JAVA code into chunks within a model's token limit
     """
 
-    def chunk(self, content: str) -> List[str]:
+    def chunk(self, content: str, id_: str = None) -> List[str]:
         """
         Chunks the given JAVA code into pieces that are beneath the model's token limit
         :param content: The content to chunk
+        :param id_: The id_ associated with some content
         :return: The content chunked into sizes beneath the token limit
         """
-        # TODO
-        raise NotImplementedError()
+        lines = content.split(NEW_LINE)
+        tree = javalang.parse.parse(content)
+        tree
+
+    @staticmethod
+    def _get_node_content(node, lines: List[str]) -> str:
+        """
+        Gets the content of the node
+        :param node: The ast parsed node
+        :param lines: The lines of the code file
+        :return: The content of the node
+        """
+        start_lineno = node.lineno - 1
+        end_lineno = node.end_lineno
+        return os.linesep.join(lines[start_lineno:end_lineno])
+
+
+if __name__ == "__main__":
+    chunker = JavaChunker("model")
+    content = FileUtil.read_file("/home/kat/git-repos/safa/tgen/tgen/testres/data/chunker/test_java.java")
+    chunker.chunk(content)

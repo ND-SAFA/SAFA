da46d3e79f4afe619074f0d30eb04628238f0116
Tim <tjnewman111@gmail.com>
2023-05-01 16:44:50 -0700

    updated the search view to use proper components

--- src/views/private/SearchView.vue
+++ src/views/private/SearchView.vue
@@ -8,9 +8,22 @@
             outlined
             label="Search Mode"
             :options="modes"
+            option-label="id"
+            option-value="id"
+            emit-value
             class="nav-search-mode"
             style="min-width: 150px"
-          />
+          >
+            <template #option="{ opt, itemProps }">
+              <q-separator v-if="opt.id === 'Search'" />
+              <list-item
+                clickable
+                :title="opt.id"
+                :subtitle="opt.name"
+                v-bind="itemProps"
+              />
+            </template>
+          </q-select>
           <q-select
             v-model="prompt"
             outlined
@@ -24,29 +37,28 @@
               <icon variant="search" />
             </template>
             <template #before-options> </template>
-            <template #no-option>
+            <template v-if="mode !== 'Search'" #no-option>
               <flex-box column y="2" x="2">
                 <flex-box full-width>
-                  <q-select
+                  <artifact-type-input
                     v-model="searchTypes"
-                    outlined
+                    multiple
                     label="Search Artifact Types"
                     hint="What types am I generating relationships to."
                     class="full-width"
                     style="min-width: 250px"
                   />
-                  <q-input
+                  <text-input
                     v-model="maxResults"
-                    outlined
                     type="number"
                     label="Max Results"
                     class="q-ml-sm"
                     style="min-width: 100px; width: 100px"
                   />
                 </flex-box>
-                <q-select
+                <artifact-type-input
                   v-model="relateTypes"
-                  outlined
+                  multiple
                   label="Include Artifact Types"
                   hint="What types do I want to see related to my search."
                   class="full-width q-my-md"
@@ -75,11 +87,32 @@ export default {
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import { PrivatePage, FlexBox, PanelCard, Icon } from "@/components";
+import { SelectOption } from "@/types";
+import { artifactStore, typeOptionsStore } from "@/hooks";
+import {
+  PrivatePage,
+  FlexBox,
+  PanelCard,
+  Icon,
+  ArtifactTypeInput,
+  ListItem,
+  TextInput,
+} from "@/components";
 
-type Mode = "Prompt" | "Artifact" | "Artifact Type";
+type Mode = "Prompt" | "Artifact" | "Artifact Type" | "Search";
 
-const modes: Mode[] = ["Prompt", "Artifact", "Artifact Type"];
+const modes: SelectOption[] = [
+  { id: "Prompt", name: "Find artifacts that match a search prompt." },
+  { id: "Artifact", name: "Find artifacts related to a specific artifact." },
+  {
+    id: "Artifact Type",
+    name: "Find artifacts related to a specific artifact type.",
+  },
+  {
+    id: "Search",
+    name: "Search through currently displayed artifacts.",
+  },
+];
 
 const mode = ref<Mode>("Prompt");
 const prompt = ref<string>("");
@@ -93,8 +126,19 @@ const placeholder = computed(
       Prompt: "Enter a prompt...",
       Artifact: "Search artifacts...",
       "Artifact Type": "Search artifact types...",
+      Search: "Search current artifacts...",
     }[mode.value])
 );
+
+// const searchOptions = computed(() => {
+//   if (["Artifact", "Search"].includes(mode.value)) {
+//     return artifactStore.currentArtifacts;
+//   } else if (mode.value === "Artifact Type") {
+//     return typeOptionsStore.artifactTypes;
+//   } else {
+//     return [];
+//   }
+// });
 </script>
 
 <style lang="scss">

d956cbb48da34d76616f4d66e96d71c6e15e4bfc
Evan Allan <evanallan1026@gmail.com>
2023-05-06 13:09:49 -0500

    Don't make a constructor at all

--- src/main/java/edu/nd/crc/safa/features/github/entities/api/graphql/GithubGraphQlQueries.java
+++ src/main/java/edu/nd/crc/safa/features/github/entities/api/graphql/GithubGraphQlQueries.java
@@ -3,7 +3,7 @@ package edu.nd.crc.safa.features.github.entities.api.graphql;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.NONE)
 public final class GithubGraphQlQueries {
     public static final String GET_REPOSITORIES = "github/GetRepositories";
     public static final String GET_REPOSITORY_BY_NAME = "github/GetRepositoryByName";

eca57dcd94ac853698099a079cce02c83615c5b1
thearod5 <vhsalbertorodriguez@gmail.com>
2023-05-08 13:44:13 -0700

    Fixing linting errors

--- src/main/java/edu/nd/crc/safa/features/summary/TGenSummaryArtifact.java
+++ src/main/java/edu/nd/crc/safa/features/summary/TGenSummaryArtifact.java
@@ -2,21 +2,9 @@ package edu.nd.crc.safa.features.summary;
 
 import lombok.Data;
 
-enum TGenSummaryArtifactType {
-    /**
-     * Identifies natural language artifacts.
-     */
-    NL,
-    /**
-     * Identifies python files.
-     */
-    PY,
-    /**
-     * Identifies java files.
-     */
-    JAVA
-}
-
+/**
+ * The artifact representation for summarization.
+ */
 @Data
 class TGenSummaryArtifact {
     /**
---
+++ src/main/java/edu/nd/crc/safa/features/summary/TGenSummaryArtifactType.java
@@ -0,0 +1,19 @@
+package edu.nd.crc.safa.features.summary;
+
+/**
+ * Enumerates all the different types of artifacts that can be sumarized.
+ */
+enum TGenSummaryArtifactType {
+    /**
+     * Identifies natural language artifacts.
+     */
+    NL,
+    /**
+     * Identifies python files.
+     */
+    PY,
+    /**
+     * Identifies java files.
+     */
+    JAVA
+}

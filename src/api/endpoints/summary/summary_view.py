69e09c42fed41afcec438ff6b7eba34bf83bb882
thearod5 <vhsalbertorodriguez@gmail.com>
2023-05-12 10:37:29 -0700

    Quick fix for summarization API, tech debt is observed and will be managed

--- src/api/endpoints/summary/summary_view.py
+++ src/api/endpoints/summary/summary_view.py
@@ -1,15 +1,15 @@
+import json
+
 from django.http import HttpRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 
-from api.endpoints.summary.summary_serializer import SummarySerializer
 from api.utils.model_util import ModelUtil
-from api.utils.view_util import ViewUtil
 from tgen.data.summarizer.summarizer import Summarizer
 from tgen.jobs.components.args.job_args import JobArgs
-from tgen.jobs.components.job_result import JobResult
 from tgen.jobs.data_jobs.summarize_artifacts_job import SummarizeArtifactsJob
 from tgen.util.json_util import NpEncoder
+from tgen.util.status import Status
 
 
 class SummaryView(APIView):
@@ -24,13 +24,17 @@ class SummaryView(APIView):
         :param request: Request containing artifacts to summarize.
         :return: The same artifacts with content as summary.
         """
-        request_data = ViewUtil.read_request(request, SummarySerializer)
+        request_data = json.loads(request.body)
         artifacts = request_data["artifacts"]
         llm_name = request_data.get("model", ModelUtil.get_default_model())
         job_args = JobArgs()
         model, llm_manager = ModelUtil.get_model_manager(llm_name)
 
-        summarizer = Summarizer(llm_manager)
+        summarizer = Summarizer(llm_manager, code_or_exceeds_limit_only=False)
         summarize_job = SummarizeArtifactsJob(artifacts, job_args=job_args, summarizer=summarizer)
-        summarized_artifacts = summarize_job.run()[JobResult.ARTIFACTS]
-        return JsonResponse({"artifacts": summarized_artifacts}, encoder=NpEncoder)
+        job_result = summarize_job.run()
+        job_body = job_result.to_json(as_dict=True)["body"]
+        if job_result.status == Status.FAILURE:
+            return JsonResponse(job_body, status=400)
+        summarized_artifacts = job_body
+        return JsonResponse({"body": {"artifacts": summarized_artifacts}}, encoder=NpEncoder)

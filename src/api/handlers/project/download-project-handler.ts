a0634f4672037fd35391a027cc2553752b080ee6
Tim <tjnewman111@gmail.com>
2023-05-18 14:16:53 -0700

    added a button for downloading project files in JSON

--- src/api/handlers/project/download-project-handler.ts
+++ src/api/handlers/project/download-project-handler.ts
@@ -4,10 +4,13 @@ import { projectStore } from "@/hooks";
 import { getProjectFiles } from "@/api";
 
 /**
- * Creates a file download for project csv files.
+ * Creates a file download for project files, either in csv for json format.
+ * @param fileType - The file format to download.
  */
-export async function handleDownloadProjectCSV(): Promise<void> {
-  const data = await getProjectFiles(projectStore.versionId);
+export async function handleDownloadProject(
+  fileType: "csv" | "json" = "csv"
+): Promise<void> {
+  const data = await getProjectFiles(projectStore.versionId, fileType);
 
   const fileName = `${projectStore.project.name}-${versionToString(
     projectStore.version

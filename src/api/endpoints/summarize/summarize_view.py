abd196d0d5c060139ea69319836ba1ae598d8b07
thearod5 <vhsalbertorodriguez@gmail.com>
2023-05-21 12:33:46 -0700

    Removed conversion to EnumDict (already happens in job)

--- src/api/endpoints/summarize/summarize_view.py
+++ src/api/endpoints/summarize/summarize_view.py
@@ -6,7 +6,6 @@ from api.utils.model_util import ModelUtil
 from tgen.data.summarizer.summarizer import Summarizer
 from tgen.jobs.components.args.job_args import JobArgs
 from tgen.jobs.data_jobs.summarize_artifacts_job import SummarizeArtifactsJob
-from tgen.util.enum_util import EnumDict
 
 
 class SummarizeView(APIView):
@@ -22,7 +21,6 @@ class SummarizeView(APIView):
         :return: The same artifacts with content as summary.
         """
         artifacts = request_data["artifacts"]
-        artifacts = [EnumDict(a) for a in artifacts]
         llm_name = request_data.get("model", ModelUtil.get_default_model())
         job_args = JobArgs()
         model, llm_manager = ModelUtil.get_model_manager(llm_name)

40ad8a412780fe804c0d8e125143c7db936f96f0
Tim <tjnewman111@gmail.com>
2023-05-24 13:25:34 -0700

    outlined hgen panel

---
+++ src/components/hgen/ArtifactGenerationPanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <details-panel panel="displayArtifactBody"> </details-panel>
+</template>
+
+<script lang="ts">
+/**
+ * Displays inputs for generating new parent artifacts based on child artifacts.
+ */
+export default {
+  name: "ArtifactGenerationPanel",
+};
+</script>
+
+<script setup lang="ts">
+import { DetailsPanel } from "@/components/common";
+</script>

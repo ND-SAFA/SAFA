937b0d94ce319725088856b94b870817c081a601
kat-dearstyne <katherine@jklfamily.com>
2023-05-31 21:28:56 -0400

    update docs

--- tgen/train/metrics/specificity_metric.py
+++ tgen/train/metrics/specificity_metric.py
@@ -9,16 +9,15 @@ from tgen.train.metrics.abstract_trace_metric import AbstractTraceMetric
 from tgen.train.metrics.confusion_matrix_at_threshold_metric import ConfusionMatrixAtThresholdMetric
 
 _DESCRIPTION = """
-Confusion matrix metric calculates the number of true and false positives and the true/false negatives.
+Specificity metric calculates the number of true negatives over all predicted negatives.
 """
 
 _KWARGS_DESCRIPTION = """
 Args:
     predictions (`list` of `int`): Predicted similarity scores.
     references (`list` of `int`): Ground truth labels.
-    k (int): The level of the threshold to consider a similar score a true label.
 Returns:
-    precision_at_k (`float` or `int`): Precision@K score. 
+    specificity (`float` or `int`): Specificity score. 
 """
 
 _CITATION = """
@@ -30,12 +29,11 @@ class SpecificityMetric(AbstractTraceMetric):
 
     def _compute(self, predictions, references, **kwargs) -> float:
         """
-        Confusion matrix metric calculates the number of true and false positives and the true/false negatives.
+        Calculates specificity (the number of true negatives over all predicted negatives).
         :param predictions: predicted labels
         :param labels: ground truth labels.
-        :param k: considers only the subset of recommendations from rank 1 through k
         :param kwargs: any other necessary params
-        :return: Dictionary containing counts for the different results.
+        :return: The specificity
         """
         matrix: Dict = ConfusionMatrixAtThresholdMetric()._compute(predictions, references, **kwargs)
         return matrix["tn"] / (matrix["tn"] + matrix["fp"])

48b5329e51cc067d506cfe73b630b03289aa4741
Tim <tjnewman111@gmail.com>
2023-06-14 16:54:53 -0700

    added clicking TIM elements a second time to open a view of their artifacts

--- src/cytoscape/events/artifact-tree-events.ts
+++ src/cytoscape/events/artifact-tree-events.ts
@@ -6,7 +6,13 @@ import {
   TimNodeCytoElementData,
   TraceCytoElementData,
 } from "@/types";
-import { appStore, layoutStore, selectionStore, traceStore } from "@/hooks";
+import {
+  appStore,
+  documentStore,
+  layoutStore,
+  selectionStore,
+  traceStore,
+} from "@/hooks";
 import {
   ARTIFACT_EDGE_SELECTOR,
   ARTIFACT_NODE_SELECTOR,
@@ -92,7 +98,13 @@ export const ArtifactTreeCytoEvents: CytoEventHandlers = {
 
       if (!artifactLevel.artifactType) return;
 
-      selectionStore.selectArtifactLevel(artifactLevel.artifactType);
+      if (
+        selectionStore.selectedArtifactLevelType !== artifactLevel.artifactType
+      ) {
+        selectionStore.selectArtifactLevel(artifactLevel.artifactType);
+      } else {
+        documentStore.addDocumentOfTypes([artifactLevel.artifactType]);
+      }
     },
   },
   selectTimEdge: {
@@ -103,10 +115,20 @@ export const ArtifactTreeCytoEvents: CytoEventHandlers = {
 
       if (!artifactLevel.source || !artifactLevel.target) return;
 
-      selectionStore.selectTraceMatrix(
-        artifactLevel.target,
-        artifactLevel.source
-      );
+      if (
+        selectionStore.selectedTraceMatrixTypes[0] !== artifactLevel.target ||
+        selectionStore.selectedTraceMatrixTypes[1] !== artifactLevel.source
+      ) {
+        selectionStore.selectTraceMatrix(
+          artifactLevel.target,
+          artifactLevel.source
+        );
+      } else {
+        documentStore.addDocumentOfTypes([
+          artifactLevel.targetType,
+          artifactLevel.sourceType,
+        ]);
+      }
     },
   },
 };

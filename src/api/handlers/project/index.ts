f2b7441b2b91c63dad155ff6d941ab3e7a8d220d
Tim <tjnewman111@gmail.com>
2023-06-15 17:34:50 -0700

    attribute handler -> api store

--- src/api/handlers/project/index.ts
+++ src/api/handlers/project/index.ts
@@ -1,3 +1,2 @@
 export * from "./integration-handler";
 export * from "./model-handler";
-export * from "./attribute-handler";
--- src/hooks/api/settings/index.ts
+++ src/hooks/api/settings/index.ts
@@ -1,1 +1,2 @@
 export { default as memberApiStore } from "./useMemberApi";
+export { default as attributeApiStore } from "./useAttributeApi";

1fe163091a4e43ae5d8af2e8cc1c58bc1ba3080f
thearod5 <vhsalbertorodriguez@gmail.com>
2023-06-29 19:57:10 -0400

    Adding updates after Kat's feedback

--- tgen/jobs/trainer_jobs/tracing_job.py
+++ tgen/jobs/trainer_jobs/tracing_job.py
@@ -9,6 +9,8 @@ from tgen.models.llm.anthropic_manager import AnthropicManager
 from tgen.models.llm.llm_responses import GenerationResponse
 from tgen.models.llm.llm_task import LLMCompletionType
 from tgen.train.args.anthropic_args import AnthropicArgs, AnthropicParams
+from tgen.train.metrics.metrics_manager import MetricsManager
+from tgen.train.metrics.supported_trace_metric import SupportedTraceMetric
 from tgen.train.trace_output.abstract_trace_output import AbstractTraceOutput
 from tgen.train.trace_output.trace_prediction_output import TracePredictionEntry, TracePredictionOutput
 from tgen.train.trainers.trainer_task import TrainerTask
@@ -40,6 +42,12 @@ class TracingJob(LLMJob):
             source_predicted_entries = self.parse_ranking_response(source, response, source2entries)
             predicted_entries.extend(source_predicted_entries)
 
+        metrics_manager = MetricsManager(trace_predictions=predicted_entries)
+        metrics = list(SupportedTraceMetric.get_keys())
+        metrics = metrics_manager.eval(metrics)
+        print("Metrics")
+        print(metrics)
+        print()
         return TracePredictionOutput(prediction_entries=predicted_entries)
 
     @staticmethod
@@ -52,7 +60,7 @@ class TracingJob(LLMJob):
     @staticmethod
     def parse_ranking_response(source, r, source2entries):
         source_entries = source2entries[source]
-        source_target_ids = list(map(lambda t: t["target"], source_entries))
+        source_target_ids = [t["target"] for t in source_entries]
         target2labels = {entry["target"]: entry["label"] for entry in source_entries}
         ranked_target_ids = TracingJob.parse_target_ids(r, source_target_ids)
         scores = TracingJob.assign_scores_to_targets(ranked_target_ids)
@@ -68,11 +76,12 @@ class TracingJob(LLMJob):
         return source_predicted_entries
 
     @staticmethod
-    def create_ranking_prompts(artifact_map, source2entries):
+    def create_ranking_prompts(artifact_map, source2entries: Dict[str, List[TracePredictionEntry]]):
         sources = []
         prompts = []
         for source, source_predicted_entries in source2entries.items():
-            target_ids = list(map(lambda t: t["target"], source_predicted_entries))
+            source_predicted_entries = sorted(source_predicted_entries, key=lambda t: t["score"], reverse=True)
+            target_ids = [t["target"] for t in source_predicted_entries]
             prompt = TracingJob.format_ranking_prompt(source, target_ids, artifact_map)
             sources.append(source)
             prompts.append(prompt)
@@ -101,7 +110,7 @@ class TracingJob(LLMJob):
 
     @staticmethod
     def format_ranking_prompt(source: str, targets: List[str], artifact_map: Dict[str, str]):
-        raise NotImplementedError();
+        raise NotImplementedError()
 
     @staticmethod
     def format_targets(artifacts: List[str], artifact_map: Dict[str, str]):

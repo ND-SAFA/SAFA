063131419ee824dc7527219767c16db53c46122e
kat-dearstyne <katherine@jklfamily.com>
2023-06-29 14:36:49 -0400

    update llmresponseutil for multiple tags

--- tgen/util/llm_response_util.py
+++ tgen/util/llm_response_util.py
@@ -9,21 +9,25 @@ from tgen.util.logging.logger_manager import logger
 class LLMResponseUtil:
 
     @staticmethod
-    def parse(res: str, tag_name: str, is_nested: bool = False) -> Union[str, List[Tag]]:
+    def parse(res: str, tag_name: str, is_nested: bool = False, raise_exception: bool = False) -> Union[List[str], List[Tag]]:
         """
         Parses the LLM response for the given html tags
         :param res: The LLM response
         :param tag_name: The name of the tag to find
         :param is_nested: If True, the response contains nested tags so all Tag objects are returned, else just the single content
+        :param raise_exception: if True, raises an exception if parsing fails
         :return: Either a list of tags (if nested) or the content inside the tag (not nested)
         """
         tags = BeautifulSoup(res, features="lxml").findAll(tag_name)
 
         try:
             assert len(tags) > 0, f"Missing expected tag {tag_name}"
-            content = tags[0].contents[0] if not is_nested else tags
+            content = [tag.contents[0] for tag in tags] if not is_nested else tags
         except (AssertionError, IndexError):
-            logger.exception(f"Unable to parse {res}")
+            error = f"Unable to parse {res}"
+            logger.exception(error)
+            if raise_exception:
+                raise Exception(error)
             content = res if not is_nested else []
         return content
 
@@ -39,7 +43,10 @@ class LLMResponseUtil:
             labels2props = {label: label for label in labels2props}
         props = {}
         for tag, prop in labels2props.items():
-            prop_value = LLMResponseUtil.parse(r, tag)
+            try:
+                prop_value = LLMResponseUtil.parse(r, tag, raise_exception=True)
+            except Exception:
+                prop_value = []
             props[prop] = prop_value
         return props
 

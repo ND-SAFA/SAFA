ecb36af4a9a3a170f0cff079ffd210644a14088d
Tim <tjnewman111@gmail.com>
2023-07-19 18:05:45 -0400

    added basis for tracking the position of a node on the graph

--- src/components/graph/base/CyElement.vue
+++ src/components/graph/base/CyElement.vue
@@ -1,44 +1,94 @@
 <template>
-  <div />
+  <div
+    :style="
+      isNode
+        ? 'position: absolute; ' +
+          // 'webkitTransformOrigin: top left; msTransformOrigin: top left; transformOrigin: top left;' +
+          'background: red; height: 4px; width: 4px; z-index: 1000; ' +
+          style
+        : undefined
+    "
+  />
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onBeforeUnmount, onMounted, inject } from "vue";
+import { ref, watch, onBeforeUnmount, onMounted, inject, computed } from "vue";
 import {
   Selector,
   Core,
   ElementDefinition,
   CollectionReturnValue,
   Position,
+  EventObject,
 } from "cytoscape";
+import { GraphElementType } from "@/types";
 
 const props = defineProps<{
   definition: ElementDefinition;
 }>();
 
-// const emit = defineEmits<{
-//   (e: CytoEvent, event: EventObject): void;
-// }>();
-
 const id = ref<string>(props.definition.data.id || "");
 const selector = ref<Selector>(`#${id.value}`);
 const instance = ref<Core | undefined>(undefined);
+const pos = ref<{ x: number; y: number } | undefined>();
+const absTransform = ref("");
+const relTransform = ref("");
+const style = ref("");
 
 const cy = inject<Promise<Core>>("cy");
 
+const isNode = computed(
+  () => props.definition.data.type === GraphElementType.node
+);
+
 function add(): CollectionReturnValue | undefined {
-  // register all the component events as cytoscape ones
-  // for (const eventType of Object.values(CytoEvent)) {
-  //   instance.value?.on(eventType, selector.value, (event: EventObject) => {
-  //     emit(eventType, event);
-  //   });
-  // }
+  listenForMove();
+  listenForPanZoom();
 
   // strip observers from the original definition
   const def = JSON.parse(JSON.stringify(props.definition));
 
   // add the element to cytoscape
   return instance.value?.add(def);
+}
+
+function listenForMove(): void {
+  if (!isNode.value) return;
+
+  const onMove = (event: EventObject) => {
+    const oldPos = pos.value;
+    const newPos = event.target.position();
+
+    if (oldPos?.x !== newPos.x || oldPos?.y !== newPos.y) {
+      console.log("moving", selector.value, newPos);
+
+      absTransform.value = `translate(${newPos.x.toFixed(
+        2
+      )}px,${newPos.y.toFixed(2)}px)`;
+      pos.value = { ...newPos };
+    }
+  };
+
+  instance.value?.on("position bounds", selector.value, onMove);
+
+  // TODO: cleanup listener on unmount
+}
+
+function listenForPanZoom(): void {
+  if (!isNode.value) return;
+
+  const onPanZoom = (event: EventObject) => {
+    const pan = event.cy.pan();
+    const zoom = event.cy.zoom();
+
+    console.log("panning", pan, zoom);
+
+    relTransform.value = `translate(${pan.x}%,${pan.y}%) scale(${zoom})`;
+  };
+
+  instance.value?.on("pan zoom", onPanZoom);
+
+  // TODO: cleanup listener on unmount
 }
 
 onMounted(() => {
@@ -78,5 +128,16 @@ watch(
     ele?.position(JSON.parse(JSON.stringify(position)));
   },
   { deep: true }
+);
+
+watch(
+  () => [absTransform.value, relTransform.value],
+  ([abs, rel]) => {
+    const transform = `${rel} ${abs}`;
+
+    console.log("updating style", selector.value, transform);
+
+    style.value = `webkitTransform: ${transform}; msTransform: ${transform}; transform: ${transform};`;
+  }
 );
 </script>

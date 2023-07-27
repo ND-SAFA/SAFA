435d7b0b4ff65b635b140550e7a172eb01300205
thearod5 <vhsalbertorodriguez@gmail.com>
2023-07-27 01:01:34 -0400

    Minor enhancement
    # Conflicts:
    #	tgen/ranking/ranking_state.py

--- tgen/ranking/ranking_state.py
+++ tgen/ranking/ranking_state.py
@@ -6,20 +6,27 @@ from tgen.models.llm.llm_responses import GenerationResponse
 from tgen.state.state import State
 
 DEFAULT_RANKING_GOAL = "# Task\n\n" \
-                       "You are a software engineer on a software project. " \
-                       "You are given a parent software artifact and a list of potential children." \
-                       "You task is to find the children of the parent artifact.\n\n"
+                       "Below is a set of software artifacts of a project. You are the expert on this project. " \
+                       "You are performing traceability by finding the children associated with a given parent artifact. " \
+                       "You are focusing on a single parent artifact. " \
+                       "Enclosed in <parent-artifact><parent-artifact> the parent artifact followed by " \
+                       "description of the project and then by the list of potential children (formatted in XML)." \
+                       "\n\n"
 DEFAULT_RANKING_INSTRUCTIONS = "# Instructions\n" \
-                               "1. Concisely describe the functionality of the parent. " \
-                               " Focus on the unique job of the parent in the context of the system." \
+                               "1. Provide 2-3 sentences describing the job of the parent artifact. " \
+                               "Include its responsibilities, its dependencies, and its purpose in the system. " \
                                "Enclose your answer in <function></function>" \
-                               "\n2. List all artifacts that help implement the parent functionality. " \
-                               "Provide the list of artifacts along with a sentence describing how the artifact helps the parent. " \
-                               "Put each entry on different lines and follow the format: ID - RELATIONSHIP. " \
+                               "\n2. Read each potential child artifact in order. " \
+                               "Decide whether the artifact impacts, implements, or refines the parent artifact functionality. " \
+                               "Format your answer as: ID - Yes/No. Enclose your answer in <classification></classification>." \
+                               "\n3. For each selected artifact, " \
+                               "provide a sentence describing how its role in the system helps the parent artifact achieve its job. " \
+                               "Put each entry on different lines and follow the format: ID - DESCRIPTION. " \
+                               "Work through one artifact at a time so that the IDs are in ascending order. " \
                                "Enclose your answer in <related></related>." \
-                               "\n3. Rank the artifacts from most to least helpful to the implementation of parent's functionality. " \
-                               "Provide the ranking as comma delimited list of artifact ids where the " \
-                               "first element is the most related while the last element is the least. " \
+                               "\n3. Rank the children by proximity to the parent artifact functionality." \
+                               "Provide a comma delimited list of artifact ids where the " \
+                               "first element is the most similar to the parent artifact while the last element is the least. " \
                                "Enclose the list in <links></links>."
 
 DEFAULT_EXPERIMENT_DIR = os.path.expanduser("~/desktop/safa/experiments/rankings")

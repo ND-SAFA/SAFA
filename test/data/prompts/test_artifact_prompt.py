ebb63737b2540026e6a51f4971d2f16e47cc1e54
thearod5 <vhsalbertorodriguez@gmail.com>
2023-08-13 13:20:51 -0500

    Fixed all but the hgen test

--- test/data/prompts/test_artifact_prompt.py
+++ test/data/prompts/test_artifact_prompt.py
@@ -1,5 +1,5 @@
+from test.data.prompts.artifact_prompt_test_util import ArtifactPromptTestUtil
 from tgen.common.util.enum_util import EnumDict
-from tgen.constants.deliminator_constants import NEW_LINE, EMPTY_STRING
 from tgen.data.dataframes.artifact_dataframe import ArtifactKeys
 from tgen.data.prompts.artifact_prompt import ArtifactPrompt
 from tgen.testres.base_tests.base_test import BaseTest
@@ -13,28 +13,21 @@ class TestArtifactPrompt(BaseTest):
         id_, content = self.ARTIFACT[ArtifactKeys.ID], self.ARTIFACT[ArtifactKeys.CONTENT]
 
         base_with_id = ArtifactPrompt(self.PROMPT, build_method=ArtifactPrompt.BuildMethod.BASE, include_id=True)
-        output = base_with_id._build(self.ARTIFACT)
-        expected_format = f"{id_}: {content}"
-        self.eval_format(expected_format, output)
+        prompt = base_with_id._build(self.ARTIFACT)
+        expected_artifact_format = f"{id_}: {content}"
+        ArtifactPromptTestUtil.assert_expected_format(self, prompt, self.PROMPT, expected_artifact_format)
 
         base_without_id = ArtifactPrompt(self.PROMPT, build_method=ArtifactPrompt.BuildMethod.BASE, include_id=False)
-        output = base_without_id._build(self.ARTIFACT)
-        expected_format = f"{content}"
-        self.eval_format(expected_format, output)
+        prompt = base_without_id._build(self.ARTIFACT)
+        expected_artifact_format = f"{content}"
+        ArtifactPromptTestUtil.assert_expected_format(self, prompt, self.PROMPT, expected_artifact_format)
 
         xml_with_id = ArtifactPrompt(self.PROMPT, build_method=ArtifactPrompt.BuildMethod.XML, include_id=True)
-        output = xml_with_id._build(self.ARTIFACT)
-        expected_format = f"<artifact><id>{id_}</id><body>{content}</body></artifact>"
-        self.eval_format(expected_format, output)
+        prompt = xml_with_id._build(self.ARTIFACT)
+        expected_artifact_format = f"<artifact>\n\t<id>{id_}</id>\n\t<body>{content}</body>\n</artifact>"
+        ArtifactPromptTestUtil.assert_expected_format(self, prompt, self.PROMPT, expected_artifact_format)
 
         xml_without_id = ArtifactPrompt(self.PROMPT, build_method=ArtifactPrompt.BuildMethod.XML, include_id=False)
-        output = xml_without_id._build(self.ARTIFACT)
-        expected_format = f"<artifact>{content}</artifact>"
-        self.eval_format(expected_format, output)
-
-    def eval_format(self, expected_format: str, output: str):
-        split_by_newline = [p for p in output.split(NEW_LINE) if p]
-        prompt = split_by_newline[0]
-        a_format = EMPTY_STRING.join(split_by_newline[1:])
-        self.assertEqual(prompt, self.PROMPT)
-        self.assertEqual(a_format, expected_format)
+        prompt = xml_without_id._build(self.ARTIFACT)
+        expected_artifact_format = f"<artifact>\n\t{content}\n</artifact>"
+        ArtifactPromptTestUtil.assert_expected_format(self, prompt, self.PROMPT, expected_artifact_format)

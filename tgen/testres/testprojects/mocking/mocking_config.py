9cada7e5809fdbb1bc200a8df24833a6ed010283
thearod5 <vhsalbertorodriguez@gmail.com>
2023-08-14 09:54:41 -0500

    FIX: Mocking the class and not the actual object

--- tgen/testres/testprojects/mocking/mocking_config.py
+++ tgen/testres/testprojects/mocking/mocking_config.py
@@ -1,8 +1,8 @@
-from tgen.testres.testprojects.mocking.library_formatters import openai_response_formatter, anthropic_response_formatter
+from tgen.testres.testprojects.mocking.library_formatters import anthropic_response_formatter, openai_response_formatter
 
 library_mock_map = {
     "openai": "openai.ChatCompletion.create",
-    "anthropic": "tgen.testres.testprojects.mocking.mock_anthropic.MockAnthropicClient"
+    "anthropic": "tgen.testres.testprojects.mocking.mock_anthropic.MockAnthropicClient.completion"
 }
 library_formatter_map = {
     "openai": openai_response_formatter,

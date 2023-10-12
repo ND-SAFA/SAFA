17bf61f0eb19d936d3f55d2009f9a78d4f73b29c
Evan Allan <evanallan1026@gmail.com>
2023-10-12 15:44:56 -0500

    Update migration to work with test system

--- src/main/resources/db/migration/V28_3__add_users_to_their_orgs.sql
+++ src/main/resources/db/migration/V28_3__add_users_to_their_orgs.sql
@@ -1,7 +1,10 @@
-DELETE org_membership
-    FROM org_membership
-    INNER JOIN organization on org_membership.org_id = organization.id
-    WHERE organization.personal_org = True;
+DELETE FROM org_membership
+WHERE EXISTS (
+    SELECT 1
+        FROM organization
+        WHERE org_membership.org_id = organization.id
+            AND organization.personal_org = True
+);
 
 INSERT INTO org_membership (user_id, org_id, role, id)
 SELECT safa_user.user_id, organization.id, 'ADMIN', (SELECT ${uuid_generator})

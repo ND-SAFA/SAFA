d917bdcc37649a23163cf321727dfd9845a5ed5a
Tim <tjnewman111@gmail.com>
2023-10-18 17:06:26 -0400

    code cleanup

--- src/views/private/HomeView.vue
+++ src/views/private/HomeView.vue
@@ -50,52 +50,6 @@
       </flex-box>
     </panel-card>
     <external-links />
-
-    <!--    <flex-box :column="smallWindow">-->
-    <!--      <flex-item :parts="parts" :full-width="smallWindow" class="q-mr-md">-->
-    <!--        <panel-card title="Load Existing Project" icon="home-list">-->
-    <!--          <typography-->
-    <!--            variant="small"-->
-    <!--            value="Select an existing project and version to load."-->
-    <!--          />-->
-    <!--          <project-version-stepper minimal />-->
-    <!--        </panel-card>-->
-    <!--      </flex-item>-->
-    <!--      <flex-item :parts="parts" :full-width="smallWindow">-->
-    <!--        <panel-card title="Create New Project" icon="home-add">-->
-    <!--          <typography-->
-    <!--            variant="small"-->
-    <!--            value="Chose which data source you would like to create a project from."-->
-    <!--          />-->
-    <!--          <flex-box full-width justify="center" t="2">-->
-    <!--            <flex-box column>-->
-    <!--              <text-button-->
-    <!--                text-->
-    <!--                label="Create New Project"-->
-    <!--                icon="add"-->
-    <!--                color="primary"-->
-    <!--                @click="handleOpenStandard"-->
-    <!--              />-->
-    <!--              <text-button-->
-    <!--                text-->
-    <!--                label="Bulk Upload Project"-->
-    <!--                icon="upload"-->
-    <!--                color="primary"-->
-    <!--                @click="handleOpenBulk"-->
-    <!--              />-->
-    <!--              <text-button-->
-    <!--                text-->
-    <!--                label="Import Jira/GitHub Project"-->
-    <!--                icon="integrate"-->
-    <!--                color="primary"-->
-    <!--                @click="handleOpenImport"-->
-    <!--              />-->
-    <!--            </flex-box>-->
-    <!--          </flex-box>-->
-    <!--        </panel-card>-->
-    <!--        <external-links />-->
-    <!--      </flex-item>-->
-    <!--    </flex-box>-->
   </private-page>
 </template>
 
@@ -109,9 +63,8 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import { CreatorTab } from "@/types";
-import { projectStore, useScreen } from "@/hooks";
+import { projectStore } from "@/hooks";
 import { navigateTo, QueryParams, Routes } from "@/router";
 import {
   ExternalLinks,
@@ -121,12 +74,7 @@ import {
   PanelCard,
   TextButton,
   ProjectVersionStepper,
-  FlexItem,
 } from "@/components";
-
-const { smallWindow } = useScreen();
-
-const parts = computed(() => (smallWindow.value ? "12" : "6"));
 
 function handleOpenStandard() {
   navigateTo(Routes.PROJECT_CREATOR, {

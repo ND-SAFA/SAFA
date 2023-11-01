fab48dc4a672c15dabbfe68ee1c9698d6b8b99cc
Evan Allan <evanallan1026@gmail.com>
2023-11-01 04:59:32 -0500

    Clean up code a bit

--- src/main/java/edu/nd/crc/safa/features/email/InfobipEmailServiceImpl.java
+++ src/main/java/edu/nd/crc/safa/features/email/InfobipEmailServiceImpl.java
@@ -3,8 +3,6 @@ package edu.nd.crc.safa.features.email;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 
 import edu.nd.crc.safa.features.projects.entities.app.SafaError;
@@ -14,13 +12,14 @@ import com.infobip.ApiClient;
 import com.infobip.ApiKey;
 import com.infobip.BaseUrl;
 import com.infobip.api.EmailApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 /**
- * {@link EmailService} implementation using Infobip's email API. However, it's the default implementation for now
- * to be used if another one is not chosen
+ * {@link EmailService} implementation using Infobip's email API.
  */
 @Service
 @ConditionalOnProperty(
@@ -30,7 +29,7 @@ import org.springframework.stereotype.Service;
 )
 public class InfobipEmailServiceImpl implements EmailService {
 
-    private static final Logger log = Logger.getLogger(InfobipEmailServiceImpl.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(InfobipEmailServiceImpl.class.getName());
 
     @Value("${email.infobip.endpoint}")
     private String infobipEndpoint;
@@ -50,6 +49,9 @@ public class InfobipEmailServiceImpl implements EmailService {
     @Value("${fend.verify-email-path}")
     private String verifyEmailUrl;
 
+    // TODO this is unused until we can implement Infobip
+    //      templates, which is blocked by this issue:
+    //      https://github.com/infobip/infobip-api-java-client/issues/37
     @Value("${email.infobip.verify-email-template-id}")
     private Long verifyEmailTemplateId;
 
@@ -78,20 +80,27 @@ public class InfobipEmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmailVerification(String recipient, String token) {
-        String url = String.format(fendBase + verifyEmailUrl, token);
-        String emailTemplate = getVerificationEmailTemplate();
-        String emailText = String.format(emailTemplate, url);
+        wrapSendEmail(() -> {
+            String url = String.format(fendBase + verifyEmailUrl, token);
+            String emailTemplate = getVerificationEmailTemplate();
+            String emailText = String.format(emailTemplate, url);
 
-        wrapSendEmail(() ->
-            emailApi
+            return emailApi
                 .sendEmail(List.of(recipient))
                 .from(senderEmailAddress)
                 .subject("Verify your email")
                 .html(emailText)
-                .execute()
-        );
+                .execute();
+        });
     }
 
+    /**
+     * Wrap an email send call in a try catch for consistent error handling.
+     *
+     * @param emailSendFunction The function to send the email
+     * @return Whatever the email send function returns
+     * @param <T> The type of the parameter returned by the email send function
+     */
     private <T> T wrapSendEmail(Callable<T> emailSendFunction) {
         try {
             return emailSendFunction.call();
@@ -100,12 +109,19 @@ public class InfobipEmailServiceImpl implements EmailService {
         }
     }
 
+    /**
+     * Get a template email for email verification. This wraps the call to
+     * load the template from the classpath so that if there are any errors,
+     * a default value is used and the email will still get sent.
+     *
+     * @return The text of the email template
+     */
     private String getVerificationEmailTemplate() {
         try {
             return FileUtilities.readClasspathFile("verification-email-template.html");
         } catch (IOException e) {
-            log.log(Level.WARNING, "Failed to get verification email template", e);
-            return "Please click this link to verify your email: %s";
+            log.warn("Failed to get verification email template", e);
+            return "Please use this link to verify your email: %s";
         }
     }
 }

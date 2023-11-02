6b439d116805e4b852087b4e528256c03cf829d8
Alberto Daniel Rodriguez <arodri39@nd.edu>
2023-11-02 09:21:27 -0500

    modifying the push rq and renaming training script

--- tgen/scripts/toolset/tools/rq_tools.py
+++ tgen/scripts/toolset/tools/rq_tools.py
@@ -1,13 +1,23 @@
+import os
+
+from tgen.scripts.modules.script_definition import ScriptDefinition
 from tgen.scripts.run import run_rq
 
 
-def run_rq_tool(rq: str):
+def train(model: str, dataset_path: str):
     """
     Runs RQ.
     :param rq: RQ path.
     :return:
     """
+    data_path = os.environ["CURRENT_PROJECT"]
+    replacements = {
+        "MODEL": model,
+        "CURRENT_PROJECT": os.path.relpath(dataset_path, start="MODEL")
+    }
+    experiment_path = "base/huggingface/train.json"
+    ScriptDefinition.read_experiment_definition()
     run_rq(rq)
 
 
-RQ_TOOLS = [run_rq_tool]
+RQ_TOOLS = [train]

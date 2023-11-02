2a3a8f924aad4a81a35b5e9d1225cb838374bf1e
Alberto Daniel Rodriguez <arodri39@nd.edu>
2023-11-02 10:10:12 -0500

    Debugging experiment def

--- tgen/scripts/toolset/tools/rq_tools.py
+++ tgen/scripts/toolset/tools/rq_tools.py
@@ -1,8 +1,6 @@
 import os
 
 from tgen.scripts.modules.script_definition import ScriptDefinition
-from tgen.scripts.modules.script_runner import ScriptRunner
-from tgen.testres.object_creator import ObjectCreator
 
 
 def train(dataset_path: str, model: str = "all-MiniLM-L6-v2"):
@@ -22,9 +20,10 @@ def train(dataset_path: str, model: str = "all-MiniLM-L6-v2"):
     experiment_path = os.path.join(os.environ["RQ_PATH"], "base/huggingface/train.json")
     experiment_path = os.path.expanduser(experiment_path)
     experiment_definition = ScriptDefinition.read_experiment_definition(experiment_path, replacements)
-    experiment_class = ScriptRunner.get_experiment_class(experiment_definition)
-    experiment = ObjectCreator.create(experiment_class, override=True, **experiment_definition)
-    experiment.run()
+    print(experiment_definition)
+    # experiment_class = ScriptRunner.get_experiment_class(experiment_definition)
+    # experiment = ObjectCreator.create(experiment_class, override=True, **experiment_definition)
+    # experiment.run()
 
 
 RQ_TOOLS = [train]

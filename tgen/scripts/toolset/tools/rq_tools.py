5d82bb31cae6327c8525e58ebe49fc7372020fb4
Alberto Daniel Rodriguez <arodri39@nd.edu>
2023-11-02 12:40:55 -0500

    Accidentally trained the wrong model.

--- tgen/scripts/toolset/tools/rq_tools.py
+++ tgen/scripts/toolset/tools/rq_tools.py
@@ -1,9 +1,25 @@
 import os
+from typing import Dict
 
 from tgen.common.constants.hugging_face_constants import SMALL_EMBEDDING_MODEL
 from tgen.scripts.modules.script_definition import ScriptDefinition
 from tgen.scripts.modules.script_runner import ScriptRunner
 from tgen.testres.object_creator import ObjectCreator
+
+
+def push(input_model_path: str, model_name: str):
+    """
+    Pushes input model to the bug (under thearod5)
+    :param input_model_path: Path to model to push.
+    :param model_name: The name of the model to save to.
+    :return: None
+    """
+    input_model_path = os.path.expanduser(input_model_path)
+    replacements = {
+        "[MODEL_PATH]": input_model_path,
+        "[MODEL_NAME]": model_name
+    }
+    run_rq("base/huggingface/push.json", replacements)
 
 
 def train(dataset_path: str, model: str = SMALL_EMBEDDING_MODEL):
@@ -20,7 +36,11 @@ def train(dataset_path: str, model: str = SMALL_EMBEDDING_MODEL):
         "[CURRENT_PROJECT]": os.path.basename(dataset_path),
         "[OUTPUT_PATH]": os.path.expanduser(os.environ["OUTPUT_PATH"])
     }
-    experiment_path = os.path.join(os.environ["RQ_PATH"], "base/huggingface/train.json")
+    run_rq("base/huggingface/train_st.json", replacements)
+
+
+def run_rq(rq_relative_path: str, replacements: Dict[str, str]):
+    experiment_path = os.path.join(os.environ["RQ_PATH"], rq_relative_path)
     experiment_path = os.path.expanduser(experiment_path)
     experiment_definition = ScriptDefinition.read_experiment_definition(experiment_path, replacements)
     experiment_class = ScriptRunner.get_experiment_class(experiment_definition)
@@ -28,4 +48,4 @@ def train(dataset_path: str, model: str = SMALL_EMBEDDING_MODEL):
     experiment.run()
 
 
-RQ_TOOLS = [train]
+RQ_TOOLS = [train, push]

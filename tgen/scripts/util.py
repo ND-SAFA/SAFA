b93494643657e235dfeca81d9d2b1db86ae78f7e
kat-dearstyne <katherine@jklfamily.com>
2023-11-16 10:17:39 -0600

    add docs

--- tgen/scripts/util.py
+++ tgen/scripts/util.py
@@ -1,18 +1,43 @@
 class CustomReprFunc:
 
     def __init__(self, f, custom_repr):
+        """
+        Wraps a function to have a __repr__
+        :param f: The function being decorated.
+        :param custom_repr: The custom representation method.
+        """
         self.f = f
         self.custom_repr = custom_repr
 
     def __call__(self, *args, **kwargs):
+        """
+        Delegates call to function back to base function.
+        :param args: Positional args.
+        :param kwargs: Keywords args.
+        :return: The return value of the base function.
+        """
         return self.f(*args, **kwargs)
 
     def __repr__(self):
+        """
+        :return: Returns the custom repre.
+        """
         return self.custom_repr(self.f)
 
 
 def set_repr(custom_repr):
-    def set_repr_decorator(f):
+    """
+    The decorator used to wrap functions with a custom repr.
+    :param custom_repr: The custom repr function top use.
+    :return: The wrapped function.
+    """
+
+    def set_repr_decorator(f) -> CustomReprFunc:
+        """
+        Internal decorator using custom repr to wrap function.
+        :param f: The function to wrap.
+        :return: The wrapped function.
+        """
         return CustomReprFunc(f, custom_repr)
 
     return set_repr_decorator
@@ -20,4 +45,9 @@ def set_repr(custom_repr):
 
 @set_repr(lambda f: "bool")
 def bool_constructor(s: str):
+    """
+    The default boolean constructor that uses bool reprbut is able to parse wide range of true responses.
+    :param s: The string to construct into a boolean.
+    :return: The boolean constructed.
+    """
     return s.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']

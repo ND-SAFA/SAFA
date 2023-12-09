22b3740563b2e76b80aaabe3e951ad92a89cc79f
Alberto Daniel Rodriguez <arodri39@nd.edu>
2023-12-08 18:00:34 -0800

    Trying to empty cache after loading models

--- load.py
+++ load.py
@@ -1,10 +1,14 @@
+import gc
 import os
 
+from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 
-CACHE_DIR = os.environ["HF_DATASETS_CACHE"]
+load_dotenv()
+CACHE_DIR = os.environ.get("HF_DATASETS_CACHE", "~/.cache/models")
 CACHED_MODELS = ["sentence-transformers/all-roberta-large-v1",
                  "sentence-transformers/all-MiniLM-L6-v2"]
+os.makedirs(CACHE_DIR, exist_ok=True)
 print("Cache Dir:", CACHE_DIR)
 print("Models:", os.listdir(CACHE_DIR))
 os.makedirs(CACHE_DIR, exist_ok=True)
@@ -12,3 +16,4 @@ for m in CACHED_MODELS:
     model = SentenceTransformer(m, cache_folder=CACHE_DIR)
     print(f"{m} has been cached.")
 print("All models have been cached.")
+gc.collect()
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Celery worker in the background
-# python3 load.py
+python3 load.py
 celery -A api.server worker --loglevel=info &
 gunicorn --bind :80 --env DJANGO_SETTINGS_MODULE=api.server.settings --workers 4 --threads 4 --timeout 0 api.server.wsgi
 

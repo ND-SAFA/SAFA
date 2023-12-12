b9b6921826dc93a59fae7b2518056d6abcb47e84
Tim <tjnewman111@gmail.com>
2023-12-11 19:49:41 -0500

    fix jira hook

--- src/hooks/api/integrations/useJiraApi.ts
+++ src/hooks/api/integrations/useJiraApi.ts
@@ -97,6 +97,8 @@ export const useJiraApi = defineStore("jiraApi", (): JiraApiHook => {
   ): Promise<void> {
     const installationId = integrationsStore.jiraOrganization?.id;
 
+    console.log({ installationId });
+
     if (!installationId) return;
 
     await jiraApi.handleRequest(async () => {

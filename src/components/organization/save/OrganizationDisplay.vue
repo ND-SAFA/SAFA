6775bfc2bc972ba6593f8107f4238ee775fc3b72
Tim <tjnewman111@gmail.com>
2023-12-13 17:23:14 -0500

    moved admin panel to account page

--- src/components/organization/save/OrganizationDisplay.vue
+++ src/components/organization/save/OrganizationDisplay.vue
@@ -24,28 +24,6 @@
 
     <save-organization-inputs v-else />
   </panel-card>
-
-  <panel-card v-if="permissionStore.isSuperuser" title="Admin Controls">
-    <expansion-item label="Toggle Superuser Mode">
-      <q-toggle v-model="adminApiStore.activeSuperuser" class="q-mr-md">
-        <template #default>
-          <typography value="Superuser mode" el="div" />
-          <typography secondary value="Enables all permissions" />
-        </template>
-      </q-toggle>
-    </expansion-item>
-    <expansion-item label="Create Account">
-      <text-input v-model="adminCreateEmail" label="Email" />
-      <text-input v-model="adminCreatePassword" label="Password" />
-      <text-button
-        text
-        color="primary"
-        label="Create Account"
-        icon="invite"
-        @click="handleAdminCreate"
-      />
-    </expansion-item>
-  </panel-card>
 </template>
 
 <script lang="ts">
@@ -58,28 +36,16 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import {
-  adminApiStore,
-  appStore,
-  orgStore,
-  permissionStore,
-  sessionApiStore,
-  sessionStore,
-} from "@/hooks";
+import { computed } from "vue";
+import { appStore, orgStore } from "@/hooks";
 import {
   AttributeChip,
   FlexBox,
   PanelCard,
   TextButton,
   Typography,
-  ExpansionItem,
-  TextInput,
 } from "@/components/common";
 import SaveOrganizationInputs from "./SaveOrganizationInputs.vue";
-
-const adminCreateEmail = ref("");
-const adminCreatePassword = ref("");
 
 const editMode = computed(() => appStore.popups.saveOrg);
 
@@ -93,17 +59,4 @@ const projectCount = computed(
       .map(({ projects }) => projects.length)
       .reduce((a, b) => a + b, 0) + " Projects"
 );
-
-/**
- * As an admin, creates a pre-verified account.
- */
-function handleAdminCreate() {
-  sessionApiStore.handleCreateAccount(
-    {
-      email: adminCreateEmail.value,
-      password: adminCreatePassword.value,
-    },
-    true
-  );
-}
 </script>

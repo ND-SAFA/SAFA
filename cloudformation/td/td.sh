a13691c76405351b07ec5d3102bb79417a211853
Alberto Daniel Rodriguez <arodri39@nd.edu>
2023-12-18 16:42:24 -0800

    Require env file specification

--- cloudformation/td/td.sh
+++ cloudformation/td/td.sh
@@ -12,6 +12,9 @@ read -e profile
 echo "Enter Gen File System ID:"
 read -e fileSystemId
 
+echo "Enter ENV (stage,dev,prod):"
+read -e stage
+
 # Deploy CloudFormation stack with parameters
 aws cloudformation deploy \
   --profile "$profile" \
@@ -20,4 +23,6 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_IAM \
   --parameter-overrides \
     AccountID="$accountId" \
-    GenFileSystemId="$fileSystemId"
+    GenFileSystemId="$fileSystemId" \
+    BendEnvFile="bend-$stage.env" \
+    GenEnvFile="tgen-$stage.env"

ea9e5f663caca56637482025febe70e19e076f99
Evan Allan <evanallan1026@gmail.com>
2023-12-30 01:33:04 -0600

    Create a name for checking for unlimited credits

--- src/main/java/edu/nd/crc/safa/features/permissions/checks/billing/CanAffordChargeCheck.java
+++ src/main/java/edu/nd/crc/safa/features/permissions/checks/billing/CanAffordChargeCheck.java
@@ -1,9 +1,7 @@
 package edu.nd.crc.safa.features.permissions.checks.billing;
 
 import java.util.List;
-import java.util.Set;
 
-import edu.nd.crc.safa.features.organizations.entities.db.PaymentTier;
 import edu.nd.crc.safa.features.permissions.checks.utility.CompositeCheck;
 import edu.nd.crc.safa.features.permissions.checks.utility.OrPermissionCheck;
 
@@ -16,9 +14,7 @@ public class CanAffordChargeCheck extends CompositeCheck {
     public CanAffordChargeCheck(int chargeAmount) {
         super(
             new OrPermissionCheck(List.of(
-                new PaymentTierCheck(Set.of(
-                    PaymentTier.RECURRING, PaymentTier.UNLIMITED
-                )),
+                new HasUnlimitedCreditsCheck(),
                 new MinimumBalanceCheck(chargeAmount)
             ))
         );

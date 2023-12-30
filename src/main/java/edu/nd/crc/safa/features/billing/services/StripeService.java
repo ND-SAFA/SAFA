2cffaf6c36e01a5d6c82d5bb517830041c274bd5
Evan Allan <evanallan1026@gmail.com>
2023-12-30 13:33:43 -0600

    Fix server not starting

--- src/main/java/edu/nd/crc/safa/features/billing/services/StripeService.java
+++ src/main/java/edu/nd/crc/safa/features/billing/services/StripeService.java
@@ -17,6 +17,7 @@ import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 /**
@@ -36,12 +37,12 @@ public class StripeService implements IExternalBillingService {
     @Value("${stripe.credit_product_key}")
     private String creditProductKey;
 
-    @Value("${stripe.webhook_secret}")
+    @Value("${stripe.webhook_key}")
     private String webhookSecret;
 
     private final BillingService billingService;
 
-    public StripeService(@Value("${stripe.api_key}") String apiKey, BillingService billingService) {
+    public StripeService(@Value("${stripe.api_key}") String apiKey, @Lazy BillingService billingService) {
         this.billingService = billingService;
 
         if (apiKey != null && !apiKey.isBlank()) {

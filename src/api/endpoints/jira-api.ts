aa81c5e3fa72c97c9f9ee436ea18b41f08b0867e
Tim <tjnewman111@gmail.com>
2024-01-01 15:45:25 -0500

    fixed the jira import endpoint to the correct one

--- src/api/endpoints/jira-api.ts
+++ src/api/endpoints/jira-api.ts
@@ -123,7 +123,7 @@ export async function getJiraProjects(
   return (
     (
       await buildRequest<{ payload: JiraProjectSchema[] }, "cloudId">(
-        "jiraGetInstallations",
+        "jiraGetProjects",
         { cloudId }
       ).get()
     ).payload || []

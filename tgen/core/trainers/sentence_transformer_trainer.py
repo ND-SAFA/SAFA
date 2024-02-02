4aac188360b04e96b5fbdf76dc44f6f1eb837b39
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-02-02 12:36:33 -0600

    Fixing balancing of batches

--- tgen/core/trainers/sentence_transformer_trainer.py
+++ tgen/core/trainers/sentence_transformer_trainer.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+import numpy as np
 from datasets import Dataset
 from sentence_transformers import InputExample, SentenceTransformer
 from torch import Tensor, nn
@@ -26,6 +27,55 @@ from tgen.data.tdatasets.dataset_role import DatasetRole
 from tgen.embeddings.embeddings_manager import EmbeddingsManager
 from tgen.models.model_manager import ModelManager
 from tgen.models.model_properties import ModelArchitectureType, ModelTask
+
+
+class BalancedBatchSampler:
+    def __init__(self, dataset, batch_size: int):
+        self.indices = list(np.arange(len(dataset)))
+        self.num_samples = len(dataset)
+        self.labels = np.array([example.label for example in dataset])
+        self.n_positive = len([s for s in self.labels if s != 0])
+        self.batch_size = batch_size
+        self.negative_indices = self.create_negative_indices()
+        self.other_indices = self.create_other_indices()
+        self.n_batches = int(min(len(self.other_indices), len(self.negative_indices)) // (self.batch_size / 2))
+
+    def create_negative_indices(self):
+        return [i for i in self.indices if self.labels[i] == 0]
+
+    def create_other_indices(self):
+        return [i for i in self.indices if self.labels[i] != 0]
+
+    def select_negative(self, n_items: int):
+        selected, self.negative_indices = self.select_indices(self.negative_indices, n_items, self.create_negative_indices)
+        return selected
+
+    def select_other(self, n_items: int):
+        selected, self.other_indices = self.select_indices(self.other_indices, n_items, self.create_other_indices)
+        return selected
+
+    @staticmethod
+    def select_indices(indices, n_items, create_indices_method):
+        if n_items > len(indices):
+            logger.info("Resetting indices...")
+            indices = create_indices_method()  # Recreate indices using the provided method
+
+        selected = list(np.random.choice(indices, n_items, replace=False))
+        indices = [index for index in indices if index not in selected]  # Efficient way to remove selected items
+        return selected, indices  # Return both selected items and the updated indices list
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        n_negative = self.batch_size // 2
+        n_pos = self.batch_size - n_negative
+        batch_indices = self.select_negative(n_negative) + self.select_other(n_pos)
+        np.random.shuffle(batch_indices)
+        return batch_indices
+
+    def __len__(self):
+        return self.n_batches
 
 
 class SentenceTransformerTrainer(HuggingFaceTrainer):
@@ -66,7 +116,8 @@ class SentenceTransformerTrainer(HuggingFaceTrainer):
             logger.info("Using only positive links in training dataset.")
 
         train_examples = self.to_input_examples(self.train_dataset, use_scores=self.trainer_args.use_scores, model=self.model)
-        train_dataloader = DataLoader(train_examples, shuffle=self.trainer_args.shuffle, batch_size=self.args.train_batch_size)
+        train_dataloader = DataLoader(train_examples,
+                                      batch_sampler=BalancedBatchSampler(train_examples, batch_size=self.args.train_batch_size))
 
         self.loss_function = self._create_loss_function()
         n_steps = min(len(train_dataloader) + 1, self.min_eval_steps)
@@ -149,8 +200,12 @@ class SentenceTransformerTrainer(HuggingFaceTrainer):
             source_text = i[CSVKeys.SOURCE]
             target_text = i[CSVKeys.TARGET]
             label = float(i[CSVKeys.LABEL])
+            score = i.get(CSVKeys.SCORE, None)
+            if use_scores and score:
+                label = float(score)
             input_examples.append(InputExample(texts=[source_text, target_text], label=label))
-        if use_scores:
+
+        if use_scores and all([i.label is None for i in input_examples]):
             assert model, f"Model is required to be defined if use_scores is True. Received {model}."
             SentenceTransformerTrainer.replace_labels_with_scores(model, input_examples)
         return input_examples

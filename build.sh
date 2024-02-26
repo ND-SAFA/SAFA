c78480eb955aceaa0b2dae810f3f23ab1cd50fda
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-02-26 14:59:52 -0600

    removed dead code

--- build.sh
+++ build.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-#if [ $# -eq 0 ]; then
-#  echo "Error: Container name is required."
-#  echo "Usage: ./build.sh <container>"
-#  exit 1
-#fi
 
-# Check if container 'tgen' exists
-#container_name=${1}
-#if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
-#    echo "Container $container_name exists. Deleting..."
-#    docker stop $container_name # stop current
-#    docker rm -f $container_name # delete old
-#fi
-
-docker-compose build # -t $container_name . # Re-build new
+docker-compose build 
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then

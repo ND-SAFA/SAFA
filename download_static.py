069579236b7dd7b9106b21869c3ff09151fff20a
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-03-06 16:28:51 -0600

    Adding downloading of static libraries to github build steps

--- download_static.py
+++
@@ -1,7 +0,0 @@
-import nltk
-
-nltk.download('stopwords', quiet=True)
-nltk.download('wordnet', quiet=True)
-nltk.download('omw-1.4', quiet=True)
-nltk.download('punkt', quiet=True)
-nltk.download('averaged_perceptron_tagger', quiet=True)

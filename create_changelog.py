2650fa774f8e49dafb77b9a51301f733b0c21d76
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-03-10 13:42:07 -0500

    Adding change log script

--- create_changelog.py
+++ create_changelog.py
@@ -3,6 +3,9 @@ import subprocess
 
 from packaging import version
 
+from tgen.common.logging.logger_manager import logger
+from tgen.common.util.file_util import FileUtil
+
 
 def get_latest_version_from_changelog(directory):
     """
@@ -18,12 +21,14 @@ def get_latest_version_from_changelog(directory):
                 versions.append(version.parse(file_version))
             except ValueError:
                 continue
-    if versions:
-        latest_version = str(max(versions))
-        major, minor, patch = [int(x) for x in latest_version.split('.')]
-        return f"{major}.{minor}.{patch + 1}"
-    else:
-        return "0.0.1"  # Default to start versions if no files are found
+    if not versions:
+        raise Exception(f"Found no versions in {directory}")
+
+    latest_version = str(max(versions))
+    major, minor, patch = [int(x) for x in latest_version.split('.')]
+    previous_version = f"{major}.{minor}.{patch}"
+    new_version = f"{major}.{minor}.{patch + 1}"
+    return previous_version, new_version
 
 
 def get_last_changelog_commit(file_pattern) -> str:
@@ -53,35 +58,26 @@ def get_commit_messages_since(last_commit: str):
     return commits
 
 
-def create_changelog_file(version, messages, file_path):
-    """
-    Create a changelog file from a list of commit messages.
-    """
-    with open(file_path, 'w') as file:
-        file.write(f"# Changelog for Version {version}\n\n")
-        for msg in messages:
-            file.write(f"- {msg}\n")
-
-
-def create_commit_message(version_str: str) -> None:
+def create_commit_message() -> None:
     """
     Create a commit message file for the given version, including all commit messages since the last version.
     :param version_str: The version of the file to create.
     :return: None
     """
-    file_pattern = "changelog/*.md"  # Adjust this pattern to match your changelog files if needed.
-    file_name = f"changelog/{version_str}.md"  # Adjust the path as necessary.
+    previous_version, new_version = get_latest_version_from_changelog("changelog")
+    new_file_name = f"changelog/{new_version}.md"  # Adjust the path as necessary.
+    previous_file_name = f"changelog/{previous_version}.md"
 
-    last_commit = get_last_changelog_commit(file_pattern)
+    last_commit = get_last_changelog_commit(previous_file_name)
     messages = get_commit_messages_since(last_commit)
 
     if len(messages) > 0:
-        create_changelog_file(version_str, messages, file_name)
-        print(f"Changelog file created for version {version_str}: {file_name}")
+        file_content = "\n".join([f"- {m}" for m in messages])
+        FileUtil.write(file_content, new_file_name)
+        logger.info(f"Created new changelog: {new_file_name}")
     else:
         print("No new commits found.")
 
 
 if __name__ == "__main__":
-    version = get_latest_version_from_changelog("changelog")
-    create_commit_message(version)
+    create_commit_message()

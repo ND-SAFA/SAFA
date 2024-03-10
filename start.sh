673d6b731d99bf59e7c137b9461d0a93eaa7f268
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-03-10 13:10:23 -0500

    Removing dump commit

--- start.sh
+++ start.sh
@@ -8,4 +8,3 @@ gunicorn --bind :80 --env DJANGO_SETTINGS_MODULE=api.server.settings --workers 1
 # Optionally, you can add any additional commands or configuration here
 
 # Script execution will halt until the Django server is stopped
-# Adding new comment.

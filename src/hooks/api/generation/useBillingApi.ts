a909fb2ecf10e04858d37e01339445b9321cea17
Tim <tjnewman111@gmail.com>
2024-03-13 18:06:38 -0400

    refactor: added billing api hook def; added name of projects to transaction descriptions

--- src/hooks/api/generation/useBillingApi.ts
+++ src/hooks/api/generation/useBillingApi.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 
 import {
+  BillingApiHook,
   CostEstimateSchema,
   GenerateArtifactSchema,
   IOHandlerCallback,
@@ -19,36 +20,21 @@ import { pinia } from "@/plugins";
 /**
  * This store manages interactions with the billing API.
  */
-export const useBillingApi = defineStore("billingApi", () => {
+export const useBillingApi = defineStore("billingApi", (): BillingApiHook => {
   const billingApi = useApi("billingApi");
 
-  /**
-   * Gets the onboarding status for the current user.
-   * @param callbacks - The callbacks for the action, with the onboarding status as the payload.
-   */
   async function handleGetOnboardingStatus(
     callbacks: IOHandlerCallback<OnboardingStatusSchema>
   ): Promise<void> {
     await billingApi.handleRequest(() => getOnboardingStatus(), callbacks);
   }
 
-  /**
-   * Gets the onboarding status for the current user.
-   * @param status - The new onboarding status.
-   */
   async function handleUpdateOnboardingStatus(
     status: OnboardingStatusSchema
   ): Promise<void> {
     await billingApi.handleRequest(() => setOnboardingStatus(status));
   }
 
-  /**
-   * Estimates the cost of generating artifacts.
-   *
-   * @param configuration - The configuration for generating the artifacts.
-   * @param callbacks - The callbacks for the action.
-   * @return The estimated cost of generating the artifacts, in dollars.
-   */
   async function handleEstimateCost(
     configuration: GenerateArtifactSchema,
     callbacks: IOHandlerCallback<CostEstimateSchema>
@@ -60,36 +46,26 @@ export const useBillingApi = defineStore("billingApi", () => {
     );
   }
 
-  /**
-   * Creates a new checkout session to add credits.
-   * @param amount - The amount of credits to buy.
-   */
-  async function handleCheckoutSession(amount: number): Promise<void> {
+  async function handleCheckoutSession(
+    amount: number,
+    description: string
+  ): Promise<void> {
     await billingApi.handleRequest(async () => {
       const transaction = await createCheckoutSession({
         organizationId: orgStore.orgId,
         amount,
-        description: "Artifact Generation",
+        description,
       });
 
       window.open(transaction.redirectUrl);
     });
   }
 
-  /**
-   * Accepts the payment for the checkout session.
-   * Internally, the BE will still ensure that the
-   * user has enough credits to generate.
-   */
   async function handleAcceptPayment(): Promise<void> {
     await onboardingStore.handleReload(true);
     await onboardingStore.handleGenerateDocumentation(true);
   }
 
-  /**
-   * Cancels the payment for the checkout session.
-   * @param sessionId - The id of the session to cancel.
-   */
   async function handleCancelPayment(sessionId: string): Promise<void> {
     await billingApi.handleRequest(() => deleteCheckoutSession(sessionId));
   }

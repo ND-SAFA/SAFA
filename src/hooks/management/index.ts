d4f9ea75140b09402186a3a1487b10123c3c2db1
Tim <tjnewman111@gmail.com>
2024-03-19 19:27:47 -0400

    moved permission store into mgmt folder to reflect that it relies on the project, org, and team stores

--- src/hooks/management/index.ts
+++ src/hooks/management/index.ts
@@ -2,3 +2,4 @@ export { default as projectStore } from "./useProject";
 export { default as orgStore } from "./useOrg";
 export { default as teamStore } from "./useTeam";
 export { default as onboardingStore } from "./useOnboarding";
+export { default as permissionStore } from "./usePermission";

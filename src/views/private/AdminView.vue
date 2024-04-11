019cad57a9a5f02c53951bc50febae875f8cfa6c
Tim <tjnewman111@gmail.com>
2024-04-11 13:54:05 -0400

    feat: added skeleton of admin page, with check that user is a superuser

---
+++ src/views/private/AdminView.vue
@@ -0,0 +1,16 @@
+<template>
+  <private-page full-window> hello </private-page>
+</template>
+
+<script lang="ts">
+/**
+ * Displays the admin page.
+ */
+export default {
+  name: "AdminView",
+};
+</script>
+
+<script setup lang="ts">
+import { PrivatePage } from "@/components";
+</script>

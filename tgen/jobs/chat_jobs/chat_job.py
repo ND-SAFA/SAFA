8cf8958a39826d45a791d039f83ba9984f58e6a2
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-05-08 09:58:12 -0500

    Refactoring chat job

--- tgen/jobs/chat_jobs/chat_job.py
+++ tgen/jobs/chat_jobs/chat_job.py
@@ -10,7 +10,7 @@ from tgen.data.keys.prompt_keys import PromptKeys
 from tgen.data.keys.structure_keys import ArtifactKeys
 from tgen.jobs.abstract_job import AbstractJob
 from tgen.jobs.components.args.job_args import JobArgs
-from tgen.models.llm.abstract_llm_manager import AbstractLLMManager, PromptRoles, Message
+from tgen.models.llm.abstract_llm_manager import AbstractLLMManager, Message, PromptRoles
 from tgen.models.llm.llm_task import LLMCompletionType
 from tgen.prompts.context_prompt import ContextPrompt
 from tgen.prompts.multi_artifact_prompt import MultiArtifactPrompt
@@ -42,34 +42,66 @@ class ChatJob(AbstractJob):
         """
         for i, meta in enumerate(self.chat_history):
             if meta.artifact_ids or len(self.chat_history) == 1:
-                artifact_df: ArtifactDataFrame = self.job_args.dataset.artifact_df
-                unknown_ids = set(meta.artifact_ids).difference(artifact_df.index)
-                assert not unknown_ids, f"Unknown artifact ids: {unknown_ids}"
-
-                chat_content = self.chat_history[0].message["content"]
-                query_artifact = Artifact(id=f"query_artifact_{i}", content=chat_content, layer_id="query")
-                if meta.artifact_ids:
-                    id2context = {query_artifact[ArtifactKeys.ID]: [artifact_df.get_artifact(a_id) for a_id in meta.artifact_ids]}
-                else:
-                    artifact_df.add_row(query_artifact)
-                    id2context, _ = ContextFinder.find_related_artifacts(query_artifact[ArtifactKeys.ID], self.job_args.dataset,
-                                                                         base_export_dir=self.job_args.export_dir,
-                                                                         max_context=self.max_context)
-                    meta.artifact_ids = [artifact[ArtifactKeys.ID] for artifact in id2context[query_artifact[ArtifactKeys.ID]]]
-                context_prompt = ContextPrompt(id2context,
-                                               prompt_start=PromptUtil.as_markdown_header("Related Information"),
-                                               build_method=MultiArtifactPrompt.BuildMethod.MARKDOWN,
-                                               include_ids=True)
-                chat_prompt = Prompt(f"The related information is provided to help you better understand "
-                                     f"and respond to the following question. If you do not need the information to respond,"
-                                     f"you may ignore it. "
-                                     f"{NEW_LINE}{chat_content}", title="Query")
-                prompt_str = PromptBuilder(prompts=[context_prompt, chat_prompt]).build(model_format_args=self.llm_manager.prompt_args,
-                                                                                        artifact=query_artifact)[PromptKeys.PROMPT]
-                meta.message["content"] = prompt_str
+                self.add_context_to_message(meta, f"query_artifact_{i}")
 
         output = self.llm_manager.make_completion_request(completion_type=LLMCompletionType.GENERATION,
                                                           prompt=MessageMeta.to_llm_messages(self.chat_history))
         response: str = output.batch_responses[0]
+
         message = Message(content=response, role=PromptRoles.ASSISTANT)
         return MessageMeta(message=message, artifact_ids=self.chat_history[-1].artifact_ids)
+
+    def add_context_to_message(self, message: MessageMeta, query_artifact_id: str) -> None:
+        """
+        Adds context to message.
+        :param message: The message to extract context of.
+        :param query_artifact_id: Unique ID to used to create an artifact to gather context for.
+        :return: None
+        """
+        artifact_df: ArtifactDataFrame = self.job_args.dataset.artifact_df
+        self.verify_no_unknown_artifacts(message, artifact_df)
+
+        chat_content = message.message["content"]
+        query_artifact = Artifact(id=query_artifact_id, content=chat_content, layer_id="query")
+        id2context = self.extract_message_context(artifact_df, message, query_artifact)
+        context_prompt = ContextPrompt(id2context,
+                                       prompt_start=PromptUtil.as_markdown_header("Related Information"),
+                                       build_method=MultiArtifactPrompt.BuildMethod.MARKDOWN,
+                                       include_ids=True)
+        chat_prompt = Prompt(f"The related information is provided to help you better understand "
+                             f"and respond to the following question. If you do not need the information to respond,"
+                             f"you may ignore it. "
+                             f"{NEW_LINE}{chat_content}", title="Query")
+        prompt_str = PromptBuilder(prompts=[context_prompt, chat_prompt]).build(model_format_args=self.llm_manager.prompt_args,
+                                                                                artifact=query_artifact)[PromptKeys.PROMPT]
+        message.message["content"] = prompt_str
+
+    def extract_message_context(self, artifact_df: ArtifactDataFrame, meta: MessageMeta, query_artifact: Artifact):
+        """
+        Extracts the artifacts related to the query.
+        :param artifact_df: DataFrame containing artifacts to find for context.
+        :param meta: Message containing optionally defined context artifacts.
+        :param query_artifact: The query used to find which artifacts should be included in context.
+        :return: Map of query ID to artifacts in context.
+        """
+        if meta.artifact_ids:
+            id2context = {query_artifact[ArtifactKeys.ID]: [artifact_df.get_artifact(a_id) for a_id in meta.artifact_ids]}
+        else:
+            artifact_df.add_row(query_artifact)
+            id2context, _ = ContextFinder.find_related_artifacts(query_artifact[ArtifactKeys.ID],
+                                                                 self.job_args.dataset,
+                                                                 base_export_dir=self.job_args.export_dir,
+                                                                 max_context=self.max_context)
+            meta.artifact_ids = [artifact[ArtifactKeys.ID] for artifact in id2context[query_artifact[ArtifactKeys.ID]]]
+        return id2context
+
+    @staticmethod
+    def verify_no_unknown_artifacts(meta: MessageMeta, artifact_df: ArtifactDataFrame):
+        """
+        Verifies that all artifacts referenced in message are contained in dataframe.
+        :param meta: Message whose artifacts are verified.
+        :param artifact_df: Artifat Data Frame to contain artifacts.
+        :return: None
+        """
+        unknown_ids = set(meta.artifact_ids).difference(artifact_df.index)
+        assert not unknown_ids, f"Unknown artifact ids: {unknown_ids}"

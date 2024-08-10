418d5f5ce82ba809304b4154dd17dd8a1d8faebb
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-08-10 11:03:54 -0500

    Moving download_static to inside of tgen to make is accessible to module users

---
+++ tgen/scripts/tools/download_static.py
@@ -0,0 +1,8 @@
+import nltk
+
+if __name__ == "__main__":
+    nltk.download('stopwords', quiet=True)
+    nltk.download('wordnet', quiet=True)
+    nltk.download('omw-1.4', quiet=True)
+    nltk.download('punkt', quiet=True)
+    nltk.download('averaged_perceptron_tagger', quiet=True)

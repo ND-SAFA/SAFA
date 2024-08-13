1c3b95c9e3478b60569387b1f2b7878dfb67d81d
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-08-13 16:23:10 -0500

    adding updated rate limiter

--- common_resources/tools/t_threading/child_thread.py
+++ common_resources/tools/t_threading/child_thread.py
@@ -1,8 +1,6 @@
 import threading
-import time
 from typing import Any, Callable
 
-from common_resources.tools.constants.threading_constants import OVERLOADED_ERROR
 from common_resources.tools.t_logging.logger_manager import logger
 from common_resources.tools.t_threading.threading_state import MultiThreadState
 
@@ -23,46 +21,33 @@ class ChildThread(threading.Thread):
         Performs work on the next available items until no more work is available.
         :return: None
         """
-        work = self.state.get_work()
-        while work is not None:
-            index, item = work
-            work_result = self._perform_work(item, index)
-            self.state.on_item_finished(work_result, index)
+        try:
             work = self.state.get_work()
+            while work is not None:
+                index, item = work
+                work_result = self._perform_work(item, index)
+                self.state.on_item_finished(work_result, index)
+                work = self.state.get_work()
+        except Exception as e:
+            # deals with any possible exceptions that may occur outside of performing work.
+            self.state.on_exception(e)
 
-    def _perform_work(self, item: Any, index: int) -> Any:
+    def _perform_work(self, item: Any, index: int, sleep_time: int = None) -> Any:
         """
         Performs work on item.
         :param item: The item to perform work on.
         :param index: The index of the item.
+        :param sleep_time: Time to sleep before performing work.
         :return: The result of the work.
         """
         attempts = 0
         has_performed_work = False
         while not has_performed_work and self.state.should_attempt_work(attempts):
-            if self.state.pause_work:
-                time.sleep(self.state.sleep_time_on_error)
             if attempts > 0:
                 logger.info(f"Re-trying request...")
             try:
                 attempts += 1
                 thread_result = self.thread_work(item)
-                self.state.pause_work = False
                 return thread_result
             except Exception as e:
-                self._handle_exception(attempts, e, index)
-
-    def _handle_exception(self, attempts: int, e: Exception, index: int) -> None:
-        """
-        Handles exception caused while performing work.
-        :param attempts: The number of attempts on this current work.
-        :param e: The exception thrown.
-        :param index: The item's index
-        :return: None
-        """
-        if not self.state.below_attempt_threshold(attempts):
-            self.state.on_item_fail(e, index=index)
-        else:
-            self.state.on_valid_exception(e)
-        if OVERLOADED_ERROR in str(e).lower():
-            self.state.increase_interval()
+                self.state.on_exception(e=e, attempts=attempts, index=index)
--- common_resources/tools/t_threading/rate_limited_queue.py
+++ common_resources/tools/t_threading/rate_limited_queue.py
@@ -4,6 +4,7 @@ import time
 from typing import Generic, TypeVar
 
 ItemType = TypeVar("ItemType")
+SEC_PER_MIN = 60.0
 
 
 class RateLimitedQueue(Generic[ItemType]):
@@ -16,7 +17,9 @@ class RateLimitedQueue(Generic[ItemType]):
         self.items_per_minute = items_per_minute
         self.lock = threading.Lock()
         self.last_access_time = None
-        self.interval = 60.0 / items_per_minute if items_per_minute else None
+        self.time_between_requests = SEC_PER_MIN / items_per_minute if items_per_minute else None
+        self.pause_event = threading.Event()
+        self.pause_event.set()  # Start with the queue unpaused
 
     def __len__(self) -> int:
         """
@@ -37,25 +40,48 @@ class RateLimitedQueue(Generic[ItemType]):
         Gets the next item in the queue.
         :return: Returns next item in the queue.
         """
-        if self.interval is None:
+        self.pause_event.wait()  # Wait until unpaused
+
+        if self.time_between_requests is None:
             return None if self.queue.qsize() == 0 else self.queue.get()
+
         with self.lock:
             if self.queue.qsize() == 0:
                 return None
-            elapsed_time = time.time() - self.last_access_time if self.last_access_time else self.interval
-            if elapsed_time < self.interval:
-                sleep_time = self.interval - elapsed_time
+
+            elapsed_time = time.time() - self.last_access_time if self.last_access_time else self.time_between_requests
+
+            if elapsed_time < self.time_between_requests:
+                sleep_time = self.time_between_requests - elapsed_time
                 time.sleep(sleep_time)
+
             if self.queue.qsize() == 0:
                 return None
+
             self.last_access_time = time.time()
             item = self.queue.get()
             return item
 
-    def increment_interval(self, delta: float) -> None:
+    def change_time_per_request(self, delta: float) -> None:
         """
         Increments the expected seconds per request.
         :param delta: The delta to increase interval by.
         :return: None
         """
-        self.interval += delta
+        if self.time_between_requests is None:
+            raise Exception("RPM is not set, therefore, cannot change time between requests.")
+        self.time_between_requests += delta
+
+    def pause(self) -> None:
+        """
+        Pauses the queue, preventing items from being retrieved.
+        :return: None
+        """
+        self.pause_event.clear()
+
+    def unpause(self) -> None:
+        """
+        Unpauses the queue, allowing items to be retrieved again.
+        :return: None
+        """
+        self.pause_event.set()

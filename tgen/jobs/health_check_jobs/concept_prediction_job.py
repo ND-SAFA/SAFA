e12bb01f8b06d6d227ee745d581cdb1c93f2ea1a
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-08-14 15:55:28 -0500

    Moved concept predictionto new job ConceptPredictionJob from HealthCheckJob

--- tgen/jobs/health_check_jobs/concept_prediction_job.py
+++ tgen/jobs/health_check_jobs/concept_prediction_job.py
@@ -1,20 +1,21 @@
 import random
 from typing import List, Union
 
+from common_resources.data.dataframes.artifact_dataframe import ArtifactDataFrame
 from common_resources.data.keys.structure_keys import TraceKeys, TraceRelationshipType
 from common_resources.data.tdatasets.prompt_dataset import PromptDataset
 from common_resources.tools.util.reflection_util import ReflectionUtil
 
-from tgen.contradictions.contradictions_args import ContradictionsArgs
-from tgen.contradictions.contradictions_detector import ContradictionsDetector
-from tgen.contradictions.contradictions_result import ContradictionsResult
+from tgen.concepts.concept_args import ConceptArgs
+from tgen.concepts.concept_pipeline import ConceptPipeline
+from tgen.concepts.types.concept_pipeline_response import ConceptPipelineResponse
 from tgen.jobs.abstract_job import AbstractJob
 from tgen.jobs.components.args.job_args import JobArgs
-from tgen.jobs.health_check_jobs.health_check_results import HealthCheckResults
+from tgen.jobs.health_check_jobs.health_check_results import ConceptPredictionResponse
 from tgen.jobs.health_check_jobs.util import expand_query_selection
 
 
-class HealthCheckJob(AbstractJob):
+class ConceptPredictionJob(AbstractJob):
     random.seed(0)
 
     def __init__(self, job_args: JobArgs, query_ids: Union[List[str], str], concept_layer_id: str, **additional_args):
@@ -26,33 +27,37 @@ class HealthCheckJob(AbstractJob):
         :param additional_args: Any additional arguments to the concept or contradictions detectors.
         """
         super().__init__(job_args, require_data=True)
-        self.query_ids = expand_query_selection(query_ids)
+        self.query_ids = expand_query_selection(self.job_args.dataset.artifact_df, query_ids)
         self.concept_layer_id = concept_layer_id
         self.additional_args = additional_args
 
-    def _run(self) -> HealthCheckResults:
+    def _run(self) -> ConceptPredictionResponse:
         """
         Runs health checks on the query artifact.
         :return: Results from each health check.
         """
         dataset: PromptDataset = self.job_args.dataset
-        contradictions_result = self._run_contradictions_detector()
+        concept_matches = self._run_concept_matching(dataset.artifact_df)
         related_traces = dataset.trace_dataset.trace_df.get_relationships(artifact_ids=self.query_ids,
                                                                           artifact_key=TraceKeys.child_label())
         context_traces = [trace for trace in related_traces if trace[TraceKeys.RELATIONSHIP_TYPE] == TraceRelationshipType.CONTEXT]
-        results = HealthCheckResults(context_traces=context_traces,
-                                     contradictions=contradictions_result)
+        results = ConceptPredictionResponse(context_traces=context_traces,
+                                            concept_matches=concept_matches, )
         return results
 
-    def _run_contradictions_detector(self, **kwargs) -> List[ContradictionsResult]:
+    def _run_concept_matching(self, artifact_df: ArtifactDataFrame) -> ConceptPipelineResponse:
         """
-        Runs contradictions detector to identify any conflicting artifacts with the query artifact.
-        :return: Any ids of conflicting artifacts and an explanation as to why if some were found.
+        Runs concept pipeline to identify matches in the artifact.
+        :param artifact_df: Contains all project artifacts.
+        :return: The response from the concept pipeline.
         """
-        pipeline_params = self.job_args.get_args_for_pipeline(ContradictionsArgs)
-        additional_params = ReflectionUtil.get_constructor_params(ContradictionsArgs, self.additional_args)
-        kwargs.update(additional_params)
-        args = ContradictionsArgs(**pipeline_params, **kwargs)
-        detector = ContradictionsDetector(args)
-        contradictions_result = detector.detect(self.query_ids)
-        return contradictions_result
+        pipeline_params = self.job_args.get_args_for_pipeline(ConceptArgs)
+        additional_params = ReflectionUtil.get_constructor_params(ConceptArgs, self.additional_args)
+        pipeline_params.update(additional_params)
+        query_artifacts = artifact_df.filter_by_index(self.query_ids).to_artifacts()
+        assert len(query_artifacts) > 0, f"Unknown query ids : {self.query_ids}"
+
+        args = ConceptArgs(concept_layer_id=self.concept_layer_id, artifacts=query_artifacts, **pipeline_params)
+        pipeline = ConceptPipeline(args)
+        pipeline.run()
+        return pipeline.state.response
--- tgen/jobs/health_check_jobs/health_check_results.py
+++ tgen/jobs/health_check_jobs/health_check_results.py
@@ -1,6 +1,7 @@
 from typing import List, Optional, TypedDict
 
 from common_resources.tools.util.enum_util import EnumDict
+
 from tgen.concepts.types.concept_pipeline_response import ConceptPipelineResponse
 from tgen.contradictions.contradictions_result import ContradictionsResult
 
@@ -8,4 +9,7 @@ from tgen.contradictions.contradictions_result import ContradictionsResult
 class HealthCheckResults(TypedDict):
     context_traces: Optional[List[EnumDict]]
     contradictions: List[ContradictionsResult]
+
+
+class ConceptPredictionResponse(TypedDict):
     concept_matches: Optional[ConceptPipelineResponse]

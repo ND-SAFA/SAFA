776832a5df352a95dfe746e2cbf42da8767302bd
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-08-26 12:35:08 -0500

    HOTFIX: Adding supported enum to health tasks

--- gen/health/types/health_tasks.py
+++ gen/health/types/health_tasks.py
@@ -1,4 +1,7 @@
-class HealthTask:
+from gen_common.util.supported_enum import SupportedEnum
+
+
+class HealthTask(SupportedEnum):
     CONTRADICTION = "contradictions"
     CONCEPT_EXTRACTION = "concept-extraction"
     CONCEPT_MATCHING = "concept-matching"

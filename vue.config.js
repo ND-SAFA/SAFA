22c5d1851925ee42280370be57c2b01f2bc3a0db
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-10-17 10:15:42 -0500

    Adding to desktop app settings

--- vue.config.js
+++ vue.config.js
@@ -38,17 +38,4 @@ module.exports = {
       favicon32: "favicon.ico",
     },
   },
-  devServer: fs.existsSync("./certs/localhost.safa.ai.pem")
-    ? {
-        server: {
-          type: "https",
-          options: {
-            key: fs.readFileSync("./certs/localhost.safa.ai-key.pem"),
-            cert: fs.readFileSync("./certs/localhost.safa.ai.pem"),
-          },
-        },
-        host: "localhost.safa.ai",
-        port: 8080,
-      }
-    : {},
 };
